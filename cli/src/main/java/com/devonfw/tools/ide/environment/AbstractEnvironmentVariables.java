9f772768eb8f12838dc1a9263964f5ae017416f6
CORP\albega <aldo.bega@capgemini.com>
2023-09-19 08:37:02 +0200

    devonfw/ide#9999: Test

--- cli/src/main/java/com/devonfw/tools/ide/environment/AbstractEnvironmentVariables.java
+++ cli/src/main/java/com/devonfw/tools/ide/environment/AbstractEnvironmentVariables.java
@@ -64,7 +64,7 @@ public abstract class AbstractEnvironmentVariables implements EnvironmentVariabl
         this.source = this.source + "@" + propertiesPath;
       }
     }
-    return this.source;
+    return this.source; // Test
   }
 
   /**
