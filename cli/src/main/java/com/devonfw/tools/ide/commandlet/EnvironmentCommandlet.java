95ecb312d73f5dd9d38b27583e45151582a1c996
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2023-09-22 22:36:06 +0200

    #5: add PATH

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/EnvironmentCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/EnvironmentCommandlet.java
@@ -2,6 +2,7 @@ package com.devonfw.tools.ide.commandlet;
 
 import java.util.Collection;
 
+import com.devonfw.tools.ide.common.SystemPath;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.environment.VariableLine;
 import com.devonfw.tools.ide.os.WindowsPathSyntax;
@@ -49,6 +50,8 @@ public final class EnvironmentCommandlet extends Commandlet {
       }
       this.context.info(line.toString());
     }
+    SystemPath path = this.context.getPath();
+    this.context.info("export PATH={}", path.toString(this.bash.isTrue()));
   }
 
   VariableLine normalizeWindowsValue(VariableLine line) {
