cb5199d280e0887b8a764bfd9fdf92ea5a88fa03
aBega2000 <145266540+aBega2000@users.noreply.github.com>
2023-09-29 17:38:06 +0200

    #23: Implemented ToolComandlet for Gradle (#59)

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/CommandletManagerImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/CommandletManagerImpl.java
@@ -12,6 +12,7 @@ import com.devonfw.tools.ide.property.KeywordProperty;
 import com.devonfw.tools.ide.property.Property;
 import com.devonfw.tools.ide.tool.eclipse.Eclipse;
 import com.devonfw.tools.ide.tool.gh.Gh;
+import com.devonfw.tools.ide.tool.gradle.Gradle;
 import com.devonfw.tools.ide.tool.java.Java;
 import com.devonfw.tools.ide.tool.mvn.Mvn;
 
@@ -51,6 +52,7 @@ public final class CommandletManagerImpl implements CommandletManager {
     add(new Gh(context));
     add(new Java(context));
     add(new Mvn(context));
+    add(new Gradle(context));
     add(new Eclipse(context));
   }
 
---
+++ cli/src/main/java/com/devonfw/tools/ide/tool/gradle/Gradle.java
@@ -0,0 +1,31 @@
+package com.devonfw.tools.ide.tool.gradle;
+
+import java.util.Set;
+
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.ToolCommandlet;
+import com.devonfw.tools.ide.tool.java.Java;
+
+/**
+ * {@link ToolCommandlet} for <a href="https://gradle.org/">gradle</a>.
+ */
+public class Gradle extends ToolCommandlet {
+
+  /**
+   * The constructor.
+   *
+   * @param context the {@link IdeContext}.
+   */
+  public Gradle(IdeContext context) {
+
+    super(context, "gradle", Set.of(TAG_JAVA, TAG_BUILD));
+  }
+
+  @Override
+  public boolean install(boolean silent) {
+
+    getCommandlet(Java.class).install();
+    return super.install(silent);
+  }
+
+}
