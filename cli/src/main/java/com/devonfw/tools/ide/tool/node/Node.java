a7081c4e16a88aebeadcb7745fc58fd8cfc2cf9c
aBega2000 <145266540+aBega2000@users.noreply.github.com>
2023-10-05 17:51:04 +0200

    #29: implemented tool commandlet for node.js (#68)

---
+++ cli/src/main/java/com/devonfw/tools/ide/tool/node/Node.java
@@ -0,0 +1,23 @@
+package com.devonfw.tools.ide.tool.node;
+
+import java.util.Set;
+
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.ToolCommandlet;
+
+/**
+ * {@link ToolCommandlet} for <a href="https://nodejs.org/">node</a>.
+ */
+public class Node extends ToolCommandlet {
+
+  /**
+   * The constructor.
+   *
+   * @param context the {@link IdeContext}.
+   */
+  public Node(IdeContext context) {
+
+    super(context, "node", Set.of(TAG_RUNTIME));
+  }
+
+}
