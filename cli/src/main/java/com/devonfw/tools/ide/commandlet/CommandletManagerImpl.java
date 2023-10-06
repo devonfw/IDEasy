a67a66ff19d360e18c94c4f5dc6a30f7949ea08e
salimbouch <145128725+salimbouch@users.noreply.github.com>
2023-10-06 17:16:09 +0200

    #33: implemented Quarkus Toolcommandlet (#65)

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/CommandletManagerImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/CommandletManagerImpl.java
@@ -18,6 +18,7 @@ import com.devonfw.tools.ide.tool.java.Java;
 import com.devonfw.tools.ide.tool.mvn.Mvn;
 import com.devonfw.tools.ide.tool.node.Node;
 import com.devonfw.tools.ide.tool.oc.Oc;
+import com.devonfw.tools.ide.tool.quarkus.Quarkus;
 import com.devonfw.tools.ide.tool.terraform.Terraform;
 import com.devonfw.tools.ide.tool.kotlinc.Kotlinc;
 import com.devonfw.tools.ide.tool.kotlinc.KotlincNative;
@@ -65,6 +66,7 @@ public final class CommandletManagerImpl implements CommandletManager {
     add(new Eclipse(context));
     add(new Terraform(context));
     add(new Oc(context));
+    add(new Quarkus(context));
     add(new Kotlinc(context));
     add(new KotlincNative(context));
   }
