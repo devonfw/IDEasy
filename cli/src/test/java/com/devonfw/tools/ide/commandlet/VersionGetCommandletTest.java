56c956bcba2494b491e35d623c0298c0a635184a
moritzLanger <Moritz.langer2@web.de>
2023-10-09 11:36:29 +0200

    #5 added Tests for VersionCommandlets

---
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionGetCommandletTest.java
@@ -0,0 +1,54 @@
+package com.devonfw.tools.ide.commandlet;
+
+import com.devonfw.tools.ide.cli.CliException;
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.log.IdeLogLevel;
+import org.junit.jupiter.api.Test;
+
+
+/**
+   * Integration test of {@link VersionGetCommandlet}.
+   */
+public class VersionGetCommandletTest extends AbstractIdeContextTest{
+
+    /**
+     * Test of {@link VersionGetCommandlet} run, when Installed Version is null.
+     */
+    @Test
+    public void testVersionGetCommandletRunThrowsCliExeption(){
+      // arrange
+      String path = "workspaces/foo-test/my-git-repo";
+      IdeContext context = newContext("basic", path, false);
+      VersionGetCommandlet versionGet = context.getCommandletManager().getCommandlet(VersionGetCommandlet.class);
+      versionGet.tool.setValueAsString("java");
+      // act
+      try {
+        versionGet.run();
+      } catch (CliException e) {
+        //assert
+        assertThat(e).isInstanceOf(Exception.class)
+            .hasMessageContaining("Tool java is not installed!");
+        return;
+      }
+      //just a false case if versionGet.run() does not throw the Exception
+      assertThat(true).isFalse();
+    }
+
+    /**
+     * Test of {@link VersionGetCommandlet} run.
+     */
+    @Test
+    public void testVersionGetCommandletRun(){
+      //arrange
+      String path = "workspaces/foo-test/my-git-repo";
+      IdeContext context = newContext("basic", path, false);
+      VersionGetCommandlet versionGet = context.getCommandletManager().getCommandlet(VersionGetCommandlet.class);
+      //act
+      versionGet.tool.setValueAsString("mvn");
+      versionGet.run();
+      //assert
+      assertLogMessage(context, IdeLogLevel.INFO, "3.9.4");
+    }
+  }
+
---
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionSetCommandletTest.java
@@ -0,0 +1,29 @@
+package com.devonfw.tools.ide.commandlet;
+
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeContext;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Integration test of {@link VersionSetCommandlet}.
+ */
+public class VersionSetCommandletTest extends AbstractIdeContextTest{
+
+  /**
+   * Test of {@link VersionSetCommandlet} run.
+   */
+  @Test
+  public void testVersionSetCommandletRun() {
+    //arrange
+    String path = "workspaces/foo-test/my-git-repo";
+    IdeContext context = newContext("basic", path, false);
+    VersionSetCommandlet versionSet = context.getCommandletManager().getCommandlet(VersionSetCommandlet.class);
+    versionSet.tool.setValueAsString("mvn");
+    versionSet.version.setValueAsString("1.0.0");
+    //act
+    versionSet.run();
+    //assert
+    assertThat(versionSet.version.getValueAsString()).isEqualTo("1.0.0");
+  }
+}
