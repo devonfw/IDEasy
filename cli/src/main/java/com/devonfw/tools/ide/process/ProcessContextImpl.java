762a5947dd1066b791a5fe202a280711a49d1ecc
Jörg Hohwiller <hohwille@users.noreply.github.com>
2023-10-16 16:47:51 +0200

    never code Java in the browser again...

--- cli/src/main/java/com/devonfw/tools/ide/process/ProcessContextImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/process/ProcessContextImpl.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
