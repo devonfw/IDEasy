baed11e77cf36d7d2fa845732b4c95bfb7226243
salimbouch <145128725+salimbouch@users.noreply.github.com>
2023-10-24 04:50:45 +0200

    changes

--- cli/src/main/java/com/devonfw/tools/ide/tool/GlobalToolCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/GlobalToolCommandlet.java
@@ -1,10 +1,18 @@
 package com.devonfw.tools.ide.tool;
 
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessErrorHandling;
+import com.devonfw.tools.ide.repo.ToolRepository;
+import com.devonfw.tools.ide.version.VersionIdentifier;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 
-public class GlobalToolCommandlet extends ToolCommandlet {
+public abstract class GlobalToolCommandlet extends ToolCommandlet {
   /**
    * The constructor.
    *
@@ -17,4 +25,57 @@ public class GlobalToolCommandlet extends ToolCommandlet {
 
     super(context, tool, tags);
   }
+
+
+    /**
+     * @return the {@link Path} where the main executable file of this tool is installed.
+     */
+   @Override
+    public Path getToolBinary() {
+
+      String path = System.getenv("PATH");
+      String[] pathDirs = path.split(File.pathSeparator);
+      for (String dir : pathDirs) {
+        Path toolPath = Paths.get(dir, getName());
+        if (Files.isExecutable(toolPath)) {
+          return toolPath;
+        }
+      }
+      return null;
+    }
+
+  public Path getToolPath() {
+
+    return getToolBinary();
+  }
+
+    /**
+     * Installs or updates the managed {@link #getName() tool}.
+     *
+     * @param silent - {@code true} if called recursively to suppress verbose logging, {@code false} otherwise.
+     * @return {@code true} if the tool was newly installed, {@code false} if the tool was already installed before and
+     *         nothing has changed.
+     */
+    @Override
+    protected boolean doInstall(boolean silent) {
+
+      String edition = getEdition();
+      ToolRepository toolRepository = this.context.getDefaultToolRepository();
+      VersionIdentifier configuredVersion = getConfiguredVersion();
+      VersionIdentifier resolvedVersion = toolRepository.resolveVersion(this.tool, edition, configuredVersion);
+      Path binaryPath = getToolBinary();
+      if (Files.exists(binaryPath)) {
+        //TODO: check whether resolvedVersion is different than installedVersion, if so update the tool?
+        return false;
+      }
+      // download and install the global tool
+      Path target = toolRepository.download(this.tool, edition, resolvedVersion);
+      ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING).executable(target);
+      pc.run();
+      this.context.success("Successfully installed {} in version {}", this.tool, resolvedVersion);
+      //TODO: create a toolVersionFile? If so, in binDir or rootDir?
+      postInstall();
+      return true;
+    }
+
 }
--- cli/src/main/java/com/devonfw/tools/ide/tool/LocalToolCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/LocalToolCommandlet.java
@@ -1,18 +1,13 @@
 package com.devonfw.tools.ide.tool;
 
 import com.devonfw.tools.ide.commandlet.Commandlet;
-import com.devonfw.tools.ide.common.Tags;
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.environment.EnvironmentVariables;
-import com.devonfw.tools.ide.environment.EnvironmentVariablesType;
 import com.devonfw.tools.ide.io.FileAccess;
 import com.devonfw.tools.ide.io.FileCopyMode;
 import com.devonfw.tools.ide.io.TarCompression;
 import com.devonfw.tools.ide.log.IdeLogLevel;
 import com.devonfw.tools.ide.os.MacOsHelper;
 import com.devonfw.tools.ide.process.ProcessContext;
-import com.devonfw.tools.ide.process.ProcessErrorHandling;
-import com.devonfw.tools.ide.property.StringListProperty;
 import com.devonfw.tools.ide.repo.ToolRepository;
 import com.devonfw.tools.ide.util.FilenameUtil;
 import com.devonfw.tools.ide.version.VersionIdentifier;
@@ -21,10 +16,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.List;
 import java.util.Set;
 
-public class LocalToolCommandlet extends ToolCommandlet {
+public abstract class LocalToolCommandlet extends ToolCommandlet {
   /**
    * The constructor.
    *
@@ -38,37 +32,12 @@ public class LocalToolCommandlet extends ToolCommandlet {
     super(context, tool, tags);
   }
 
-    @Override
-    public void run() {
-
-      runTool(null, this.arguments.asArray());
-    }
-
-    /**
-     * Ensures the tool is installed and then runs this tool with the given arguments.
-     *
-     * @param toolVersion the explicit version (pattern) to run. Typically {@code null} to ensure the configured version
-     *        is installed and use that one. Otherwise the specified version will be installed in the software repository
-     *        without touching and IDE installation and used to run.
-     * @param args the commandline arguments to run the tool.
-     */
-    public void runTool(VersionIdentifier toolVersion, String... args) {
-
-      Path binary;
-      if (toolVersion == null) {
-        install(true);
-        binary = getToolBinary();
-      } else {
-        throw new UnsupportedOperationException("Not yet implemented!");
-      }
-      ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING).executable(binary)
-          .addArgs(args);
-      pc.run();
-    }
 
     /**
      * @return the {@link Path} where the main executable file of this tool is installed.
      */
+
+    @Override
     public Path getToolBinary() {
 
       Path binPath = getToolBinPath();
@@ -82,7 +51,7 @@ public class LocalToolCommandlet extends ToolCommandlet {
     protected boolean isBinary(Path path) {
 
       String filename = path.getFileName().toString();
-      String binaryName = getBinaryName();
+      String binaryName = super.getName();
       if (filename.equals(binaryName)) {
         return true;
       } else if (filename.startsWith(binaryName)) {
@@ -91,20 +60,6 @@ public class LocalToolCommandlet extends ToolCommandlet {
       }
       return false;
     }
-
-    protected String getBinaryName() {
-
-      return this.tool;
-    }
-
-    /**
-     * @return the {@link EnvironmentVariables#getToolEdition(String) tool edition}.
-     */
-    protected String getEdition() {
-
-      return this.context.getVariables().getToolEdition(getName());
-    }
-
 
     /**
      * @return the {@link Path} where the tool is located (installed).
@@ -129,65 +84,7 @@ public class LocalToolCommandlet extends ToolCommandlet {
       return toolPath;
     }
 
-    /**
-     * @return the {@link EnvironmentVariables#getToolVersion(String) tool version}.
-     */
-    public VersionIdentifier getConfiguredVersion() {
 
-      return this.context.getVariables().getToolVersion(getName());
-    }
-
-    /**
-     * @return the currently installed {@link VersionIdentifier version} of this tool or {@code null} if not installed.
-     */
-    public VersionIdentifier getInstalledVersion() {
-
-      Path toolPath = getToolPath();
-      if (!Files.isDirectory(toolPath)) {
-        this.context.trace("Tool {} not installed in {}", getName(), toolPath);
-        return null;
-      }
-      Path toolVersionFile = toolPath.resolve(IdeContext.FILE_SOFTWARE_VERSION);
-      if (!Files.exists(toolVersionFile)) {
-        Path legacyToolVersionFile = toolPath.resolve(IdeContext.FILE_LEGACY_SOFTWARE_VERSION);
-        if (Files.exists(legacyToolVersionFile)) {
-          toolVersionFile = legacyToolVersionFile;
-        } else {
-          this.context.warning("Tool {} is missing version file in {}", getName(), toolVersionFile);
-          return null;
-        }
-      }
-      try {
-        String version = Files.readString(toolVersionFile).trim();
-        return VersionIdentifier.of(version);
-      } catch (IOException e) {
-        throw new IllegalStateException("Failed to read file " + toolVersionFile, e);
-      }
-
-    }
-
-    /**
-     * Method to be called for {@link #install(boolean)} from dependent {@link Commandlet}s.
-     *
-     * @return {@code true} if the tool was newly installed, {@code false} if the tool was already installed before and
-     *         nothing has changed.
-     */
-    public boolean install() {
-
-      return install(true);
-    }
-
-    /**
-     * Performs the installation of the {@link #getName() tool} managed by this {@link Commandlet}.
-     *
-     * @param silent - {@code true} if called recursively to suppress verbose logging, {@code false} otherwise.
-     * @return {@code true} if the tool was newly installed, {@code false} if the tool was already installed before and
-     *         nothing has changed.
-     */
-    public boolean install(boolean silent) {
-
-      return doInstall(silent);
-    }
 
     /**
      * Installs or updates the managed {@link #getName() tool}.
@@ -196,6 +93,8 @@ public class LocalToolCommandlet extends ToolCommandlet {
      * @return {@code true} if the tool was newly installed, {@code false} if the tool was already installed before and
      *         nothing has changed.
      */
+
+    @Override
     protected boolean doInstall(boolean silent) {
 
       VersionIdentifier configuredVersion = getConfiguredVersion();
@@ -224,15 +123,6 @@ public class LocalToolCommandlet extends ToolCommandlet {
       }
       postInstall();
       return true;
-    }
-
-    /**
-     * This method is called after the tool has been newly installed or updated to a new version. Override it to add
-     * custom post intallation logic.
-     */
-    protected void postInstall() {
-
-      // nothing to do by default
     }
 
     /**
@@ -428,65 +318,6 @@ public class LocalToolCommandlet extends ToolCommandlet {
       } else {
         this.context.trace("Extraction is disabled for '{}' hence just moving the downloaded file {}.", getName(), file);
         fileAccess.move(file, targetDir);
-      }
-    }
-
-    /**
-     * List the available versions of this tool.
-     */
-    public void listVersions() {
-
-      List<VersionIdentifier> versions = this.context.getUrls().getSortedVersions(getName(), getEdition());
-      for (VersionIdentifier vi : versions) {
-        this.context.info(vi.toString());
-      }
-    }
-
-    /**
-     * Sets the tool version in the environment variable configuration file.
-     *
-     * @param version the version (pattern) to set.
-     */
-    public void setVersion(String version) {
-
-      if ((version == null) || version.isBlank()) {
-        throw new IllegalStateException("Version has to be specified!");
-      }
-      VersionIdentifier configuredVersion = VersionIdentifier.of(version);
-      if (!configuredVersion.isPattern() && !configuredVersion.isValid()) {
-        this.context.warning("Version {} seems to be invalid", version);
-      }
-      setVersion(configuredVersion, true);
-    }
-
-    /**
-     * Sets the tool version in the environment variable configuration file.
-     *
-     * @param version the version to set. May also be a {@link VersionIdentifier#isPattern() version pattern}.
-     * @param hint - {@code true} to print the installation hint, {@code false} otherwise.
-     */
-    public void setVersion(VersionIdentifier version, boolean hint) {
-
-      EnvironmentVariables variables = this.context.getVariables();
-      EnvironmentVariables settingsVariables = variables.getByType(EnvironmentVariablesType.SETTINGS);
-      String edition = getEdition();
-      String name = EnvironmentVariables.getToolVersionVariable(this.tool);
-      VersionIdentifier resolvedVersion = this.context.getUrls().getVersion(this.tool, edition, version);
-      if (version.isPattern()) {
-        this.context.debug("Resolved version {} to {} for tool {}/{}", version, resolvedVersion, this.tool, edition);
-      }
-      settingsVariables.set(name, resolvedVersion.toString(), false);
-      settingsVariables.save();
-      this.context.info("{}={} has been set in {}", name, version, settingsVariables.getSource());
-      EnvironmentVariables declaringVariables = variables.findVariable(name);
-      if ((declaringVariables != null) && (declaringVariables != settingsVariables)) {
-        this.context.warning(
-            "The variable {} is overridden in {}. Please remove the overridden declaration in order to make the change affect.",
-            name, declaringVariables.getSource());
-      }
-      if (hint) {
-        this.context.info("To install that version call the following command:");
-        this.context.info("ide install {}", this.tool);
       }
     }
 
