76eba655ef3cebc1d1d142340a7257258489e919
salimbouch <145128725+salimbouch@users.noreply.github.com>
2023-11-09 20:24:50 +0100

    implemented run method when project is given as argument

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/RepositoryCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/RepositoryCommandlet.java
@@ -1,14 +1,19 @@
 package com.devonfw.tools.ide.commandlet;
 
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.property.FolderProperty;
+import com.devonfw.tools.ide.property.PathProperty;
 import com.devonfw.tools.ide.property.StringProperty;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RepositoryCommandlet extends Commandlet {
 
 
   private StringProperty setup;
 
-  private StringProperty project;
+  private PathProperty project;
 
   /**
    * The constructor.
@@ -19,10 +24,8 @@ public class RepositoryCommandlet extends Commandlet {
 
     super(context);
     addKeyword(getName());
-    this.setup = add(new StringProperty("setup", true, "setup"));
-    this.project = add(new StringProperty("", false, "project"));
-
-
+    this.setup = add(new StringProperty("setup", true, null));
+    this.project = add(new PathProperty("", false, "project"));
   }
 
   @Override
@@ -33,6 +36,37 @@ public class RepositoryCommandlet extends Commandlet {
 
   @Override
   public void run() {
+
+    Path repositoriesPath = this.context.getSettingsPath().resolve("repositories");
+    Path legacyRepositoriesPath = this.context.getSettingsPath().resolve("projects");
+
+    if (project != null) {
+      Path projectFile = project.getValue();
+      if (!Files.exists(projectFile)) {
+        projectFile = repositoriesPath.resolve(projectFile);
+      }
+      if (!Files.exists(projectFile)) {
+        Path legacyProjectFile = legacyRepositoriesPath.resolve(projectFile);
+        if (Files.exists(legacyProjectFile)) {
+          projectFile = legacyProjectFile;
+        } else {
+          this.context.warning("Could not find " + projectFile);
+          return;
+        }
+      }
+      doImportProject(projectFile, true);
+    } else {
+      //if no project was given, check whether repositoriesPath exists, if not check the legacy repositoriesPath, if not return.
+      Path repositories = Files.exists(repositoriesPath) ? repositoriesPath : Files.exists(legacyRepositoriesPath) ? legacyRepositoriesPath : null;
+      if (repositories == null) return;
+
+      //iterate through repositories and import all active projects
+    }
+  }
+
+  private void doImportProject(Path projectFile, boolean force) {
+
+
 
   }
 }
