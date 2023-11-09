30c92af15a3fd0961f5b203ffd49be8b98de78ca
moritzLanger <Moritz.langer2@web.de>
2023-11-09 12:14:25 +0100

    tar.gz to tgz

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/InstallCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/InstallCommandletTest.java
@@ -46,12 +46,12 @@ private void mockWebServer() {
         get(urlPathEqualTo("/installTest/windows")).willReturn(aResponse().withHeader("Content-Type", "application/zip")
             .withHeader("Content-Length", windowsLength).withStatus(200).withBodyFile("java-17.0.6-windows-x64.zip")));
 
-    Path linuxFilePath = Paths.get("src/test/resources/__files/java-17.0.6-linux-x64.tar.gz");
+    Path linuxFilePath = Paths.get("src/test/resources/__files/java-17.0.6-linux-x64.tgz");
     String linuxLength = linuxFilePath.toFile().length() + "";
     server.stubFor(
         get(urlPathEqualTo("/installTest/linux"))
         .willReturn(aResponse().withHeader("Content-Type", "application/tgz")
-            .withHeader("Content-Length", linuxLength).withStatus(200).withBodyFile("java-17.0.6-linux-x64.tar.gz")));
+            .withHeader("Content-Length", linuxLength).withStatus(200).withBodyFile("java-17.0.6-linux-x64.tgz")));
 
   }
 
