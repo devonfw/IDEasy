82e4b4d155a09073f7af4bdaefbf24f29ad309b5
moritzLanger <Moritz.langer2@web.de>
2023-11-16 15:04:03 +0100

    #127 fixed missing Java.sh for tgz

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/InstallCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/InstallCommandletTest.java
@@ -107,9 +107,9 @@ public class InstallCommandletTest extends AbstractIdeContextTest {
     assertThat(context.getSoftwarePath().resolve("java/InstallTest.txt")).hasContent("This is a test file.");
     assertThat(context.getSoftwarePath().resolve("java/bin/HelloWorld.txt")).hasContent("Hello World!");
     if(context.getSystemInfo().isWindows()){
-      assertThat(context.getSoftwarePath().resolve("java/JavaTest.cmd")).exists();
+      assertThat(context.getSoftwarePath().resolve("java/Java.cmd")).exists();
     } else if (context.getSystemInfo().isLinux() || context.getSystemInfo().isMac()) {
-      assertThat(context.getSoftwarePath().resolve("java/JavaTest.sh")).exists();
+      assertThat(context.getSoftwarePath().resolve("java/Java.sh")).exists();
     }
   }
 }
