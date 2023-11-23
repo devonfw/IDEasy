7b43974811e319476a93b3a61fe010ea3eae1ba1
salimbouch <145128725+salimbouch@users.noreply.github.com>
2023-11-23 02:59:37 +0100

    created class UpdateCommandlet, implemented updateSettings method

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/UpdateCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/UpdateCommandlet.java
@@ -10,6 +10,8 @@ public class UpdateCommandlet extends Commandlet {
 
   public final StringProperty settingsURL;
 
+  private static final String defaultSettingsUrl = "https://github.com/devonfw/ide-settings.git";
+
   /**
    * The constructor.
    *
@@ -49,15 +51,30 @@ public class UpdateCommandlet extends Commandlet {
   private void updateSettings() {
 
     Path settingsPath = this.context.getSettingsPath();
+
     if (Files.isDirectory(settingsPath)) {
       //pull remote settings repo
       this.context.gitPullOrClone(settingsPath, "http");
     } else {
       //clone given settingsUrl
       String settingsUrl = this.settingsURL.getValue();
-      if (settingsUrl != null ) {
-
+      if (settingsUrl == null) {
+        if (this.context.isBatchMode()) {
+          settingsUrl = "";
+        } else {
+          this.context.info("Missing your settings at {} and no Settings URL is defined.");
+          this.context.info("Further details can be found here:");
+          this.context.info("https://github.com/devonfw/IDEasy/blob/main/documentation/settings.asciidoc");
+          this.context.info("Please contact the technical lead of your project to get the SETTINGS_URL for your project.");
+          this.context.info("In case you just want to test IDEasy you may simply hit return to install default settings.");
+          //read line settingsUrl = this.context.readLine();
+        }
       }
+
+      if (settingsUrl.isEmpty()) {
+        settingsUrl = defaultSettingsUrl;
+      }
+      this.context.gitPullOrClone(settingsPath, settingsUrl);
 
     }
 
