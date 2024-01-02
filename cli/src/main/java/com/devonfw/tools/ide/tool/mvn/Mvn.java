231ed0051710bbc2800688c05aaffdb3bf2f1a60
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-01-02 12:19:01 +0100

    Add PluginBasedCommand

--- cli/src/main/java/com/devonfw/tools/ide/tool/mvn/Mvn.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/mvn/Mvn.java
@@ -1,18 +1,19 @@
 package com.devonfw.tools.ide.tool.mvn;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Set;
 
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.PluginBasedCommandlet;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
-import com.devonfw.tools.ide.tool.ide.IdeToolCommandlet;
 import com.devonfw.tools.ide.tool.ide.PluginDescriptor;
 import com.devonfw.tools.ide.tool.java.Java;
 
 /**
  * {@link ToolCommandlet} for <a href="https://maven.apache.org/">maven</a>.
  */
-public class Mvn extends IdeToolCommandlet {
+public class Mvn extends PluginBasedCommandlet {
 
   /**
    * The constructor.
@@ -21,7 +22,7 @@ public class Mvn extends IdeToolCommandlet {
    */
   public Mvn(IdeContext context) {
 
-    super(context, "mvn", Set.of(TAG_JAVA, TAG_BUILD, TAG_IDE));
+    super(context, "mvn", Set.of(TAG_JAVA, TAG_BUILD));
   }
 
   @Override
@@ -43,8 +44,9 @@ public class Mvn extends IdeToolCommandlet {
         if (Files.exists(mavenPlugin)) {
           this.context.success("Successfully added {} to {}", plugin.getName(), mavenPlugin.toString());
         } else {
-          this.context.warning("Plugin " + mavenPlugin.getFileName() + " has wrong properties\n" //
-              + "Please check the plugin properties file in");
+          this.context.warning("Plugin {} has wrong properties\n" //
+              + "Please check the plugin properties file in {}", mavenPlugin.getFileName(),
+              mavenPlugin.toAbsolutePath());
         }
       }
     }
@@ -54,12 +56,6 @@ public class Mvn extends IdeToolCommandlet {
   private String createPluginPath(String pluginName) {
 
     return "lib/ext/" + pluginName + ".jar";
-  }
-
-  // TODO: Delete this function after abstract PluginBasedToolCommandlet class is implemented
-  @Override
-  public void installPlugin(PluginDescriptor plugin) {
-
   }
 
 }
