fa8a3f3e09afb0484cde7040a5a75abba680ad9f
moritzLanger <44358321+moritzLanger@users.noreply.github.com>
2024-01-11 11:32:45 +0100

    Update VersionListCommandletTest.java

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionListCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionListCommandletTest.java
@@ -11,22 +11,5 @@ import com.devonfw.tools.ide.log.IdeLogLevel;
  */
 public class VersionListCommandletTest extends AbstractIdeContextTest {
 
-  /**
-   * Test of {@link VersionListCommandlet} run.
-   */
-  @Test
-  public void testVersionListCommandletRun() {
-
-    // arrange
-    String path = "workspaces/foo-test/my-git-repo";
-    IdeTestContext context = newContext("basic", path, false);
-    VersionListCommandlet versionList = context.getCommandletManager().getCommandlet(VersionListCommandlet.class);
-    versionList.tool.setValueAsString("mvn");
-    // act
-    versionList.run();
-    // assert
-    assertLogMessage(context, IdeLogLevel.INFO, "3.0.5");
-    assertLogMessage(context, IdeLogLevel.INFO, "3.1.0");
-    assertLogMessage(context, IdeLogLevel.INFO, "3.2.1");
-  }
+ 
 }
