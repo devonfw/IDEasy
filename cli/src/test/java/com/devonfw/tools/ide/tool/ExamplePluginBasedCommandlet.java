fa0bebc31a23e00cb729eec534709a6c284db9cf
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-01-22 15:12:30 +0100

    #93: Support for Maven Plugins (#141)

---
+++ cli/src/test/java/com/devonfw/tools/ide/tool/ExamplePluginBasedCommandlet.java
@@ -0,0 +1,27 @@
+package com.devonfw.tools.ide.tool;
+
+import java.util.Set;
+
+import com.devonfw.tools.ide.common.Tag;
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.ide.PluginDescriptor;
+
+public class ExamplePluginBasedCommandlet extends PluginBasedCommandlet {
+  /**
+   * The constructor.
+   *
+   * @param context the {@link IdeContext}.
+   * @param tool the {@link #getName() tool name}.
+   * @param tags the {@link #getTags() tags} classifying the tool. Should be created via {@link Set#of(Object) Set.of}
+   * method.
+   */
+  public ExamplePluginBasedCommandlet(IdeContext context, String tool, Set<Tag> tags) {
+
+    super(context, tool, tags);
+  }
+
+  @Override
+  public void installPlugin(PluginDescriptor plugin) {
+
+  }
+}
---
+++ cli/src/test/java/com/devonfw/tools/ide/tool/PluginBasedCommandletTest.java
@@ -0,0 +1,41 @@
+package com.devonfw.tools.ide.tool;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.devonfw.tools.ide.common.Tag;
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeTestContext;
+import com.devonfw.tools.ide.tool.ide.PluginDescriptor;
+
+public class PluginBasedCommandletTest extends AbstractIdeContextTest {
+
+  @Test
+   void testGetPluginsMap() {
+    IdeTestContext context = newContext(PROJECT_BASIC, "", true);
+    String tool = "eclipse";
+    Set<Tag> tags = null;
+    ExamplePluginBasedCommandlet pluginBasedCommandlet = new ExamplePluginBasedCommandlet(context, tool, tags);
+
+    Map<String, PluginDescriptor> pluginsMap = pluginBasedCommandlet.getPluginsMap();
+    assertNotNull(pluginsMap);
+
+    assertTrue(pluginsMap.containsKey("checkstyle"));
+    assertTrue(pluginsMap.containsKey("anyedit"));
+
+    PluginDescriptor plugin1 = pluginsMap.get("checkstyle");
+    assertNotNull(plugin1);
+    assertEquals("checkstyle", plugin1.getName());
+
+    PluginDescriptor plugin2 = pluginsMap.get("anyedit");
+    assertNotNull(plugin2);
+    assertEquals("anyedit", plugin2.getName());
+
+    // Check if anyedit plugin has value "false" --> value from user directory
+    assertEquals(false, plugin2.isActive());
+  }
+}
