e60ea12b2ae1ad0a73c4d29d2c738d033de95b12
creitz25 <carsten.reitz@capgemini.com>
2024-01-25 11:05:25 +0100

    #15 Implement ToolCommandlet for Cobigen
    Fixing Tag to new Syntax and change to GENERATOR
    Dependency changed from Java to Mvn

--- cli/src/main/java/com/devonfw/tools/ide/tool/cobigen/Cobigen.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/cobigen/Cobigen.java
@@ -2,10 +2,11 @@ package com.devonfw.tools.ide.tool.cobigen;
 
 import java.util.Set;
 
+import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
-import com.devonfw.tools.ide.tool.java.Java;
+import com.devonfw.tools.ide.tool.mvn.Mvn;
 
 /**
  * {@link ToolCommandlet} for cobigen CLI (cobigen).
@@ -19,13 +20,13 @@ public class Cobigen extends LocalToolCommandlet {
    */
   public Cobigen(IdeContext context) {
 
-    super(context, "cobigen", Set.of(TAG_CLOUD, TAG_IAC));
+    super(context, "cobigen", Set.of(Tag.GENERATOR));
   }
 
   @Override
   public boolean install(boolean silent) {
 
-    getCommandlet(Java.class).install();
+    getCommandlet(Mvn.class).install();
     return super.install(silent);
   }
 
