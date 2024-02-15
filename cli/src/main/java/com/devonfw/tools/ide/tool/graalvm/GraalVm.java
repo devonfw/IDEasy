ceb73dfb2636b8392bb9b39445238a767df07fb9
Vincent Heußer <vincent.heusser@capgemini.com>
2024-02-15 08:35:42 +0100

    #22: add comment/description for graalvm class

--- cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
@@ -6,6 +6,10 @@ import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 
+/**
+ * {@link LocalToolCommandlet} for <a href="https://www.graalvm.org/">GraalVM</a>, an advanced JDK with ahead-of-time
+ * Native Image compilation.
+ */
 public class GraalVm extends LocalToolCommandlet {
   /**
    * The constructor.
