c0f8ec19327ac26bbba39d1834779d4e028cda2b
Jörg Hohwiller <hohwille@users.noreply.github.com>
2024-02-19 12:11:45 +0100

    Fixed package of JmcTest

--- cli/src/test/java/com/devonfw/tools/ide/tool/jmc/JmcTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/jmc/JmcTest.java
@@ -1,4 +1,4 @@
-package com.devonfw.tools.ide.tool.Jmc;
+package com.devonfw.tools.ide.tool.jmc.Jmc;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
