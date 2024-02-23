a2995047e495359bc52a520ae64b122bb1707a59
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2024-02-24 00:38:11 +0100

    #103: implemented requested changes added javadocs

--- cli/src/main/java/com/devonfw/tools/ide/tool/SecurityRiskInteractionAnswer.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/SecurityRiskInteractionAnswer.java
@@ -1,13 +1,29 @@
 package com.devonfw.tools.ide.tool;
 
+/**
+ * User interaction answers when a security risk was found and the user can f.e. choose to stay on the current unsafe
+ * version, use the latest safe version, use the latest version or use the next safe version.
+ */
 public enum SecurityRiskInteractionAnswer {
 
+  /**
+   * User answer to stay on the current unsafe version.
+   */
   STAY,
 
+  /**
+   * User answer to install the latest of all safe versions.
+   */
   LATEST_SAFE,
 
+  /**
+   * User answer to use the latest safe version.
+   */
   SAFE_LATEST,
 
+  /**
+   * User answer to use the next safe version.
+   */
   NEXT_SAFE,
 
 }
