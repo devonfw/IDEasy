1975ab209c910baf47d492eb8328796bf60d73c2
Ouchen <mouchen1998+maingit@gmail.com>
2024-02-27 13:34:18 +0100

    using const

--- cli/src/main/java/com/devonfw/tools/ide/tool/aws/AwsFileExtractor.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/aws/AwsFileExtractor.java
@@ -15,6 +15,7 @@ import com.devonfw.tools.ide.tool.ToolCommandlet;
  * Implements a {@link CommandletFileExtractorImpl} for {@link Aws}
  */
 public class AwsFileExtractor extends CommandletFileExtractorImpl {
+
   public AwsFileExtractor(IdeContext context, ToolCommandlet commandlet) {
 
     super(context, commandlet);
@@ -23,11 +24,15 @@ public class AwsFileExtractor extends CommandletFileExtractorImpl {
   @Override
   public void moveAndProcessExtraction(Path from, Path to) {
 
+    final String aws = "aws";
+    final String dist = "dist";
+    final String awsCompleter = "aws_completer";
+
     if (this.context.getSystemInfo().isLinux()) {
       // make binary executable using java nio because unpacking didn't preserve the file permissions
       // TODO this can be removed if issue #132 is fixed
-      Path awsInDistPath = from.resolve("dist").resolve("aws");
-      Path awsCompleterInDistPath = from.resolve("dist").resolve("aws_completer");
+      Path awsInDistPath = from.resolve(dist).resolve(aws);
+      Path awsCompleterInDistPath = from.resolve(dist).resolve(awsCompleter);
       makeExecutable(awsInDistPath);
       makeExecutable(awsCompleterInDistPath);
 
@@ -41,7 +46,7 @@ public class AwsFileExtractor extends CommandletFileExtractorImpl {
       // The install-script that aws ships creates symbolic links to binaries but using absolute paths.
       // Since the current process happens in a temporary dir, these links wouldn't be valid after moving the
       // installation files to the target dir. So the absolute paths are replaced by relative ones.
-      for (String file : new String[] { "aws", "aws_completer", Path.of("v2").resolve("current").toString() }) {
+      for (String file : new String[] { aws, awsCompleter, Path.of("v2").resolve("current").toString() }) {
         Path link = from.resolve(file);
         try {
           this.context.getFileAccess().symlink(link.toRealPath(), link, true);
@@ -51,7 +56,7 @@ public class AwsFileExtractor extends CommandletFileExtractorImpl {
         }
       }
       this.context.getFileAccess().delete(linuxInstallScript);
-      this.context.getFileAccess().delete(from.resolve("dist"));
+      this.context.getFileAccess().delete(from.resolve(dist));
     }
     super.moveAndProcessExtraction(from, to);
   }
