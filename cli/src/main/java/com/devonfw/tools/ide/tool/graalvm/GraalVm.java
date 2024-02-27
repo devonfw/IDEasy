ec5fb463d6a75b5428b1536245256ace80616115
Vincent Heußer <vincent.heusser@capgemini.com>
2024-02-27 10:43:33 +0100

    #22: add private methods and override postInstall/getToolpath

--- cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
@@ -1,9 +1,18 @@
 package com.devonfw.tools.ide.tool.graalvm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.io.FileAccess;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessErrorHandling;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 
 /**
@@ -20,4 +29,78 @@ public class GraalVm extends LocalToolCommandlet {
 
     super(context, "graalvm", Set.of(Tag.JAVA));
   }
+
+  @Override
+  public Path getToolPath() {
+
+    return this.context.getSoftwarePath().resolve("extra").resolve(getName());
+  }
+
+  @Override
+  public void run() {
+
+    try {
+      Files.createDirectories(getToolPath());
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to create new directory:" + e.getMessage());
+    }
+
+    String[] args = this.arguments.asArray();
+    runTool(true, null, args);
+
+    if (args.length > 0) {
+      Path toolPath = getToolPath();
+      Path binaryPath = this.context.getUserHome().resolve(toolPath + "/bin");
+      FileAccess fileAccess = this.context.getFileAccess();
+
+      Path executableFile = fileAccess.findFirst(binaryPath,
+          p -> p.getFileName().toString().startsWith(args[0] + ".exe")
+              | p.getFileName().toString().startsWith(args[0] + ".cmd"),
+          false);
+
+      if (executableFile != null) {
+        ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING)
+            .executable(executableFile).addArgs(Arrays.copyOfRange(args, 1, args.length));
+        pc.run(false, false);
+      }
+    }
+  }
+
+  @Override
+  public void postInstall() {
+
+    super.postInstall();
+    Path devonIdeHome = this.context.getConfPath().resolve("devon.properties");
+    final String graalvmExport = "export GRAALVM_HOME="
+        + this.context.getSoftwarePath().resolve("extra").resolve(getName());
+
+    if (isPatternInPath(devonIdeHome, graalvmExport) == false) {
+      writeTextToFile(devonIdeHome, graalvmExport);
+    }
+  }
+
+  private boolean isPatternInPath(Path path, String textToSearch) {
+
+    try (BufferedReader br = Files.newBufferedReader(path)) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        if (line.contains(textToSearch)) {
+          return true;
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(("Failed to open a file for reading:" + e.getMessage()));
+    }
+    return false;
+  }
+
+  private void writeTextToFile(Path path, String textToAdd) {
+
+    try {
+      Files.write(path, textToAdd.getBytes(), StandardOpenOption.APPEND);
+    } catch (IOException e) {
+      throw new RuntimeException("Unable to write text to file:" + e.getMessage());
+    }
+  }
+
 }
