f38b3105f3d92472f07749bddf903d31c70068c1
Ouchen <mouchen1998+maingit@gmail.com>
2024-02-29 10:24:59 +0100

    Forget dot

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/FileExtractor/ExctractorFileType.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/FileExtractor/ExctractorFileType.java
@@ -1,7 +1,7 @@
 package com.devonfw.tools.ide.commandlet.FileExtractor;
 
 /**
- * Enum to represent different file types which can be extracted using {@link CommandletFileExtractorImpl}
+ * Enum to represent different file types which can be extracted using {@link CommandletFileExtractorImpl}.
  */
 enum ExtractorFileType {
   ZIP, JAR, DMG, MSI, PKG, TAR, GZ, TGZ
