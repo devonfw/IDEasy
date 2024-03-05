df57163574e5383816a17d373dfa6eae36d108f5
Marco Vomiero <colocomarco1@gmail.com>
2024-03-05 15:05:17 +0100

    Tests implemented and working

--- cli/src/test/java/com/devonfw/tools/ide/tool/jasypt/JasyptTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/jasypt/JasyptTest.java
@@ -1,31 +1,30 @@
 package com.devonfw.tools.ide.tool.jasypt;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import com.devonfw.tools.ide.commandlet.InstallCommandlet;
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 import com.devonfw.tools.ide.log.IdeLogLevel;
 import com.devonfw.tools.ide.os.SystemInfo;
 import com.devonfw.tools.ide.os.SystemInfoMock;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Integration test of {@link Jasypt}.
  */
 public class JasyptTest extends AbstractIdeContextTest {
 
-  private static final String PROJECT_JMC = "jasypt";
+  private static final String PROJECT_JASYPT = "jasypt";
 
   @Test
   public void testJasyptInstallCommandlet() throws IOException {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_JMC);
+    IdeTestContext context = newContext(PROJECT_JASYPT);
     InstallCommandlet install = context.getCommandletManager().getCommandlet(InstallCommandlet.class);
     install.tool.setValueAsString("jasypt", context);
     // act
@@ -39,7 +38,7 @@ public class JasyptTest extends AbstractIdeContextTest {
   public void testJasyptInstall() {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_JMC);
+    IdeTestContext context = newContext(PROJECT_JASYPT);
 
     Jasypt commandlet = new Jasypt(context);
 
@@ -50,14 +49,13 @@ public class JasyptTest extends AbstractIdeContextTest {
     checkInstallation(context);
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = { "windows", "mac", "linux" })
-  public void testJasyptRun(String os) {
+  @Test
+  public void testJasyptRun() {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_JMC);
-    SystemInfo systemInfo = SystemInfoMock.of(os);
-    context.setSystemInfo(systemInfo);
+    IdeTestContext context = newContext(PROJECT_JASYPT);
+    //SystemInfo systemInfo = SystemInfoMock.of(os);
+   // context.setSystemInfo(systemInfo);
     Jasypt commandlet = new Jasypt(context);
     commandlet.arguments.setValue(List.of("foo", "bar"));
     // act
@@ -65,22 +63,21 @@ public class JasyptTest extends AbstractIdeContextTest {
 
     // assert
     assertLogMessage(context, IdeLogLevel.INFO, "java jasypt");
-    assertLogMessage(context, IdeLogLevel.INFO, "jasypt " + os + " foo bar");
+    //assertLogMessage(context, IdeLogLevel.INFO, "jasypt " + os + " foo bar");
     checkInstallation(context);
   }
 
   private void checkInstallation(IdeTestContext context) {
 
     assertThat(context.getSoftwarePath().resolve("java/bin/java")).exists();
+    assertThat(context.getSoftwarePath().resolve("mvn/bin/mvn")).exists();
 
-    if (context.getSystemInfo().isWindows() || context.getSystemInfo().isLinux()) {
-      assertThat(context.getSoftwarePath().resolve("jasypt/HelloWorld.txt")).hasContent("Hello World!");
-      assertThat(context.getSoftwarePath().resolve("jasypt/JDK Mission Control")).doesNotExist();
-    } else if (context.getSystemInfo().isMac()) {
-      assertThat(context.getSoftwarePath().resolve("jasypt/jasypt")).exists();
-    }
-    assertThat(context.getSoftwarePath().resolve("jasypt/.ide.software.version")).exists().hasContent("8.3.0");
-    assertLogMessage(context, IdeLogLevel.SUCCESS, "Successfully installed jasypt in version 8.3.0");
+    assertThat(context.getSoftwarePath().resolve("jasypt/META-INF/HelloWorld.txt")).hasContent("Hello World!");
+    assertThat(context.getSoftwarePath().resolve("jasypt/org/HelloWorld.txt")).hasContent("Hello World!");
+    assertThat(context.getSoftwarePath().resolve("jasypt/JDK Mission Control")).doesNotExist();
+
+    assertThat(context.getSoftwarePath().resolve("jasypt/.ide.software.version")).exists().hasContent("1.9.3");
+    assertLogMessage(context, IdeLogLevel.SUCCESS, "Successfully installed jasypt in version 1.9.3");
   }
 
 }
