5f4e9b2d3e96ec7e53ed931162e17d9cbe771278
diiinesh <63147290+diiinesh@users.noreply.github.com>
2024-03-15 11:27:16 +0100

    changed testcontext declaration

--- cli/src/test/java/com/devonfw/tools/ide/tool/PluginBasedCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/PluginBasedCommandletTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class PluginBasedCommandletTest extends AbstractIdeContextTest {
 
- public IdeTestContext context = newContext(PROJECT_BASIC, "", false);
+ public IdeTestContext context = newContext(PROJECT_BASIC);
  public ExamplePluginBasedCommandlet pluginBasedCommandlet = new ExamplePluginBasedCommandlet(context, "eclipse", null);
 
  @Test
