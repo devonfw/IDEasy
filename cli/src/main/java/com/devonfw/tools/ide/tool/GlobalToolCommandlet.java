e7a5327343b9d78c5d1d3c484be863e13d02c88b
KGoroll1 <136723015+KGoroll1@users.noreply.github.com>
2024-03-19 12:54:43 +0100

    #16: implement tool commandlet docker (#249)

--- cli/src/main/java/com/devonfw/tools/ide/tool/GlobalToolCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/GlobalToolCommandlet.java
@@ -1,5 +1,11 @@
 package com.devonfw.tools.ide.tool;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.io.FileAccess;
@@ -8,10 +14,6 @@ import com.devonfw.tools.ide.process.ProcessContext;
 import com.devonfw.tools.ide.process.ProcessErrorHandling;
 import com.devonfw.tools.ide.repo.ToolRepository;
 import com.devonfw.tools.ide.version.VersionIdentifier;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Set;
 
 /**
  * {@link ToolCommandlet} that is installed globally.
@@ -24,11 +26,68 @@ public abstract class GlobalToolCommandlet extends ToolCommandlet {
    * @param context the {@link IdeContext}.
    * @param tool the {@link #getName() tool name}.
    * @param tags the {@link #getTags() tags} classifying the tool. Should be created via {@link Set#of(Object) Set.of}
-   * method.
+   *        method.
    */
   public GlobalToolCommandlet(IdeContext context, String tool, Set<Tag> tags) {
 
     super(context, tool, tags);
+  }
+
+  /**
+   * Performs the installation of the {@link #getName() tool} via a package manager.
+   *
+   * @param silent - {@code true} if called recursively to suppress verbose logging, {@code false} otherwise.
+   * @param commands - A {@link Map} containing the commands used to perform the installation for each package manager.
+   * @return {@code true} if the tool was newly installed, {@code false} if the tool was already installed before and
+   *         nothing has changed.
+   */
+  protected boolean installWithPackageManger(Map<PackageManager, List<String>> commands, boolean silent) {
+
+    Path binaryPath = this.context.getPath().findBinary(Path.of(getBinaryName()));
+
+    if (binaryPath != null && Files.exists(binaryPath) && !this.context.isForceMode()) {
+      IdeLogLevel level = silent ? IdeLogLevel.DEBUG : IdeLogLevel.INFO;
+      this.context.level(level).log("{} is already installed at {}", this.tool, binaryPath);
+      return false;
+    }
+
+    Path bashPath = this.context.getPath().findBinary(Path.of("bash"));
+    if (bashPath == null || !Files.exists(bashPath)) {
+      context.warning("Bash was not found on this machine. Not Proceeding with installation of tool " + this.tool);
+      return false;
+    }
+
+    PackageManager foundPackageManager = null;
+    for (PackageManager pm : commands.keySet()) {
+      if (Files.exists(this.context.getPath().findBinary(Path.of(pm.toString().toLowerCase())))) {
+        foundPackageManager = pm;
+        break;
+      }
+    }
+
+    int finalExitCode = 0;
+    if (foundPackageManager == null) {
+      context.warning("No supported Package Manager found for installation");
+      return false;
+    } else {
+      List<String> commandList = commands.get(foundPackageManager);
+      if (commandList != null) {
+        for (String command : commandList) {
+          ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING).executable(bashPath)
+              .addArgs("-c", command);
+          finalExitCode = pc.run();
+        }
+      }
+    }
+
+    if (finalExitCode == 0) {
+      this.context.success("Successfully installed {}", this.tool);
+    } else {
+      this.context.warning("{} was not successfully installed", this.tool);
+      return false;
+    }
+    postInstall();
+    return true;
   }
 
   @Override
@@ -79,5 +138,4 @@ public abstract class GlobalToolCommandlet extends ToolCommandlet {
     postInstall();
     return true;
   }
-
 }
---
+++ cli/src/main/java/com/devonfw/tools/ide/tool/PackageManager.java
@@ -0,0 +1,5 @@
+package com.devonfw.tools.ide.tool;
+
+public enum PackageManager {
+  APT, ZYPPER, YUM, DNF
+}
