67596e0d0d102fb2cdaa72a9a04520fe85ba125a
Vincent Heußer <vincent.heusser@capgemini.com>
2024-04-05 14:50:02 +0200

    #22: refactored GraalVm and GraalVmTest

--- cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/graalvm/GraalVm.java
@@ -1,19 +1,14 @@
 package com.devonfw.tools.ide.tool.graalvm;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.io.FileAccess;
-import com.devonfw.tools.ide.process.ProcessContext;
-import com.devonfw.tools.ide.process.ProcessErrorHandling;
-import com.devonfw.tools.ide.process.ProcessMode;
+import com.devonfw.tools.ide.environment.EnvironmentVariables;
+import com.devonfw.tools.ide.environment.EnvironmentVariablesType;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
@@ -56,78 +51,15 @@ public class GraalVm extends LocalToolCommandlet {
     } catch (IOException e) {
       throw new RuntimeException("Failed to create new directory:" + e);
     }
-
-    String[] args = this.arguments.asArray();
-
-    runTool(ProcessMode.BACKGROUND, null, args);
-    runCommand(args, toolPath);
-  }
-
-  @Override
-  public void runTool(ProcessMode processMode, VersionIdentifier toolVersion, String... args) {
-
-    if (toolVersion == null) {
-      install(true);
-    } else {
-      throw new UnsupportedOperationException("Not yet implemented!");
-    }
+    super.install(true);
   }
 
   @Override
   public void postInstall() {
 
+    EnvironmentVariables envVars = this.context.getVariables().getByType(EnvironmentVariablesType.CONF);
+    envVars.set("GRAALVM_HOME", getToolPath().toString(), true);
+    envVars.save();
     super.postInstall();
-    Path devonIdeHome = this.context.getConfPath().resolve("devon.properties");
-    final String graalvmExport = "export GRAALVM_HOME=" + getToolPath();
-
-    if (!isTextInFile(devonIdeHome, graalvmExport)) {
-      addTextToFile(devonIdeHome, graalvmExport);
-    }
   }
-
-  private void runCommand(String[] args, Path toolPath) {
-
-    if (args.length > 0) {
-      Path binaryPath = this.context.getUserHome().resolve(toolPath + "/bin");
-
-      FileAccess fileAccess = this.context.getFileAccess();
-      Path executableFile = fileAccess.findFirst(binaryPath,
-          p -> p.getFileName().toString().startsWith(args[0] + ".exe")
-              | p.getFileName().toString().startsWith(args[0] + ".cmd"),
-          false);
-
-      if (executableFile != null) {
-        ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING)
-            .executable(executableFile).addArgs(Arrays.copyOfRange(args, 1, args.length));
-        pc.run(ProcessMode.BACKGROUND);
-      } else {
-        this.context.warning("Unknown command '" + args[0] + "'");
-      }
-    }
-  }
-
-  private boolean isTextInFile(Path path, String textToSearch) {
-
-    try (BufferedReader br = Files.newBufferedReader(path)) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        if (line.contains(textToSearch)) {
-          return true;
-        }
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(("Failed to open file for reading:" + e));
-    }
-    return false;
-  }
-
-  private void addTextToFile(Path path, String textToAdd) {
-
-    try {
-      Files.write(path, textToAdd.getBytes(), StandardOpenOption.APPEND);
-    } catch (IOException e) {
-      throw new RuntimeException("Unable to write text to file:" + e);
-    }
-  }
-
 }
