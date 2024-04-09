de64024eb96ea97117d832c88a89ed5c92bc01a2
CORP\albega <aldo.bega@capgemini.com>
2024-04-09 19:39:07 +0200

    #36: Added Mac testing

--- cli/src/test/java/com/devonfw/tools/ide/tool/tomcat/TomcatTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/tomcat/TomcatTest.java
@@ -1,20 +1,19 @@
 package com.devonfw.tools.ide.tool.tomcat;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 import com.devonfw.tools.ide.log.IdeLogLevel;
 import com.devonfw.tools.ide.os.SystemInfo;
 import com.devonfw.tools.ide.os.SystemInfoMock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class TomcatTest extends AbstractIdeContextTest {
 
   private static final String PROJECT_TOMCAT = "tomcat";
 
   @ParameterizedTest
-  @ValueSource(strings = { "windows", "linux" })
+  @ValueSource(strings = { "windows", "linux", "mac" })
   public void testTomcatInstall(String os) {
 
     // arrange
@@ -33,18 +32,15 @@ public class TomcatTest extends AbstractIdeContextTest {
 
   private void checkDependencyInstallation(IdeTestContext context) {
 
-    assertLogMessage(context, IdeLogLevel.INFO,
-        "Necessary version of the dependency java is already installed in repository");
+    assertLogMessage(context, IdeLogLevel.INFO, "Necessary version of the dependency java is already installed in repository");
 
   }
 
   private void checkInstallation(IdeTestContext context) {
 
     if (context.getSystemInfo().isWindows() || context.getSystemInfo().isLinux()) {
-      assertThat(context.getSoftwarePath().resolve("tomcat/bin/tomcat")).exists()
-          .hasContent("#!/bin/bash\n" + "echo \"tomcat $*\"");
-      assertThat(context.getSoftwarePath().resolve("tomcat/bin/tomcat.cmd")).exists()
-          .hasContent("@echo off\n" + "echo tomcat %*");
+      assertThat(context.getSoftwarePath().resolve("tomcat/bin/tomcat")).exists().hasContent("#!/bin/bash\n" + "echo \"tomcat $*\"");
+      assertThat(context.getSoftwarePath().resolve("tomcat/bin/tomcat.cmd")).exists().hasContent("@echo off\n" + "echo tomcat %*");
     } else if (context.getSystemInfo().isMac()) {
       assertThat(context.getSoftwarePath().resolve("tomcat/tomcat")).exists();
     }
