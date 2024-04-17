2d3945454bccc74776dbd8d3bacc3ff542173eed
ndemirca <nursura.demircan@capgemini.com>
2024-04-17 13:29:02 +0200

    fixed intellij Unittests

--- cli/src/test/java/com/devonfw/tools/ide/tool/intellij/IntellijTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/intellij/IntellijTest.java
@@ -1,16 +1,10 @@
 package com.devonfw.tools.ide.tool.intellij;
 
-import java.nio.file.Path;
-
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 import com.devonfw.tools.ide.log.IdeLogLevel;
-import com.devonfw.tools.ide.os.SystemInfo;
-import com.devonfw.tools.ide.os.SystemInfoMock;
 import com.devonfw.tools.ide.tool.jmc.Jmc;
+import org.junit.jupiter.api.Test;
 
 /**
  * Integration test of {@link Jmc}.
@@ -19,14 +13,11 @@ public class IntellijTest extends AbstractIdeContextTest {
 
   private static final String PROJECT_INTELLIJ = "intellij";
 
-  @ParameterizedTest
-  @ValueSource(strings = { "windows", "mac", "linux" })
-  public void testIntellijInstall(String os) {
+  @Test
+  public void testIntellijInstall() {
 
     // arrange
     IdeTestContext context = newContext(PROJECT_INTELLIJ);
-    SystemInfo systemInfo = SystemInfoMock.of(os);
-    context.setSystemInfo(systemInfo);
     Intellij commandlet = new Intellij(context);
 
     // act
@@ -39,7 +30,7 @@ public class IntellijTest extends AbstractIdeContextTest {
   private void checkInstallation(IdeTestContext context) {
 
     if (context.getSystemInfo().isLinux()) {
-      Path binPath = context.getSoftwarePath().resolve("bin");
+      assertThat(context.getSoftwarePath().resolve("intellij/bin/idea")).exists().isSymbolicLink();
     }
     assertThat(context.getSoftwarePath().resolve("intellij/.ide.software.version")).exists().hasContent("2023.3.3");
     assertLogMessage(context, IdeLogLevel.SUCCESS, "Successfully installed java in version 17.0.10_7");
