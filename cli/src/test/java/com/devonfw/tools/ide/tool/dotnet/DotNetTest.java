4eb927e90bb1a9f3ea69644ccda0b52a899715fe
ndemirca <nursura.demircan@capgemini.com>
2024-04-29 18:32:24 +0200

    fixed run for windows

--- cli/src/test/java/com/devonfw/tools/ide/tool/dotnet/DotNetTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/dotnet/DotNetTest.java
@@ -4,7 +4,9 @@ import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 import com.devonfw.tools.ide.log.IdeLogLevel;
 import com.devonfw.tools.ide.os.SystemInfo;
+import com.devonfw.tools.ide.os.SystemInfoImpl;
 import com.devonfw.tools.ide.os.SystemInfoMock;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -13,8 +15,6 @@ import java.nio.file.Path;
 public class DotNetTest extends AbstractIdeContextTest {
 
   private static final Path PROJECTS_TARGET_PATH = Path.of("target/test-projects");
-
-  private static final Path MOCK_RESULT_PATH = PROJECTS_TARGET_PATH.resolve("dotnet/project");
 
   private static final String PROJECT_DOTNET = "dotnet";
 
@@ -51,18 +51,25 @@ public class DotNetTest extends AbstractIdeContextTest {
     assertLogMessage(context, IdeLogLevel.SUCCESS, "Successfully installed dotnet in version 6.0.419", false);
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = { "windows", "mac", "linux" })
-  public void dotnetShouldRunExecutableSuccessful(String os) {
+  @Test
+  public void dotnetShouldRunExecutableForWindowsSuccessful() {
 
     String expectedOutputWindows = "Dummy dotnet 6.0.419 on windows ";
+    if (SystemInfoImpl.INSTANCE.isWindows()) {
+      runExecutable("windows");
+    }
+    checkExpectedOutput(expectedOutputWindows);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = { "mac", "linux" })
+  public void dotnetShouldRunExecutableSuccessful(String os) {
+
     String expectedOutputLinux = "Dummy dotnet 6.0.419 on linux ";
     String expectedOutputMacOs = "Dummy dotnet 6.0.419 on mac ";
     runExecutable(os);
 
-    if (context.getSystemInfo().isWindows()) {
-      checkExpectedOutput(expectedOutputWindows);
-    } else if (context.getSystemInfo().isLinux()) {
+    if (context.getSystemInfo().isLinux()) {
       checkExpectedOutput(expectedOutputLinux);
     } else if (context.getSystemInfo().isMac()) {
       checkExpectedOutput(expectedOutputMacOs);
@@ -78,7 +85,6 @@ public class DotNetTest extends AbstractIdeContextTest {
 
     SystemInfo systemInfo = SystemInfoMock.of(operatingSystem);
     context.setSystemInfo(systemInfo);
-    assignDummyUserHome(context, "dummyUserHome");
 
     commandlet.run();
   }
