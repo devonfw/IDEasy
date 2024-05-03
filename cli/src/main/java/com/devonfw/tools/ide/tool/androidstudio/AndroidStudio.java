170c8347f3059c84e36699098268caa2da363796
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2024-05-03 12:30:35 +0200

    #12: re-added file permission setter

--- cli/src/main/java/com/devonfw/tools/ide/tool/androidstudio/AndroidStudio.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/androidstudio/AndroidStudio.java
@@ -3,12 +3,17 @@ package com.devonfw.tools.ide.tool.androidstudio;
 import com.devonfw.tools.ide.cli.CliArgument;
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.io.FileAccessImpl;
 import com.devonfw.tools.ide.process.ProcessMode;
 import com.devonfw.tools.ide.tool.ide.IdeToolCommandlet;
 import com.devonfw.tools.ide.tool.ide.PluginDescriptor;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
 
 /**
@@ -36,23 +41,19 @@ public class AndroidStudio extends IdeToolCommandlet {
   protected String getBinaryName() {
 
     if (this.context.getSystemInfo().isWindows()) {
-      return STUDIO64_EXE;
+      return getToolBinPath().resolve(STUDIO64_EXE).toString();
     } else if (this.context.getSystemInfo().isLinux()) {
-      return STUDIO_BASH;
+      return getToolBinPath().resolve(STUDIO_BASH).toString();
     } else {
-      return "open";
+      return getToolPath().resolve("Android Studio Preview.app").resolve("Contents").resolve("MacOS").resolve(STUDIO).toString();
     }
   }
 
   @Override
   public void runTool(ProcessMode processMode, VersionIdentifier toolVersion, String... args) {
 
-    if (this.context.getSystemInfo().isMac()) {
-      Path studioPath = getToolPath().resolve("Contents").resolve("MacOS").resolve(STUDIO);
-      args = CliArgument.prepend(args, "-na", studioPath.toString(), "--args", this.context.getWorkspacePath().toString());
-    } else {
-      args = CliArgument.prepend(args, this.context.getWorkspacePath().toString());
-    }
+    args = CliArgument.prepend(args, this.context.getWorkspacePath().toString());
+
     super.runTool(processMode, toolVersion, args);
 
   }
@@ -61,6 +62,28 @@ public class AndroidStudio extends IdeToolCommandlet {
   public boolean install(boolean silent) {
 
     return super.install(silent);
+  }
+
+  @Override
+  protected void postInstall() {
+
+    super.postInstall();
+    if (this.context.getSystemInfo().isMac()) {
+      setMacOsFilePermissions(getToolPath().resolve("Android Studio Preview.app").resolve("Contents").resolve("MacOS").resolve(STUDIO));
+    }
+  }
+
+  private static void setMacOsFilePermissions(Path binaryFile) {
+
+    if (Files.exists(binaryFile)) {
+      String permissionStr = FileAccessImpl.generatePermissionString(493);
+      Set<PosixFilePermission> permissions = PosixFilePermissions.fromString(permissionStr);
+      try {
+        Files.setPosixFilePermissions(binaryFile, permissions);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 
   @Override
