2dc761eb7c490fe6ed75b847c142859d213e8780
Jörg Hohwiller <hohwille@users.noreply.github.com>
2024-05-28 21:40:47 +0200

    #329: simplify version determination (#368)

--- cli/src/main/java/com/devonfw/tools/ide/version/IdeVersion.java
+++ cli/src/main/java/com/devonfw/tools/ide/version/IdeVersion.java
@@ -29,14 +29,7 @@ public final class IdeVersion {
   private IdeVersion() {
 
     super();
-    Manifest manifest = loadManifest(IdeVersion.class);
-    String v = null;
-    if (manifest != null) {
-      v = getValue(manifest, Attributes.Name.IMPLEMENTATION_VERSION);
-      if (v == null) {
-        v = getValue(manifest, Attributes.Name.SPECIFICATION_VERSION);
-      }
-    }
+    String v = getClass().getPackage().getImplementationVersion();
     if (v == null) {
       v = "SNAPSHOT";
     }
@@ -46,52 +39,6 @@ public final class IdeVersion {
   private String getValue(Manifest manifest, Name name) {
 
     return manifest.getMainAttributes().getValue(name);
-  }
-
-  /**
-   * This method tries to load the {@link Manifest} for the given {@link Class}. E.g. if the given {@link Class} comes
-   * from a {@link JarFile} the {@link JarFile#getManifest() Manifest} of that {@link JarFile} is returned.
-   *
-   * @param type is the {@link Class} for which the according {@link Manifest} is requested.
-   * @return the according {@link Manifest} or {@code null} if NOT available.
-   */
-  private static Manifest loadManifest(Class<?> type) {
-
-    try {
-      Manifest manifest = null;
-      String classFile = type.getName().replace('.', '/') + ".class";
-      URL classUrl = type.getClassLoader().getResource(classFile);
-      if (classUrl != null) {
-        String protocol = classUrl.getProtocol().toLowerCase(Locale.US);
-        if ("jar".equals(protocol)) {
-          JarURLConnection connection = (JarURLConnection) classUrl.openConnection();
-          JarFile jarFile = connection.getJarFile();
-          // create a copy to avoid modification of the original manifest...
-          manifest = jarFile.getManifest();
-        } else if ("file".equals(protocol)) {
-          try {
-            Path classPath = Path.of(classUrl.toURI());
-            Path manifestPath = classPath;
-            int slashIndex = 0;
-            while (slashIndex >= 0) {
-              slashIndex = classFile.indexOf('/', slashIndex + 1);
-              manifestPath = manifestPath.getParent();
-            }
-            manifestPath = manifestPath.resolve(JarFile.MANIFEST_NAME);
-            if (Files.exists(manifestPath)) {
-              try (InputStream inputStream = Files.newInputStream(manifestPath)) {
-                manifest = new Manifest(inputStream);
-              }
-            }
-          } catch (URISyntaxException e) {
-            // ignore
-          }
-        }
-      }
-      return manifest;
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to read manifest.", e);
-    }
   }
 
   /**
