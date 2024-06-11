d3a1932deb900b4bac20ebbf2afddc1561d803f7
Sonja Skiba <sonja.skiba@capgemini.com>
2024-06-11 12:03:15 +0200

    Add urlupdater for pgadmin mac

--- cli/src/main/java/com/devonfw/tools/ide/tool/pgadmin/PgAdminUrlUpdater.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/pgadmin/PgAdminUrlUpdater.java
@@ -3,6 +3,7 @@ package com.devonfw.tools.ide.tool.pgadmin;
 import com.devonfw.tools.ide.os.OperatingSystem;
 import com.devonfw.tools.ide.url.model.folder.UrlVersion;
 import com.devonfw.tools.ide.url.updater.WebsiteUrlUpdater;
+import com.devonfw.tools.ide.version.VersionIdentifier;
 
 import java.util.regex.Pattern;
 
@@ -20,13 +21,13 @@ public class PgAdminUrlUpdater extends WebsiteUrlUpdater {
   @Override
   protected String getVersionUrl() {
 
-    return "https://www.pgadmin.org/docs/pgadmin4/latest/release_notes.html";
+    return "https://www.postgresql.org/ftp/pgadmin/pgadmin4/";
   }
 
   @Override
   protected Pattern getVersionPattern() {
 
-    return Pattern.compile("Version (\\d{1,2}+\\.\\d+)");
+    return Pattern.compile("v(\\d{1,2}+\\.\\d+)");
   }
 
   @Override
@@ -38,9 +39,17 @@ public class PgAdminUrlUpdater extends WebsiteUrlUpdater {
   @Override
   protected void addVersion(UrlVersion urlVersion) {
 
-    doAddVersion(urlVersion,
-        "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v${version}/windows/pgadmin4-${version}-x64.exe",
-        OperatingSystem.WINDOWS);
+    VersionIdentifier vid = urlVersion.getVersionIdentifier();
+
+    String baseUrl = "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/";
+    doAddVersion(urlVersion, baseUrl + "v${version}/windows/pgadmin4-${version}-x64.exe", OperatingSystem.WINDOWS);
+
+    if (vid.compareVersion(VersionIdentifier.of("7.6")).isGreater()) {
+      doAddVersion(urlVersion, baseUrl + "v${version}/macos/pgadmin4-${version}-arm64.dmg", MAC, ARM64);
+      doAddVersion(urlVersion, baseUrl + "v${version}/macos/pgadmin4-${version}-x86_64.dmg", MAC, X64);
+    } else {
+      doAddVersion(urlVersion, baseUrl + "v${version}/macos/pgadmin4-${version}.dmg", MAC);
+    }
   }
 
 }
