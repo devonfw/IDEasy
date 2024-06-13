86dabe20fd4e3bcae60bcbe959ceb6301220e1a7
slskiba <161473135+slskiba@users.noreply.github.com>
2024-06-13 11:14:48 +0200

    #80: GUI prototype (#349)

---
+++ gui/src/test/java/com/devonfw/ide/gui/AppBaseTest.java
@@ -0,0 +1,54 @@
+package com.devonfw.ide.gui;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+
+import java.io.IOException;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+/**
+ * Base Test
+ */
+public class AppBaseTest extends ApplicationTest {
+
+  @Override
+  public void start(Stage stage) throws IOException {
+
+    new App().start(stage);
+  }
+
+  /**
+   * Set up headless testing
+   *
+   * @throws IOException
+   */
+  @BeforeAll
+  public static void setupHeadlessMode() {
+
+    //Enable headless testing. Should be moved as a system property "-Dheadless=true" into workflows to only affect CI
+    System.setProperty("headless", "true");
+
+    if (Boolean.getBoolean("headless")) {
+      System.setProperty("testfx.robot", "glass");
+      System.setProperty("glass.platform", "Monocle");
+      System.setProperty("testfx.headless", "true");
+      System.setProperty("prism.order", "sw");
+      System.setProperty("java.awt.headless", "true");
+    }
+
+  }
+
+  /**
+   * Test if welcome message is shown when GUI is started
+   */
+  @Test
+  public void ensureHelloMessageIsShownOnStartUp() {
+
+    verifyThat("#hellomessage", NodeMatchers.isNotNull());
+  }
+
+}
