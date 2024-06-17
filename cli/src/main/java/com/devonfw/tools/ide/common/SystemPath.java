19e770ae196467aa4abebad199c781644108f2d7
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-06-17 18:32:48 +0200

    review rework: avoid star import

--- cli/src/main/java/com/devonfw/tools/ide/common/SystemPath.java
+++ cli/src/main/java/com/devonfw/tools/ide/common/SystemPath.java
@@ -9,7 +9,11 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
