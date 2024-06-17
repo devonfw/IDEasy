1b6880975bb977f46a82b74752c7ebca02741c82
Jörg Hohwiller <hohwille@users.noreply.github.com>
2024-06-17 19:25:05 +0200

    Update FileAccessImpl.java: constructive review

--- cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
@@ -57,7 +57,7 @@ public class FileAccessImpl implements FileAccess {
   private static final String WINDOWS_FILE_LOCK_DOCUMENTATION_PAGE = "https://github.com/devonfw/IDEasy/blob/main/documentation/windows-file-lock.adoc";
 
   private static final String WINDOWS_FILE_LOCK_WARNING =
-      "On Windows, file operations could fail due to file locks. Please ensure the files in the moved directory are not in use. For further details, see: "
+      "On Windows, file operations could fail due to file locks. Please ensure the files in the moved directory are not in use. For further details, see: \n"
           + WINDOWS_FILE_LOCK_DOCUMENTATION_PAGE;
 
   private final IdeContext context;
@@ -282,7 +282,11 @@ public class FileAccessImpl implements FileAccess {
       Files.move(source, targetDir);
     } catch (IOException e) {
       String fileType = Files.isSymbolicLink(source) ? "symlink" : isJunction(source) ? "junction" : Files.isDirectory(source) ? "directory" : "file";
-      throw new IllegalStateException("Failed to move " + fileType + ": " + source + " to " + targetDir + ". " + WINDOWS_FILE_LOCK_WARNING, e);
+      String message = "Failed to move " + fileType + ": " + source + " to " + targetDir + ".";
+      if (this.context.getSystemInfo().isWindows()) {
+        message = message + "\n" + WINDOWS_FILE_LOCK_WARNING
+      }
+      throw new IllegalStateException(message, e);
     }
   }
 
