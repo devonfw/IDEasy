a4c536a19277cdf730944bb7ce03fc50379b3127
Sonja Skiba <sonja.skiba@capgemini.com>
2024-06-24 11:27:25 +0200

    Implement uninstall for pgadmin

--- cli/src/main/java/com/devonfw/tools/ide/tool/pgadmin/PgAdmin.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/pgadmin/PgAdmin.java
@@ -49,12 +49,32 @@ public class PgAdmin extends GlobalToolCommandlet {
 
     pmCommands.add(new PackageManagerCommand(PackageManager.APT, Arrays.asList(
         "curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | "
-            + "sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg",
+            + "sudo gpg --yes --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg",
         "sudo sh -c 'echo \"deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] "
             + "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main\" "
             + "> /etc/apt/sources.list.d/pgadmin4.list && apt update'", String.format(
             "sudo apt install -y --allow-downgrades pgadmin4=%1$s pgadmin4-server=%1$s pgadmin4-desktop=%1$s pgadmin4-web=%1$s",
             resolvedVersion))));
+
+    return pmCommands;
+  }
+
+  @Override
+  public void uninstall() {
+
+    if (this.context.getSystemInfo().isLinux()) {
+      runWithPackageManager(false, getPackageManagerCommandsUninstall());
+    } else {
+      super.uninstall();
+    }
+  }
+
+  private List<PackageManagerCommand> getPackageManagerCommandsUninstall() {
+
+    List<PackageManagerCommand> pmCommands = new ArrayList<>();
+
+    pmCommands.add(new PackageManagerCommand(PackageManager.APT,
+        Arrays.asList("sudo apt -y autoremove pgadmin4 pgadmin4-server pgadmin4-desktop pgadmin4-web")));
 
     return pmCommands;
   }
