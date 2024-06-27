06fc7447424f153c06e713bb510a6c4247e2cd8b
Sonja Skiba <sonja.skiba@capgemini.com>
2024-06-27 09:02:22 +0200

    Check docker requirement before installing lazydocker

--- cli/src/main/java/com/devonfw/tools/ide/tool/lazydocker/LazyDocker.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/lazydocker/LazyDocker.java
@@ -2,8 +2,12 @@ package com.devonfw.tools.ide.tool.lazydocker;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessErrorHandling;
+import com.devonfw.tools.ide.process.ProcessMode;
+import com.devonfw.tools.ide.process.ProcessResult;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
-import com.devonfw.tools.ide.tool.docker.Docker;
+import com.devonfw.tools.ide.version.VersionIdentifier;
 
 import java.util.Set;
 
@@ -22,7 +26,28 @@ public class LazyDocker extends LocalToolCommandlet {
   @Override
   public boolean install(boolean silent) {
 
-    getCommandlet(Docker.class).install();
+    String bashPath = this.context.findBash();
+    String command = "docker version --format '{{.Client.APIVersion}}'";
+    ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.NONE).executable(bashPath)
+        .addArgs("-c", command);
+    ProcessResult result = pc.run(ProcessMode.DEFAULT_CAPTURE);
+
+    if (result.getOut().isEmpty()) {
+      this.context.info("Docker is not installed, but required for lazydocker");
+      this.context.info("To install docker, call the following command");
+      this.context.info("ide install docker");
+      return false;
+    }
+
+    VersionIdentifier dockerAPIversion = VersionIdentifier.of(result.getOut().get(0).toString());
+    this.context.debug("Found docker API version:" + dockerAPIversion.toString());
+
+    if (dockerAPIversion.compareVersion(VersionIdentifier.of("1.25")).isLess()) {
+      this.context.info("The installed version of docker does not meet the requirements of lazydocker");
+      this.context.info("Please upgrade your installation of docker");
+      return false;
+    }
+
     return super.doInstall(silent);
   }
 }
