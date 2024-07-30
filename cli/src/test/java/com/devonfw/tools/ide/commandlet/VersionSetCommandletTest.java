d2367e5b1e775a9ab10fbb5e6d6f1b26c2f20ec8
slskiba <161473135+slskiba@users.noreply.github.com>
2024-07-30 14:59:20 +0200

    #429: writing properties to conf (#454)

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionSetCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/VersionSetCommandletTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.environment.EnvironmentVariablesFiles;
 
 /**
  * Integration test of {@link VersionSetCommandlet}.
@@ -49,5 +50,40 @@ public class VersionSetCommandletTest extends AbstractIdeContextTest {
         TEST_ARGS9=settings9
         TEST_ARGSb=${TEST_ARGS10} settingsb ${TEST_ARGSa} ${TEST_ARGSb}
         TEST_ARGSc=${TEST_ARGSc} settingsc""");
+  }
+
+  /**
+   * Test of {@link VersionSetCommandlet} run.
+   *
+   * @throws IOException on error.
+   */
+  @Test
+  public void testVersionSetCommandletConfRun() throws IOException {
+
+    // arrange
+    IdeContext context = newContext(PROJECT_BASIC);
+    VersionSetCommandlet versionSet = context.getCommandletManager().getCommandlet(VersionSetCommandlet.class);
+    versionSet.tool.setValueAsString("mvn", context);
+    versionSet.version.setValueAsString("3.2.1", context);
+    versionSet.cfg.setValue(EnvironmentVariablesFiles.CONF);
+    // act
+    versionSet.run();
+    // assert
+    Path confIdeProperties = context.getConfPath().resolve("ide.properties");
+    assertThat(confIdeProperties).hasContent("""
+        #********************************************************************************
+        # This file contains project specific environment variables defined by the user
+        #********************************************************************************
+        M2_REPO=~/.m2/repository
+        MVN_VERSION=3.2.1
+        SOME=some-${UNDEFINED}
+        TEST_ARGS1=${TEST_ARGS1} conf1
+        TEST_ARGS2=${TEST_ARGS2} conf2
+        TEST_ARGS5=${TEST_ARGS5} conf5
+        TEST_ARGS6=${TEST_ARGS6} conf6
+        TEST_ARGS7=${TEST_ARGS7} conf7
+        TEST_ARGS8=${TEST_ARGS8} conf8
+        TEST_ARGSa=${TEST_ARGS1} ${TEST_ARGS3} confa
+        TEST_ARGSc=${TEST_ARGSc} confc""");
   }
 }
