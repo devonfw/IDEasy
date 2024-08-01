d43691c5023b3f0092cdfadde71f1e26280c7433
Jörg Hohwiller <hohwille@users.noreply.github.com>
2024-08-01 09:13:34 +0200

    Update HelpCommandlet.java: revert changes

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/HelpCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/HelpCommandlet.java
@@ -20,12 +20,12 @@ import com.devonfw.tools.ide.version.IdeVersion;
 public final class HelpCommandlet extends Commandlet {
 
   static final String LOGO = """
-          __       ___ ___  ___
-          ╲ ╲     |_ _|   ╲| __|__ _ ____ _
-           > >     | || |) | _|/ _` (_-< || |
-          /_/ ___ |___|___/|___╲__,_/__/╲_, |
-             |___|                       |__/
-          """.replace('╲', '\\');
+      __       ___ ___  ___
+      ╲ ╲     |_ _|   ╲| __|__ _ ____ _
+       > >     | || |) | _|/ _` (_-< || |
+      /_/ ___ |___|___/|___╲__,_/__/╲_, |
+         |___|                       |__/
+      """.replace('╲', '\\');
 
   /** The optional commandlet to get help about. */
   public final CommandletProperty commandlet;
