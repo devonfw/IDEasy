ed15b696c3eebaaaf30b213831ee75504d276baf
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-08-09 15:35:04 +0200

    red/green pattern: add failing test to proof bug

---
+++ cli/src/test/java/com/devonfw/tools/ide/cli/CliAdvancedParsingTest.java
@@ -0,0 +1,31 @@
+package com.devonfw.tools.ide.cli;
+
+import org.junit.jupiter.api.Test;
+
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeTestContext;
+import com.devonfw.tools.ide.tool.mvn.Mvn;
+
+/**
+ * Integration test of {@link Mvn}.
+ */
+public class CliAdvancedParsingTest extends AbstractIdeContextTest {
+
+  private static final String PROJECT_MVN = "mvn";
+
+  /**
+   * Test that implicit end-options is triggered for multi-valued arguments to prevent splitting odd-formatted short-options like "-version".
+   */
+  @Test
+  public void testMvnInstall() {
+
+    // arrange
+    IdeTestContext context = newContext(PROJECT_MVN);
+    CliArguments args = new CliArguments("java", "-version");
+    args.next();
+    // act
+    context.run(args);
+    // assert
+    assertThat(context).logAtInfo().hasNoMessage("java -v -e -r -s -i -o -n").hasMessage("java -version");
+  }
+}
