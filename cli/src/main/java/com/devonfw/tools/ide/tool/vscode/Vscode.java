124e6b88101785c902de94110b7324eb5356ce18
CORP\albega <aldo.bega@capgemini.com>
2024-08-12 17:10:47 +0200

    #37: Added Plugin Installation

--- cli/src/main/java/com/devonfw/tools/ide/tool/vscode/Vscode.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/vscode/Vscode.java
@@ -1,16 +1,20 @@
 package com.devonfw.tools.ide.tool.vscode;
 
+import java.nio.file.Path;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.tool.LocalToolCommandlet;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessErrorHandling;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
+import com.devonfw.tools.ide.tool.ide.IdeToolCommandlet;
+import com.devonfw.tools.ide.tool.ide.PluginDescriptor;
 
 /**
  * {@link ToolCommandlet} for <a href="https://code.visualstudio.com/">vscode</a>.
  */
-public class Vscode extends LocalToolCommandlet {
+public class Vscode extends IdeToolCommandlet {
 
   /**
    * The constructor.
@@ -27,5 +31,15 @@ public class Vscode extends LocalToolCommandlet {
 
     return "code";
   }
+
+  @Override
+  public void installPlugin(PluginDescriptor plugin) {
+    Path binaryPath = Path.of(getBinaryName());
+    ProcessContext pc = this.context.newProcess().errorHandling(ProcessErrorHandling.WARNING).executable(binaryPath);
+    pc.addArg("--install-extension");
+    pc.addArg(plugin.getId());
+    pc.run();
+  }
+
 
 }
