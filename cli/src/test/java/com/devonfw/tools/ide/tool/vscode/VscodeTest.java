0e14ff6e769d96a0625fbcca79ed0def377b7d69
CORP\albega <aldo.bega@capgemini.com>
2024-08-14 20:49:14 +0200

    #37: Added tests for Vscode

---
+++ cli/src/test/java/com/devonfw/tools/ide/tool/vscode/VscodeTest.java
@@ -0,0 +1,38 @@
+package com.devonfw.tools.ide.tool.vscode;
+
+import org.junit.jupiter.api.Test;
+
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeTestContext;
+
+public class VscodeTest extends AbstractIdeContextTest {
+
+  private static final String PROJECT_VSCODE = "vscode";
+
+
+  @Test
+  public void testVscode() {
+
+    // arrange
+    IdeTestContext context = newContext(PROJECT_VSCODE);
+    Vscode vscodeCommandlet = new Vscode(context);
+
+    // install
+    vscodeCommandlet.install();
+
+    // run
+    vscodeCommandlet.run();
+
+    // assert
+    checkInstallation(context);
+  }
+
+  private void checkInstallation(IdeTestContext context) {
+
+    assertThat(context.getSoftwarePath().resolve("vscode/bin/code.bat")).exists().hasContent("@echo test for windows");
+    assertThat(context.getSoftwarePath().resolve("vscode/bin/code.sh")).exists().hasContent("#!/bin/bash\n" + "echo \"Test for linux and Mac\"");
+
+    assertThat(context.getSoftwarePath().resolve("vscode/.ide.software.version")).exists().hasContent("1.92.1");
+    assertThat(context).logAtSuccess().hasMessage("Successfully installed vscode in version 1.92.1");
+  }
+}
