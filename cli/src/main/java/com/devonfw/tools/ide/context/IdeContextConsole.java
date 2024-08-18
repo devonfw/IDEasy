95cc47bec6d67147dfe3c7ea5a50ac1ee588da84
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-08-18 18:54:35 +0200

    #511: consolidate context, imrpove logger

--- cli/src/main/java/com/devonfw/tools/ide/context/IdeContextConsole.java
+++ cli/src/main/java/com/devonfw/tools/ide/context/IdeContextConsole.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 import com.devonfw.tools.ide.io.IdeProgressBar;
 import com.devonfw.tools.ide.io.IdeProgressBarConsole;
 import com.devonfw.tools.ide.log.IdeLogLevel;
+import com.devonfw.tools.ide.log.IdeLoggerImpl;
 import com.devonfw.tools.ide.log.IdeSubLoggerOut;
 
 import me.tongfei.progressbar.ProgressBarBuilder;
@@ -26,7 +27,7 @@ public class IdeContextConsole extends AbstractIdeContext {
    */
   public IdeContextConsole(IdeLogLevel minLogLevel, Appendable out, boolean colored) {
 
-    super(minLogLevel, level -> new IdeSubLoggerOut(level, out, colored, minLogLevel), null, null);
+    super(new IdeLoggerImpl(minLogLevel, level -> new IdeSubLoggerOut(level, out, colored, minLogLevel)), null, null);
     if (System.console() == null) {
       debug("System console not available - using System.in as fallback");
       this.scanner = new Scanner(System.in);
