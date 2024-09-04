a59460cb21a454ac99a15c676db0396a6a64af38
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-09-04 11:43:51 +0200

    Revert updater changes that were for testing purposes

--- cli/src/main/java/com/devonfw/tools/ide/url/updater/UpdateManager.java
+++ cli/src/main/java/com/devonfw/tools/ide/url/updater/UpdateManager.java
@@ -8,7 +8,43 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.devonfw.tools.ide.tool.androidstudio.AndroidStudioUrlUpdater;
+import com.devonfw.tools.ide.tool.aws.AwsUrlUpdater;
+import com.devonfw.tools.ide.tool.az.AzureUrlUpdater;
+import com.devonfw.tools.ide.tool.docker.DockerDesktopUrlUpdater;
+import com.devonfw.tools.ide.tool.docker.DockerRancherDesktopUrlUpdater;
+import com.devonfw.tools.ide.tool.dotnet.DotNetUrlUpdater;
+import com.devonfw.tools.ide.tool.eclipse.EclipseCppUrlUpdater;
+import com.devonfw.tools.ide.tool.eclipse.EclipseJavaUrlUpdater;
+import com.devonfw.tools.ide.tool.eclipse.EclipseJeeUrlUpdater;
+import com.devonfw.tools.ide.tool.gcloud.GCloudUrlUpdater;
+import com.devonfw.tools.ide.tool.gcviewer.GcViewerUrlUpdater;
+import com.devonfw.tools.ide.tool.gh.GhUrlUpdater;
+import com.devonfw.tools.ide.tool.graalvm.GraalVmCommunityUpdater;
+import com.devonfw.tools.ide.tool.graalvm.GraalVmOracleUrlUpdater;
+import com.devonfw.tools.ide.tool.gradle.GradleUrlUpdater;
+import com.devonfw.tools.ide.tool.helm.HelmUrlUpdater;
+import com.devonfw.tools.ide.tool.intellij.IntellijUrlUpdater;
+import com.devonfw.tools.ide.tool.jasypt.JasyptUrlUpdater;
+import com.devonfw.tools.ide.tool.java.JavaUrlUpdater;
+import com.devonfw.tools.ide.tool.jenkins.JenkinsUrlUpdater;
+import com.devonfw.tools.ide.tool.jmc.JmcUrlUpdater;
 import com.devonfw.tools.ide.tool.kotlinc.KotlincNativeUrlUpdater;
+import com.devonfw.tools.ide.tool.kotlinc.KotlincUrlUpdater;
+import com.devonfw.tools.ide.tool.lazydocker.LazyDockerUrlUpdater;
+import com.devonfw.tools.ide.tool.mvn.Mvn4UrlUpdater;
+import com.devonfw.tools.ide.tool.mvn.MvnUrlUpdater;
+import com.devonfw.tools.ide.tool.node.NodeUrlUpdater;
+import com.devonfw.tools.ide.tool.npm.NpmUrlUpdater;
+import com.devonfw.tools.ide.tool.oc.OcUrlUpdater;
+import com.devonfw.tools.ide.tool.pgadmin.PgAdminUrlUpdater;
+import com.devonfw.tools.ide.tool.pip.PipUrlUpdater;
+import com.devonfw.tools.ide.tool.python.PythonUrlUpdater;
+import com.devonfw.tools.ide.tool.quarkus.QuarkusUrlUpdater;
+import com.devonfw.tools.ide.tool.sonar.SonarUrlUpdater;
+import com.devonfw.tools.ide.tool.terraform.TerraformUrlUpdater;
+import com.devonfw.tools.ide.tool.tomcat.TomcatUrlUpdater;
+import com.devonfw.tools.ide.tool.vscode.VsCodeUrlUpdater;
 import com.devonfw.tools.ide.url.model.folder.UrlRepository;
 import com.devonfw.tools.ide.url.model.report.UrlFinalReport;
 
@@ -23,8 +59,16 @@ public class UpdateManager extends AbstractProcessorWithTimeout {
 
   private final UrlRepository urlRepository;
 
-  private final List<AbstractUrlUpdater> updaters = Arrays.asList(
-      new KotlincNativeUrlUpdater());
+  private final List<AbstractUrlUpdater> updaters = Arrays.asList(new AndroidStudioUrlUpdater(), new AwsUrlUpdater(),
+      new AzureUrlUpdater(), new DockerDesktopUrlUpdater(), new DotNetUrlUpdater(),
+      new EclipseCppUrlUpdater(), new EclipseJeeUrlUpdater(), new EclipseJavaUrlUpdater(), new GCloudUrlUpdater(),
+      new GcViewerUrlUpdater(), new GhUrlUpdater(), new GraalVmCommunityUpdater(), new GraalVmOracleUrlUpdater(),
+      new GradleUrlUpdater(), new HelmUrlUpdater(), new IntellijUrlUpdater(), new JasyptUrlUpdater(),
+      new JavaUrlUpdater(), new JenkinsUrlUpdater(), new JmcUrlUpdater(), new KotlincUrlUpdater(),
+      new KotlincNativeUrlUpdater(), new LazyDockerUrlUpdater(), new MvnUrlUpdater(), new Mvn4UrlUpdater(),
+      new NodeUrlUpdater(), new NpmUrlUpdater(), new OcUrlUpdater(), new PgAdminUrlUpdater(), new PipUrlUpdater(),
+      new PythonUrlUpdater(), new QuarkusUrlUpdater(), new DockerRancherDesktopUrlUpdater(), new SonarUrlUpdater(),
+      new TerraformUrlUpdater(), new TomcatUrlUpdater(), new VsCodeUrlUpdater());
 
   /**
    * The constructor.
