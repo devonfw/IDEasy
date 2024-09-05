1f31a460700adaee49fb65fd05e327c601ba6d7b
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-09-05 13:51:56 +0200

    Split test into smaller tests for better understaing

--- cli/src/test/java/com/devonfw/tools/ide/url/report/UrlUpdaterReportTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/url/report/UrlUpdaterReportTest.java
@@ -26,23 +26,34 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 @WireMockTest
 public class UrlUpdaterReportTest extends AbstractUrlUpdaterTest {
 
+  /**
+   * Function to test the equality of two {@link UrlUpdaterReport} instances
+   *
+   * @param report1 first report
+   * @param report2 second report
+   * @return true if equal otherwise false
+   */
   public boolean equalsReport(UrlUpdaterReport report1, UrlUpdaterReport report2) {
 
     return (report1.getAddVersionSuccess() == report2.getAddVersionSuccess()) && (report1.getAddVersionFailure() == report2.getAddVersionFailure())
         && (report1.getVerificationSuccess() == report2.getVerificationSuccess()) && (report1.getVerificationFailure() == report2.getVerificationFailure());
   }
 
+  /**
+   * Tests for {@link UrlUpdaterReport} if information of updaters is collected correctly
+   *
+   * @param tempDir Temporary directory
+   * @param wmRuntimeInfo wireMock server on a random port
+   * @throws IOException test fails
+   */
   @Test
   public void testReportWithoutErrorsAndEmptyRepo(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
 
-    // init test infrastructure
     UrlRepository urlRepository = UrlRepository.load(tempDir);
     UrlUpdaterMock updater = new UrlUpdaterMock(wmRuntimeInfo);
     UrlFinalReport urlFinalReport = new UrlFinalReport();
     updater.setUrlFinalReport(urlFinalReport);
-    Path versionsPath = tempDir.resolve("mocked").resolve("mocked").resolve("1.0");
 
-    // first case: url downloads of versions of all os's works
     // assign
     stubFor(any(urlMatching("/os.*")).willReturn(aResponse().withStatus(200).withBody("aBody")));
     UrlUpdaterTestReport expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 3, 0, 12, 0);
@@ -51,19 +62,52 @@ public class UrlUpdaterReportTest extends AbstractUrlUpdaterTest {
     // assert
     assertThat(equalsReport(urlFinalReport.getUrlUpdaterReports().get(0), expectedReport)).isEqualTo(true);
 
-    // second case: url downloads of existing versions and mac url doesn't work
+  }
+
+  /**
+   * Test with existing versions and not empty repo and some failing urls
+   */
+  @Test
+  public void testReportWithExistVersionsAndFailedDownloads(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+
+    UrlRepository urlRepository = UrlRepository.load(tempDir);
+    UrlUpdaterMock updater = new UrlUpdaterMock(wmRuntimeInfo);
+    UrlFinalReport urlFinalReport = new UrlFinalReport();
+    updater.setUrlFinalReport(urlFinalReport);
+    Path versionsPath = tempDir.resolve("mocked").resolve("mocked").resolve("1.0");
+
+    // pre configuration
+    stubFor(any(urlMatching("/os.*")).willReturn(aResponse().withStatus(200).withBody("aBody")));
+    updater.update(urlRepository);
     // assign
     stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
-    expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 0, 0, 6, 6);
+    UrlUpdaterTestReport expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 0, 0, 6, 6);
     // act
     updater.update(urlRepository);
     // assert
     assertThat(equalsReport(urlFinalReport.getUrlUpdaterReports().get(1), expectedReport)).isEqualTo(true);
+  }
 
-    // third case: one version for Windows removed
+  /**
+   * Test one os version url file is removed in already existing repository
+   */
+  @Test
+  public void testReportAfterVersionForOsRemoved(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+
+    UrlRepository urlRepository = UrlRepository.load(tempDir);
+    UrlUpdaterMock updater = new UrlUpdaterMock(wmRuntimeInfo);
+    UrlFinalReport urlFinalReport = new UrlFinalReport();
+    updater.setUrlFinalReport(urlFinalReport);
+    Path versionsPath = tempDir.resolve("mocked").resolve("mocked").resolve("1.0");
+
+    // pre configuration
+    stubFor(any(urlMatching("/os.*")).willReturn(aResponse().withStatus(200).withBody("aBody")));
+    updater.update(urlRepository);
+    stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
+    updater.update(urlRepository);
     // assign
     stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
-    expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 1, 0, 7, 8);
+    UrlUpdaterReport expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 1, 0, 7, 8);
     // act
     Files.deleteIfExists(versionsPath.resolve("windows_x64.urls"));
     Files.deleteIfExists(versionsPath.resolve("windows_x64.urls.sha256"));
@@ -71,12 +115,30 @@ public class UrlUpdaterReportTest extends AbstractUrlUpdaterTest {
     updater.update(urlRepositoryWithError);
     // assert
     assertThat(equalsReport(urlFinalReport.getUrlUpdaterReports().get(2), expectedReport)).isEqualTo(true);
+  }
 
-    // fourth case: one complete version removed and mac urls work and others don't work
+  /**
+   * Test after one version is completely removed in already existing repository and download urls response is reversed
+   */
+  @Test
+  public void testReportAfterVersionsForRemovedAndReversedUrlResponses(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+    UrlRepository urlRepository = UrlRepository.load(tempDir);
+    UrlUpdaterMock updater = new UrlUpdaterMock(wmRuntimeInfo);
+    UrlFinalReport urlFinalReport = new UrlFinalReport();
+    updater.setUrlFinalReport(urlFinalReport);
+    Path versionsPath = tempDir.resolve("mocked").resolve("mocked").resolve("1.0");
+
+    // pre configuration
+    stubFor(any(urlMatching("/os.*")).willReturn(aResponse().withStatus(200).withBody("aBody")));
+    updater.update(urlRepository);
+    stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
+    updater.update(urlRepository);
+    stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
+    updater.update(urlRepository);
     // assign
     stubFor(any(urlMatching("/os.*")).willReturn(aResponse().withStatus(400).withBody("aBody")));
     stubFor(any(urlMatching("/os/mac.*")).willReturn(aResponse().withStatus(200).withBody("aBody")));
-    expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 1, 0, 6, 6);
+    UrlUpdaterReport expectedReport = new UrlUpdaterTestReport("mocked", "mocked", 1, 0, 6, 6);
     // act
     Files.deleteIfExists(versionsPath.resolve("windows_x64.urls"));
     Files.deleteIfExists(versionsPath.resolve("windows_x64.urls.sha256"));
@@ -88,7 +150,7 @@ public class UrlUpdaterReportTest extends AbstractUrlUpdaterTest {
     Files.deleteIfExists(versionsPath.resolve("linux_x64.urls.sha256"));
     Files.deleteIfExists(versionsPath.resolve("status.json"));
     Files.deleteIfExists(versionsPath);
-    urlRepositoryWithError = UrlRepository.load(tempDir);
+    UrlRepository urlRepositoryWithError = UrlRepository.load(tempDir);
     updater.update(urlRepositoryWithError);
     // assert
     assertThat(equalsReport(urlFinalReport.getUrlUpdaterReports().get(3), expectedReport)).isEqualTo(true);
