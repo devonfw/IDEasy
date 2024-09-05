d82063e48595f838346e68055230b2bcabfe7e19
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-09-05 10:53:59 +0200

    Some refactor to output the report

--- cli/src/main/java/com/devonfw/tools/ide/url/model/report/UrlFinalReport.java
+++ cli/src/main/java/com/devonfw/tools/ide/url/model/report/UrlFinalReport.java
@@ -18,19 +18,4 @@ public class UrlFinalReport {
 
     this.urlUpdaterReports.add(urlUpdaterReport);
   }
-
-  /**
-   * @return «tool»/«edition»: versions added: 5 failed, 7 succeeded, 13 total, 38,4% error - versions verified: 0 failed, 0 succeeded, 0 total, 0% error
-   */
-  @Override
-  public String toString() {
-    for (UrlUpdaterReport report : this.urlUpdaterReports) {
-      return report.getTool() + "/" + report.getEdition() + " versions added: " + report.getAddVersionFailure() + " failed, "
-          + report.getAddVersionSuccess() + " succeeded, " + report.getTotalAdditions() + " total, " + String.format("%.2f", report.getErrorRateAdditions())
-          + "% error - versions verified: " + report.getVerificationFailure() + " failed, " + report.getVerificationSuccess() + " succeeded, "
-          + report.getTotalVerificitations() + " total, " + String.format("%.2f", report.getErrorRateVerificiations()) + "% error";
-    }
-    return "";
-  }
-
 }
