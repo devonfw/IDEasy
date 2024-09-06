b70c7708d30988c016c99c32da38a3d648b55384
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2024-09-06 09:27:50 +0200

    #130: progressbar unknown file size (#575)

--- cli/src/test/java/com/devonfw/tools/ide/io/IdeProgressBarTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/io/IdeProgressBarTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.io.TempDir;
 
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.context.IdeTestContext;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
@@ -20,6 +21,10 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
  */
 @WireMockTest
 public class IdeProgressBarTest extends AbstractIdeContextTest {
+
+  private static final int MAX_LENGTH = 10_000;
+
+  private static final String TEST_URL = "/os/windows_x64_url.tgz";
 
   /**
    * Tests if a download of a file with a valid content length was displaying an {@link IdeProgressBar} properly.
@@ -30,15 +35,71 @@ public class IdeProgressBarTest extends AbstractIdeContextTest {
   @Test
   public void testProgressBarDownloadWithValidContentLength(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) {
 
-    int maxLength = 10_000;
-
-    stubFor(any(urlMatching("/os/.*")).willReturn(aResponse().withStatus(200).withBody(new byte[maxLength])
-        .withHeader("Content-Length", String.valueOf(maxLength))));
+    stubFor(any(urlMatching("/os/.*")).willReturn(
+        aResponse().withStatus(200).withBody(new byte[MAX_LENGTH]).withHeader("Content-Length", String.valueOf(MAX_LENGTH))));
 
     IdeContext context = newContext(tempDir);
     FileAccess impl = context.getFileAccess();
-    impl.download(wmRuntimeInfo.getHttpBaseUrl() + "/os/windows_x64_url.tgz", tempDir.resolve("windows_x64_url.tgz"));
+    impl.download(wmRuntimeInfo.getHttpBaseUrl() + TEST_URL, tempDir.resolve("windows_x64_url.tgz"));
     assertThat(tempDir.resolve("windows_x64_url.tgz")).exists();
-    assertProgressBar(context, "Downloading", maxLength);
+    assertProgressBar(context, "Downloading", MAX_LENGTH);
+  }
+
+  /**
+   * Tests if {@link FileAccess#download(String, Path)} with missing content length is working properly.
+   *
+   * @param tempDir temporary directory to use.
+   * @param wmRuntimeInfo wireMock server on a random port
+   */
+  @Test
+  public void testProgressBarDownloadWithMissingContentLength(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) {
+
+    //arrange
+    String taskName = "Downloading";
+    stubFor(any(urlMatching("/os/.*")).willReturn(aResponse().withStatus(200).withBody(new byte[MAX_LENGTH])));
+    IdeTestContext context = newContext(tempDir);
+    FileAccess impl = context.getFileAccess();
+    //act
+    String testUrl = wmRuntimeInfo.getHttpBaseUrl() + TEST_URL;
+    impl.download(testUrl, tempDir.resolve("windows_x64_url.tgz"));
+
+    //assert
+    assertUnknownProgressBar(context, "Downloading", MAX_LENGTH);
+    checkLogMessageForMissingContentLength(context, testUrl);
+    assertThat(tempDir.resolve("windows_x64_url.tgz")).exists();
+    IdeProgressBarTestImpl progressBar = context.getProgressBarMap().get(taskName);
+
+    assertThat(progressBar.getMaxLength()).isEqualTo(-1);
+  }
+
+  private void checkLogMessageForMissingContentLength(IdeTestContext context, String source) {
+
+    assertThat(context).logAtWarning().hasMessage(
+        "Content-Length was not provided by download/copy source: " + source + ".");
+  }
+
+  /**
+   * Tests if {@link FileAccess#download(String, Path)} with missing file size is working properly.
+   *
+   * @param tempDir temporary directory to use.
+   */
+  @Test
+  public void testProgressBarCopyWithMissingFileSize(@TempDir Path tempDir) {
+
+    //arrange
+    String taskName = "Copying";
+    IdeTestContext context = newContext(tempDir);
+
+    FileAccessTestImpl impl = new FileAccessTestImpl(context);
+    String source = Path.of("src/test/resources/__files").resolve("testZip").toString();
+
+    //act
+    impl.download(source, tempDir.resolve("windows_x64_url.tgz"));
+
+    //assert
+    checkLogMessageForMissingContentLength(context, source);
+    assertThat(tempDir.resolve("windows_x64_url.tgz")).exists();
+    IdeProgressBarTestImpl progressBar = context.getProgressBarMap().get(taskName);
+    assertThat(progressBar.getMaxLength()).isEqualTo(-1);
   }
 }
