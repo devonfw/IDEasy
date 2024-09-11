1891b0d9b5817a808e7f0d1a3cdb53daae22ada7
Sonja Skiba <sonja.skiba@capgemini.com>
2024-09-11 11:32:11 +0200

    Fix download url for spaces in plugin name

--- cli/src/main/java/com/devonfw/tools/ide/tool/ide/IdeaBasedIdeToolCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/ide/IdeaBasedIdeToolCommandlet.java
@@ -68,7 +68,7 @@ public class IdeaBasedIdeToolCommandlet extends IdeToolCommandlet {
    */
   private String getDownloadUrl(PluginDescriptor plugin) {
     String downloadUrl = plugin.getUrl();
-    String pluginId = plugin.getId();
+    String pluginId = plugin.getId().replace(" ", "%20");
 
     String buildVersion = readBuildVersion();
 
