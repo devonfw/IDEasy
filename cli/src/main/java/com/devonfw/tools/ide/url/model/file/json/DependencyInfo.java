70523be7b0f13000e87b3161370c95f263f87bc5
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-09-19 10:20:05 +0200

    #634: preparation: change DependencyInfo to record

--- cli/src/main/java/com/devonfw/tools/ide/url/model/file/json/DependencyInfo.java
+++ cli/src/main/java/com/devonfw/tools/ide/url/model/file/json/DependencyInfo.java
@@ -3,34 +3,9 @@ package com.devonfw.tools.ide.url.model.file.json;
 import com.devonfw.tools.ide.version.VersionRange;
 
 /**
- * Model to represent the Object of the dependencies inside the Json file.
+ * Model to represent a dependency of a tool (inside a "dependencies.json" file).
  */
-public final class DependencyInfo {
-
-  private String tool;
-
-  private VersionRange versionRange;
-
-  /**
-   * @return the dependency name
-   */
-  public String getTool() {
-
-    return this.tool;
-  }
-
-  void setTool(String tool) {
-
-    this.tool = tool;
-  }
-
-  /**
-   * @return the VersionRange of the dependency
-   */
-  public VersionRange getVersionRange() {
-
-    return this.versionRange;
-  }
+public record DependencyInfo(String tool, VersionRange versionRange) {
 
   void setVersionRange(VersionRange versionRange) {
 
