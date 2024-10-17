c1e29fa4827bd3fc20a1ae5946c5572e650a2d1c
KianRolf <kian.loroff@capgemini.com>
2024-10-17 16:37:24 +0200

    egal

---
+++ cli/src/main/java/com/devonfw/tools/ide/tool/kubectl/KubeCtl.java
@@ -0,0 +1,28 @@
+package com.devonfw.tools.ide.tool.kubectl;
+
+import java.util.Set;
+
+import com.devonfw.tools.ide.common.Tag;
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.GlobalToolCommandlet;
+import com.devonfw.tools.ide.tool.docker.Docker;
+
+public class KubeCtl extends GlobalToolCommandlet {
+
+  /**
+   * The constructor.
+   *
+   * @param context the {@link IdeContext}.
+   */
+  public KubeCtl(IdeContext context) {
+
+    super(context, "kubectl", Set.of(Tag.DOCKER));
+  }
+
+  @Override
+  public boolean install() {
+    // TODO create kubectl/kubectl/dependencies.json file in ide-urls and delete this method
+    getCommandlet(Docker.class).install();
+    return super.install();
+  }
+}
