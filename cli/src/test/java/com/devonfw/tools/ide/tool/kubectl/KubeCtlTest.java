f8da022f884b49aed256d5e64040d6d34c73fbc4
KianRolf <kian.loroff@capgemini.com>
2024-11-06 11:01:25 +0100

    #81: Remove Test

--- cli/src/test/java/com/devonfw/tools/ide/tool/kubectl/KubeCtlTest.java
+++
@@ -1,9 +0,0 @@
-package com.devonfw.tools.ide.tool.kubectl;
-
-import com.devonfw.tools.ide.context.AbstractIdeContextTest;
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
-
-@WireMockTest
-public class KubeCtlTest extends AbstractIdeContextTest {
-
-}
