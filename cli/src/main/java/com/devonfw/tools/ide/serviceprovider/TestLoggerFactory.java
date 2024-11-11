6a139b3529d4b81a6512f6651b86867cd5debfbc
KianRolf <kian.loroff@capgemini.com>
2024-11-11 13:47:11 +0100

    extension

--- cli/src/main/java/com/devonfw/tools/ide/serviceprovider/TestLoggerFactory.java
+++ cli/src/main/java/com/devonfw/tools/ide/serviceprovider/TestLoggerFactory.java
@@ -1,2 +1,13 @@
-package com.devonfw.tools.ide.serviceprovider;public class TestLoggerFactory {
+package com.devonfw.tools.ide.serviceprovider;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+
+public class TestLoggerFactory implements ILoggerFactory {
+
+  @Override
+  public Logger getLogger(String name) {
+
+    return new TestLogger(name);
+  }
 }
--- cli/src/main/java/com/devonfw/tools/ide/serviceprovider/TestProvider.java
+++ cli/src/main/java/com/devonfw/tools/ide/serviceprovider/TestProvider.java
@@ -1,2 +1,43 @@
-package com.devonfw.tools.ide.serviceprovider;public class TestProvider {
+package com.devonfw.tools.ide.serviceprovider;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.IMarkerFactory;
+import org.slf4j.helpers.NOPMDCAdapter;
+import org.slf4j.spi.MDCAdapter;
+import org.slf4j.spi.SLF4JServiceProvider;
+
+public class TestProvider implements SLF4JServiceProvider {
+
+  private final String REQUESTED_API_VERSION = "2.0.12";
+  private TestLoggerFactory testLoggerFactory;
+  
+
+  @Override
+  public ILoggerFactory getLoggerFactory() {
+
+    return testLoggerFactory;
+  }
+
+  @Override
+  public IMarkerFactory getMarkerFactory() {
+
+    return null;
+  }
+
+  @Override
+  public MDCAdapter getMDCAdapter() {
+
+    return new NOPMDCAdapter();
+  }
+
+  @Override
+  public String getRequestedApiVersion() {
+
+    return REQUESTED_API_VERSION;
+  }
+
+  @Override
+  public void initialize() {
+    testLoggerFactory = new TestLoggerFactory();
+  }
 }
