7bb5674882fe4a8dac11e922c808bead73e4f7f9
KianRolf <kian.loroff@capgemini.com>
2024-11-19 16:18:02 +0100

    kian ist der best

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/UpdateSettingsCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/UpdateSettingsCommandlet.java
@@ -26,7 +26,7 @@ public class UpdateSettingsCommandlet extends Commandlet {
   @Override
   public void run() {
     Path source = context.getIdeHome();
-    if (context.getFileAccess().findFirst(source, path -> path.getFileName().toString().equals("devon.properties"), true)) {
+    if (null != context.getFileAccess().findFirst(source, path -> path.getFileName().toString().equals("devon.properties"), true)) {
       this.context.info("hi");
     } else {
       this.context.info("nein");
