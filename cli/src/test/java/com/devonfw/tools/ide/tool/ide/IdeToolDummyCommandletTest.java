e44c9f059b177485db0c87db07dd1553a40cd6a2
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2024-11-22 00:08:51 +0100

    #775: review rework (JavaDoc)

--- cli/src/test/java/com/devonfw/tools/ide/tool/ide/IdeToolDummyCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/ide/IdeToolDummyCommandletTest.java
@@ -22,8 +22,16 @@ import com.devonfw.tools.ide.step.Step;
 import com.devonfw.tools.ide.tool.plugin.ToolPluginDescriptor;
 import com.devonfw.tools.ide.version.GenericVersionRange;
 
+/**
+ * Test of {@link IdeToolCommandlet} using {@link IdeToolDummyCommandlet}.
+ */
 public class IdeToolDummyCommandletTest extends AbstractIdeContextTest {
 
+  /**
+   * Run the dummy commandlet and test that only active plugins are passed to installPlugin method.
+   *
+   * @param tempDir the {@link TempDir}.
+   */
   @Test
   public void testDummyCommandlet(@TempDir Path tempDir) {
 
@@ -43,11 +51,14 @@ public class IdeToolDummyCommandletTest extends AbstractIdeContextTest {
     assertThat(plugin.url()).isEqualTo("https://dummy.com/plugins/plugin1-url");
   }
 
+  /**
+   * Dummy commandlet extending {@link IdeToolCommandlet} for testing.
+   */
   public static class IdeToolDummyCommandlet extends IdeToolCommandlet {
 
     final List<ToolPluginDescriptor> installedPlugins;
 
-    public IdeToolDummyCommandlet(IdeContext context, String tool, Set<Tag> tags) {
+    IdeToolDummyCommandlet(IdeContext context, String tool, Set<Tag> tags) {
 
       super(context, tool, tags);
       this.installedPlugins = new ArrayList<>();
@@ -56,11 +67,13 @@ public class IdeToolDummyCommandletTest extends AbstractIdeContextTest {
     @Override
     protected void configureWorkspace() {
 
+      // disable workspace configuration since we have no IDE_HOME and therefore no settings
     }
 
     @Override
     public ProcessResult runTool(ProcessMode processMode, GenericVersionRange toolVersion, ProcessErrorHandling errorHandling, String... args) {
 
+      // skip installation but trigger postInstall to test mocked plugin installation
       postInstall(true);
       return new ProcessResultImpl(0, List.of(), List.of());
     }
