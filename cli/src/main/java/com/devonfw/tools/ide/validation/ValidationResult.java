1b9c55b23d192c6c348975ca4fe3768734ff3fd3
Jörg Hohwiller <hohwille@users.noreply.github.com>
2024-11-29 12:46:30 +0100

    #569: improve Validation (added JavaDoc, etc.) (#817)

--- cli/src/main/java/com/devonfw/tools/ide/validation/ValidationResult.java
+++ cli/src/main/java/com/devonfw/tools/ide/validation/ValidationResult.java
@@ -1,9 +1,18 @@
 package com.devonfw.tools.ide.validation;
 
+/**
+ * Interface for the result of a validation.
+ */
 public interface ValidationResult {
 
+  /**
+   * @return {@code true} if the validation was successful, {@code false} otherwise (validation constraint(s) violated).
+   */
   boolean isValid();
 
+  /**
+   * @return the error messsage(s) of the validation or {@code null} if {@link #isValid() valid}.
+   */
   String getErrorMessage();
 
 }
