23a692407fd55ccdadc4e6547b86972f460f210f
alfeilex <101652401+alfeilex@users.noreply.github.com>
2024-12-17 16:43:04 +0100

    fix using the right ERRORLEVEL inside IF statement to output error

--- cli/src/main/package/bin/ide.bat
+++ cli/src/main/package/bin/ide.bat
@@ -7,11 +7,14 @@ Set _RESET=[0m
 
 if not "%1%" == "" (
   ideasy %IDE_OPTIONS% %*
-  if not %ERRORLEVEL% == 0 (
-    echo %_fBRed%Error: IDEasy failed with exit code %ERRORLEVEL% %_RESET%
-    exit /b %ERRORLEVEL%
-  )
+  goto :output_error
 )
+
+:output_error
+if not %ERRORLEVEL% == 0 (
+	echo %_fBRed%Error: IDEasy failed with exit code %ERRORLEVEL% %_RESET%
+	exit /b %ERRORLEVEL%
+  )
 
 REM https://stackoverflow.com/questions/61888625/what-is-f-in-the-for-loop-command
 for /f "tokens=*" %%i in ('ideasy %IDE_OPTIONS% env') do (
