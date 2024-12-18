190272929d58af297b87ec2e8cc475f0fbb3a769
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2024-12-18 13:50:57 +0100

    #759: fixed test

--- cli/src/test/java/com/devonfw/tools/ide/repo/CustomToolTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/repo/CustomToolTest.java
@@ -5,8 +5,11 @@ import java.nio.file.Path;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.devonfw.tools.ide.context.AbstractIdeTestContext;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.context.IdeSlf4jContext;
 import com.devonfw.tools.ide.context.IdeTestContext;
+import com.devonfw.tools.ide.os.SystemInfo;
 import com.devonfw.tools.ide.os.SystemInfoMock;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
@@ -71,7 +74,9 @@ public class CustomToolTest extends Assertions {
   @Test
   public void testReadCustomToolsFromJson() {
     // arrange
-    IdeContext context = new IdeTestContext();
+    AbstractIdeTestContext context = new IdeSlf4jContext(Path.of(""));
+    SystemInfo systemInfo = SystemInfoMock.of("linux");
+    context.setSystemInfo(systemInfo);
     Path testPath = Path.of("src/test/resources/customtools");
     // act
     CustomToolsJson customToolsJson = CustomToolRepositoryImpl.readCustomToolsFromJson(context, testPath.resolve("custom-tools.json"));
@@ -80,7 +85,7 @@ public class CustomToolTest extends Assertions {
     assertThat(customToolsJson.tools().get(0).getUrl()).isEqualTo(
         "https://some-file-server.company.com/projects/my-project/jboss-eap/7.1.4.GA/jboss-eap-7.1.4.GA.tgz");
     assertThat(customToolsJson.tools().get(1).getUrl()).isEqualTo(
-        "https://some-file-server.company.com/projects/my-project2/firefox/70.0.1/firefox-70.0.1-windows-x64.tgz");
+        "https://some-file-server.company.com/projects/my-project2/firefox/70.0.1/firefox-70.0.1-linux-x64.tgz");
   }
 
   @Test
