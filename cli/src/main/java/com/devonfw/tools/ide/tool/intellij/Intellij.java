9acfffdb4af0b248b66abc31c698e59f44d3ec09
alfeilex <101652401+alfeilex@users.noreply.github.com>
2025-01-08 13:08:18 +0100

    add plugin installation with installPlugins

--- cli/src/main/java/com/devonfw/tools/ide/tool/intellij/Intellij.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/intellij/Intellij.java
@@ -1,15 +1,21 @@
 package com.devonfw.tools.ide.tool.intellij;
 
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.EnvironmentContext;
+import com.devonfw.tools.ide.process.ProcessMode;
+import com.devonfw.tools.ide.step.Step;
 import com.devonfw.tools.ide.tool.ToolInstallation;
 import com.devonfw.tools.ide.tool.ide.IdeToolCommandlet;
 import com.devonfw.tools.ide.tool.ide.IdeaBasedIdeToolCommandlet;
 import com.devonfw.tools.ide.tool.java.Java;
+import com.devonfw.tools.ide.tool.plugin.ToolPluginDescriptor;
 
 /**
  * {@link IdeToolCommandlet} for <a href="https://www.jetbrains.com/idea/">IntelliJ</a>.
@@ -63,4 +69,41 @@ public class Intellij extends IdeaBasedIdeToolCommandlet {
     getCommandlet(Java.class).install();
   }
 
+  @Override
+  public void installPlugin(ToolPluginDescriptor plugin, Step step) {
+
+    doInstallPlugins(List.of(plugin));
+    step.success();
+  }
+
+  @Override
+  protected void installPlugins(Collection<ToolPluginDescriptor> plugins) {
+
+    List<ToolPluginDescriptor> pluginsToInstall = new ArrayList<>();
+
+    for (ToolPluginDescriptor plugin : plugins) {
+      if (plugin.active()) {
+        pluginsToInstall.add(plugin);
+      }
+    }
+    doInstallPlugins(pluginsToInstall);
+  }
+
+  private void doInstallPlugins(List<ToolPluginDescriptor> pluginsToInstall) {
+
+    List<String> extensionsCommands = new ArrayList<>();
+
+    if (pluginsToInstall.isEmpty()) {
+      this.context.info("No plugins to be installed");
+    } else {
+
+      extensionsCommands.add("installPlugins");
+
+      for (ToolPluginDescriptor plugin : pluginsToInstall) {
+        extensionsCommands.add(plugin.id());
+      }
+    }
+
+    runTool(ProcessMode.DEFAULT, null, extensionsCommands.toArray(new String[0]));
+  }
 }
