e70b96e8e2443e2d03256649125544ae5ff6f734
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-02-05 11:52:27 +0100

    #404: fixed assertThat import

--- cli/src/test/java/com/devonfw/tools/ide/url/model/file/UrlStatusFileTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/url/model/file/UrlStatusFileTest.java
@@ -2,7 +2,6 @@ package com.devonfw.tools.ide.url.model.file;
 
 import java.time.Instant;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.devonfw.tools.ide.url.model.AbstractUrlModelTest;
@@ -34,16 +33,16 @@ public class UrlStatusFileTest extends AbstractUrlModelTest {
     UrlStatusFile status = version.getOrCreateStatus();
     StatusJson statusJson = status.getStatusJson();
     // then
-    Assertions.assertThat(statusJson.getUrls()).hasSize(1);
+    assertThat(statusJson.getUrls()).hasSize(1);
     UrlStatus urlStatus = statusJson.getUrls().values().iterator().next();
     UrlStatusState success = urlStatus.getSuccess();
-    Assertions.assertThat(success.getTimestamp()).isEqualTo(Instant.parse("2023-02-21T15:03:09.387386Z"));
-    Assertions.assertThat(success.getCode()).isNull();
-    Assertions.assertThat(success.getMessage()).isNull();
+    assertThat(success.getTimestamp()).isEqualTo(Instant.parse("2023-02-21T15:03:09.387386Z"));
+    assertThat(success.getCode()).isNull();
+    assertThat(success.getMessage()).isNull();
     UrlStatusState error = urlStatus.getError();
-    Assertions.assertThat(error.getTimestamp()).isEqualTo(Instant.parse("2023-01-01T23:59:59.999999Z"));
-    Assertions.assertThat(error.getCode()).isEqualTo(500);
-    Assertions.assertThat(error.getMessage()).isEqualTo("core dumped");
+    assertThat(error.getTimestamp()).isEqualTo(Instant.parse("2023-01-01T23:59:59.999999Z"));
+    assertThat(error.getCode()).isEqualTo(500);
+    assertThat(error.getMessage()).isEqualTo("core dumped");
   }
 
 }
