0f46dc816319a3dc1fb2d965d19f5dae1415c313
Julia Assi <julia.assi@capgemini.com>
2025-02-10 11:59:16 +0100

    #925: Corrected test project setup and tests

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/EnvironmentCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/EnvironmentCommandletTest.java
@@ -24,10 +24,11 @@ public class EnvironmentCommandletTest extends AbstractIdeContextTest {
   public void testRunDebugLogging() {
 
     // arrange
-    String path = "project/workspaces/main";
+    String path = "project/workspaces/foo-test";
     IdeTestContext context = newContext(ENVIRONMENT_COMMANDLET, path, false);
     context.setSystemInfo(SystemInfoMock.LINUX_X64);
     context.getSystem().setProperty("JAVA_HOME", "${USER_HOME}/not_existing");
+//    context.set
     EnvironmentCommandlet env = context.getCommandletManager().getCommandlet(EnvironmentCommandlet.class);
     Path userProperties = context.getUserHomeIde().resolve("ide.properties");
     Path settingsIdeProperties = context.getSettingsPath().resolve("ide.properties");
@@ -42,23 +43,41 @@ public class EnvironmentCommandletTest extends AbstractIdeContextTest {
     assertThat(context).log().hasEntriesWithNothingElseInBetween( //
         IdeLogEntry.ofDebug("from USER@" + userProperties.toString() + ":"), //
         IdeLogEntry.ofProcessable("DOCKER_EDITION=\"docker\""), //
+        IdeLogEntry.ofProcessable("FOO=\"foo-bar-some-${UNDEFINED}\""), //
 
         IdeLogEntry.ofDebug("from SETTINGS@" + settingsIdeProperties + ":"),
+        IdeLogEntry.ofProcessable("BAR=\"bar-some-${UNDEFINED}\""), //
+        IdeLogEntry.ofProcessable("ECLIPSE_VERSION=\"2023-03\""), //
+        IdeLogEntry.ofProcessable("IDE_TOOLS=\"mvn,eclipse\""), //
         IdeLogEntry.ofProcessable("INTELLIJ_EDITION=\"ultimate\""), //
         IdeLogEntry.ofProcessable("JAVA_VERSION=\"17*\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS4=\" settings4\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGSb=\"user10 workspace10 settingsb  user1 settings1 workspace1 conf1  user3 workspace3 confa userb\""), //
 
         IdeLogEntry.ofDebug("from WORKSPACE@" + workspaceIdeProperties + ":"), //
-        IdeLogEntry.ofProcessable("TEST_ARGS1=\"workspace1\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS10=\"user10 workspace10\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS3=\" user3 workspace3\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS9=\"settings9 workspace9\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGSd=\" user1 settings1 workspace1 conf1 userd workspaced\""), //
 
         IdeLogEntry.ofDebug("from CONF@" + confIdeProperties + ":"), //
         IdeLogEntry.ofProcessable("MVN_VERSION=\"3.9.1\""), //
+        IdeLogEntry.ofProcessable("SOME=\"some-${UNDEFINED}\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS1=\" user1 settings1 workspace1 conf1\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS2=\" user2 conf2\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS5=\" settings5 conf5\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS6=\" settings6 workspace6 conf6\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS7=\"user7 settings7 workspace7 conf7\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGS8=\"settings8 workspace8 conf8\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGSa=\" user1 settings1 workspace1 conf1  user3 workspace3 confa\""), //
+        IdeLogEntry.ofProcessable("TEST_ARGSc=\" user1 settings1 workspace1 conf1 userc settingsc confc\""), //
 
         IdeLogEntry.ofDebug("from RESOLVED:"), //
         IdeLogEntry.ofProcessable("HOME=\"" + normalize(context.getUserHome()) + "\""), //
         IdeLogEntry.ofProcessable("IDE_HOME=\"" + normalize(context.getIdeHome()) + "\""), //
         IdeLogEntry.ofProcessable("export M2_REPO=\"" + context.getUserHome() + "/.m2/repository\""), //
         new IdeLogEntry(IdeLogLevel.PROCESSABLE, "export PATH=", true), //
-        IdeLogEntry.ofProcessable("WORKSPACE=\"main\""), //
+        IdeLogEntry.ofProcessable("WORKSPACE=\"foo-test\""), //
         IdeLogEntry.ofProcessable("WORKSPACE_PATH=\"" + normalize(context.getWorkspacePath()) + "\""), //
 
         IdeLogEntry.ofDebug("from TOOL@" + softwarePath + ":"), //
