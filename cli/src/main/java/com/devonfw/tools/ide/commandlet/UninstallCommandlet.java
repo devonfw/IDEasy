7967bdc13d2283dc09e0df0ea1f1eba89e572f53
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2025-02-24 16:35:51 +0100

    #789: implement uninstall of IDEasy

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/UninstallCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/UninstallCommandlet.java
@@ -2,6 +2,7 @@ package com.devonfw.tools.ide.commandlet;
 
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.property.ToolProperty;
+import com.devonfw.tools.ide.tool.IdeasyCommandlet;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
 
 /**
@@ -21,7 +22,7 @@ public class UninstallCommandlet extends Commandlet {
 
     super(context);
     addKeyword(getName());
-    this.tools = add(new ToolProperty("", true, true, "tool"));
+    this.tools = add(new ToolProperty("", false, true, "tool"));
   }
 
   @Override
@@ -33,7 +34,18 @@ public class UninstallCommandlet extends Commandlet {
   @Override
   public void run() {
 
-    for (int i = 0; i < this.tools.getValueCount(); i++) {
+    int valueCount = this.tools.getValueCount();
+    if (valueCount == 0) {
+      if (!this.context.isForceMode()) {
+        this.context.askToContinue("Sub-command uninstall without any further arguments will perform the entire uninstallation of IDEasy.\n"
+            + "Since this is typically not to be called manually, you may have forgotten to specify the tool to install as extra argument.\n"
+            + "The current command will uninstall IDEasy from your computer. Are you sure?");
+      }
+      IdeasyCommandlet ideasy = new IdeasyCommandlet(this.context);
+      ideasy.uninstallIdeasy();
+      return;
+    }
+    for (int i = 0; i < valueCount; i++) {
       ToolCommandlet toolCommandlet = this.tools.getValue(i);
       if (toolCommandlet.getInstalledVersion() != null) {
         toolCommandlet.uninstall();
