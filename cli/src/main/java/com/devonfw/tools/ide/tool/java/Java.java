f24a728c291e36ceb3e90f946645e2ac00bfcf26
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-02-28 18:19:04 +0100

    #1058: fixed missing JAVA_HOME env var Fixes: #1058
    ### Implements:
    * added missing JAVA_HOME to environment variables

--- cli/src/main/java/com/devonfw/tools/ide/tool/java/Java.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/java/Java.java
@@ -4,8 +4,10 @@ import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.process.EnvironmentContext;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
+import com.devonfw.tools.ide.tool.ToolInstallation;
 
 /**
  * {@link ToolCommandlet} for Java (Java Virtual Machine and Java Development Kit).
@@ -20,6 +22,13 @@ public class Java extends LocalToolCommandlet {
   public Java(IdeContext context) {
 
     super(context, "java", Set.of(Tag.JAVA, Tag.RUNTIME));
+  }
+
+  @Override
+  public void setEnvironment(EnvironmentContext environmentContext, ToolInstallation toolInstallation, boolean extraInstallation) {
+
+    super.setEnvironment(environmentContext, toolInstallation, extraInstallation);
+    environmentContext.withEnvVar("JAVA_HOME", toolInstallation.linkDir().toString());
   }
 
   @Override
