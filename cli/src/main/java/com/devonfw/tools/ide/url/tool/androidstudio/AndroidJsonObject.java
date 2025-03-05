b003ef91493b2322fbd383a6e85b39bce6439373
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-03-05 16:21:25 +0100

    #1024: implemented requested changes moved url specific classes from cli module to url-updater module

--- cli/src/main/java/com/devonfw/tools/ide/url/tool/androidstudio/AndroidJsonObject.java
+++ cli/src/main/java/com/devonfw/tools/ide/url/tool/androidstudio/AndroidJsonObject.java
@@ -1,6 +1,7 @@
-package com.devonfw.tools.ide.tool.androidstudio;
+package com.devonfw.tools.ide.url.tool.androidstudio;
 
 import com.devonfw.tools.ide.json.JsonObject;
+import com.devonfw.tools.ide.tool.androidstudio.AndroidJsonContent;
 
 /**
  * {@link JsonObject} for Android Studio content.
