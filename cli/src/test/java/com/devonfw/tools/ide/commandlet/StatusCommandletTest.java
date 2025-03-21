c743c4277d124ed7deca6cc62c45ef2cfcd0878b
paz-capgemini <pazifik.ngo@capgemini.com>
2025-03-21 09:22:20 +0100

    #1110: Fixed ide status fails with IllegalStateException when being offline (#1125)
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/test/java/com/devonfw/tools/ide/commandlet/StatusCommandletTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/commandlet/StatusCommandletTest.java
@@ -2,7 +2,6 @@ package com.devonfw.tools.ide.commandlet;
 
 import org.junit.jupiter.api.Test;
 
-import com.devonfw.tools.ide.cli.CliArguments;
 import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 
@@ -17,13 +16,30 @@ public class StatusCommandletTest extends AbstractIdeContextTest {
   public void testStatusOutsideOfHome() {
     //arrange
     IdeTestContext context = new IdeTestContext();
-    CliArguments args = new CliArguments("status");
-    args.next();
+    StatusCommandlet status = context.getCommandletManager().getCommandlet(StatusCommandlet.class);
 
     //act
-    context.run(args);
+    status.run();
 
     //assert
     assertThat(context).logAtWarning().hasMessageContaining("You are not inside an IDE project: ");
+  }
+
+  /**
+   * Tests the output if {@link StatusCommandlet} is run without internet connection.
+   */
+  @Test
+  public void testStatusWhenOffline() {
+
+    // arrange
+    IdeTestContext context = new IdeTestContext();
+    context.setOnline(false);
+    StatusCommandlet status = context.getCommandletManager().getCommandlet(StatusCommandlet.class);
+
+    // act
+    status.run();
+
+    // assert
+    assertThat(context).logAtWarning().hasMessage("Check for newer version of IDEasy is skipped due to no network connectivity.");
   }
 }
