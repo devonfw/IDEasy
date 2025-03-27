874660d8cea93227173ae7733f5e2cc20c4011f7
KianRolf <kian.loroff@capgemini.com>
2025-03-27 09:20:34 +0100

    test fix

--- cli/src/test/java/com/devonfw/tools/ide/url/model/file/json/ToolSecurityTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/url/model/file/json/ToolSecurityTest.java
@@ -27,7 +27,10 @@ public class ToolSecurityTest extends AbstractUrlModelTest {
     Collection<CVE> security = context.getDefaultToolRepository()
         .findSecurity("tomcat", "tomcat", VersionIdentifier.of("2.40.1"));
     List<VersionRange> versionRanges = new ArrayList<>();
+    versionRanges.add(VersionRange.of("(0,2.39.4)"));
+    versionRanges.add(VersionRange.of("[2.40.0,2.40.2)"));
+
     // assert
-    assertThat(security).containsExactly(new CVE("java", 2.0f, versionRanges));
+    assertThat(security).containsExactly(new CVE("CVE-2024-32002", 9.0f, versionRanges));
   }
 }
