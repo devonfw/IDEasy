66646da3312571f1738a46eb6dd39fe83c529152
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-04-28 14:46:41 +0200

    #583: implemented requested changes added some assertions to help test added assertThat function to help test

--- cli/src/test/integration-tests/help.sh
+++ cli/src/test/integration-tests/help.sh
@@ -1,3 +1,22 @@
 #--- Body ---
 echo "Running help integration test"
-ide help
+output=$(ide help)
+
+assertThat() {
+  local output="$1"
+  local keyword="$2"
+
+  if echo "$output" | grep -q "$keyword"; then
+    echo "Assertion passed: '$keyword' found in output"
+    return 0
+  else
+    echo "Assertion failed: '$keyword' not found in output"
+    return 1
+  fi
+}
+
+assertThat "$output" "Current version"
+assertThat "$output" "build"
+assertThat "$output" "Usage"
+assertThat "$output" "docker"
+assertThat "$output" "--locale"
