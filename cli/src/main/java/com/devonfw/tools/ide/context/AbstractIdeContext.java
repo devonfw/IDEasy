25b2adc370c457bea9092d9dc517bf52b328ef3a
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-06-04 11:25:05 +0200

    #1340: fixed order of variables enhanced warning message with hint for the user

--- cli/src/main/java/com/devonfw/tools/ide/context/AbstractIdeContext.java
+++ cli/src/main/java/com/devonfw/tools/ide/context/AbstractIdeContext.java
@@ -210,8 +210,11 @@ public abstract class AbstractIdeContext implements IdeContext {
       Path ideRootPathFromEnv = getIdeRootPathFromEnv(true);
       ideRootPath = ideHomePath.getParent();
       if ((ideRootPathFromEnv != null) && !ideRootPath.toString().equals(ideRootPathFromEnv.toString())) {
-        warning("Variable IDE_ROOT is set to '{}' but for your project '{}' the path '{}' would have been expected.", ideRootPathFromEnv,
-            ideRootPath, ideHomePath.getFileName());
+        warning(
+            "Variable IDE_ROOT is set to '{}' but for your project '{}' the path '{}' would have been expected.\n"
+                + "Please check your 'user.dir' or working directory setting and make sure that it matches your IDE_ROOT variable.",
+            ideRootPathFromEnv,
+            ideHomePath.getFileName(), ideRootPath);
       }
 
     } else if (!isTest()) {
