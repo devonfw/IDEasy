361465e78261d8c1a9d758a537f9bdbdf9e6bcc3
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2025-06-26 17:14:35 +0200

    #1372: Add gradle wrapper support (#1388)
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/main/java/com/devonfw/tools/ide/tool/gradle/Gradle.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/gradle/Gradle.java
@@ -2,10 +2,14 @@ package com.devonfw.tools.ide.tool.gradle;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessErrorHandling;
+import com.devonfw.tools.ide.process.ProcessMode;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.tool.ToolCommandlet;
 
@@ -13,6 +17,10 @@ import com.devonfw.tools.ide.tool.ToolCommandlet;
  * {@link ToolCommandlet} for <a href="https://gradle.org/">gradle</a>.
  */
 public class Gradle extends LocalToolCommandlet {
+
+  private static final String BUILD_GRADLE = "build.gradle";
+  private static final String BUILD_GRADLE_KTS = "build.gradle.kts";
+  private static final String GRADLE_WRAPPER_FILENAME = "gradlew";
 
   /**
    * The name of the gradle configuration folder.
@@ -36,6 +44,13 @@ public class Gradle extends LocalToolCommandlet {
     return "--help";
   }
 
+  @Override
+  protected void configureToolBinary(ProcessContext pc, ProcessMode processMode, ProcessErrorHandling errorHandling) {
+    Path gradle = Path.of(getBinaryName());
+    Path wrapper = findWrapper(GRADLE_WRAPPER_FILENAME, path -> Files.exists(path.resolve(BUILD_GRADLE)) || Files.exists(path.resolve(BUILD_GRADLE_KTS)));
+    pc.executable(Objects.requireNonNullElse(wrapper, gradle));
+  }
+  
   /**
    * @return the {@link Path} to the gradle configuration folder, creates the folder if it was not existing.
    */
