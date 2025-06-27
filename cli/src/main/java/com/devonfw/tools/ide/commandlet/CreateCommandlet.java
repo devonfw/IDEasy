d847f1f94b7cd01575e4f345dd5e91dd1cff6244
Yong Wu <wustudent@outlook.com>
2025-06-27 15:45:21 +0200

    #742 Move strategies to AbstractUpdateCommandlet.
    By moving the strategies to AbstractUpdateCommandlet and defining the default behavior of processRepository, the bug is fixed.

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/CreateCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/CreateCommandlet.java
@@ -5,12 +5,10 @@ import java.nio.file.Path;
 import java.util.function.Predicate;
 
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.git.GitContext;
 import com.devonfw.tools.ide.git.GitUrl;
 import com.devonfw.tools.ide.io.FileAccess;
 import com.devonfw.tools.ide.property.FlagProperty;
 import com.devonfw.tools.ide.property.StringProperty;
-import com.devonfw.tools.ide.step.Step;
 import com.devonfw.tools.ide.version.IdeVersion;
 
 /**
@@ -81,9 +79,6 @@ public class CreateCommandlet extends AbstractUpdateCommandlet {
     fileAccess.mkdirs(newInstancePath.resolve(IdeContext.FOLDER_WORKSPACES).resolve(IdeContext.WORKSPACE_MAIN));
   }
 
-  /**
-   * Handles cases for settings and code repository during creation.
-   */
   @Override
   protected void processRepository() {
     RepositoryStrategy repositoryStrategy = new SettingsRepositoryStrategy();
@@ -92,169 +87,6 @@ public class CreateCommandlet extends AbstractUpdateCommandlet {
     }
 
     processRepositoryUsingStrategy(repositoryStrategy);
-  }
-
-  private void processRepositoryUsingStrategy(RepositoryStrategy strategy) {
-    Step step = strategy.createNewStep(this.context);
-    String repository = this.settingsRepo.getValue();
-    if (repository == null || repository.isBlank()) {
-      repository = strategy.handleBlankRepository(this.context);
-    }
-    if ("-".equals(repository)) {
-      repository = IdeContext.DEFAULT_SETTINGS_REPO_URL;
-    }
-    GitUrl gitUrl = GitUrl.of(repository);
-    strategy.checkProjectNameConvention(this.context, gitUrl.getProjectName());
-    strategy.initializeRepository(this.context, gitUrl);
-    strategy.resolveStep(step);
-  }
-
-  /**
-   * Strategy for handling repository.
-   */
-  interface RepositoryStrategy {
-
-    /**
-     * Handler for blank repository, displays warning and asks for input of repository URL.
-     *
-     * @param context ide context
-     * @return repository url from user input
-     */
-    String handleBlankRepository(IdeContext context);
-
-    /**
-     * Check the given project name, displays warning when name does not meet convention.
-     *
-     * @param context ide context
-     * @param projectName the project name of repository
-     */
-    void checkProjectNameConvention(IdeContext context, String projectName);
-
-    /**
-     * Initialize the given Git repository.
-     *
-     * @param context ide context
-     * @param gitUrl URL of the git repository
-     */
-    void initializeRepository(IdeContext context, GitUrl gitUrl);
-
-    /**
-     * Create a new commandlet step.
-     *
-     * @param context ide context
-     * @return the created new commandlet Step
-     */
-    Step createNewStep(IdeContext context);
-
-    /**
-     * Resolve the given commandlet step.
-     *
-     * @param step to resolve
-     */
-    void resolveStep(Step step);
-  }
-
-  /**
-   * Strategy implementation for code repository.
-   */
-  static class CodeRepositoryStrategy implements RepositoryStrategy {
-
-    @Override
-    public String handleBlankRepository(IdeContext context) {
-      String message = """
-          No code repository was given after '--code'.
-          Please give the code repository below that includes your settings folder.
-          Further details can be found here: https://github.com/devonfw/IDEasy/blob/main/documentation/settings.adoc
-          Code repository URL:
-          """;
-      return context.askForInput(message);
-    }
-
-    @Override
-    public void checkProjectNameConvention(IdeContext context, String projectName) {
-      if (projectName.contains(IdeContext.SETTINGS_REPOSITORY_KEYWORD)) {
-        String warningTemplate = """
-            Your git URL is pointing to the project name {} that contains the keyword '{}'.
-            Therefore we assume that you did a mistake by adding the '--code' option to the ide project creation.
-            Do you really want to create the project?
-            """;
-        context.askToContinue(warningTemplate, projectName, IdeContext.SETTINGS_REPOSITORY_KEYWORD);
-      }
-    }
-
-    @Override
-    public void initializeRepository(IdeContext context, GitUrl gitUrl) {
-      // clone the given repository into IDE_HOME/workspaces/main
-      Path codeRepoPath = context.getWorkspacePath().resolve(gitUrl.getProjectName());
-      context.getGitContext().pullOrClone(gitUrl, codeRepoPath);
-
-      // check for settings folder and create symlink to IDE_HOME/settings
-      Path settingsFolder = codeRepoPath.resolve(IdeContext.FOLDER_SETTINGS);
-      if (Files.exists(settingsFolder)) {
-        context.getFileAccess().symlink(settingsFolder, context.getSettingsPath());
-        // create a file in IDE_HOME with the current local commit id
-        context.getGitContext().saveCurrentCommitId(codeRepoPath, context.getSettingsCommitIdPath());
-      } else {
-        context.warning("No settings folder was found inside the code repository.");
-      }
-    }
-
-    @Override
-    public Step createNewStep(IdeContext context) {
-      return context.newStep("Clone code repository");
-    }
-
-    @Override
-    public void resolveStep(Step step) {
-      step.success("Successfully updated code repository.");
-    }
-  }
-
-  /**
-   * Strategy implementation for settings repository.
-   */
-  static class SettingsRepositoryStrategy implements RepositoryStrategy {
-
-    @Override
-    public String handleBlankRepository(IdeContext context) {
-      Path settingsPath = context.getSettingsPath();
-      String message = "Missing your settings at " + settingsPath + " and no SETTINGS_URL is defined.\n"
-          + "Further details can be found here: https://github.com/devonfw/IDEasy/blob/main/documentation/settings.adoc\n"
-          + "Please contact the technical lead of your project to get the SETTINGS_URL for your project.\n"
-          + "In case you just want to test IDEasy you may simply hit return to install the default settings.\n" + "Settings URL ["
-          + IdeContext.DEFAULT_SETTINGS_REPO_URL + "]:";
-      return context.askForInput(message, IdeContext.DEFAULT_SETTINGS_REPO_URL);
-    }
-
-    @Override
-    public void checkProjectNameConvention(IdeContext context, String projectName) {
-      if (!projectName.contains(IdeContext.SETTINGS_REPOSITORY_KEYWORD)) {
-        String warningTemplate = """
-            Your git URL is pointing to the project name {} that does not contain the keyword ''{}''.
-            Therefore we assume that you forgot to add the '--code' option to the ide project creation.
-            Do you really want to create the project?
-            """;
-        context.askToContinue(warningTemplate, projectName, IdeContext.SETTINGS_REPOSITORY_KEYWORD);
-      }
-    }
-
-    @Override
-    public void initializeRepository(IdeContext context, GitUrl gitUrl) {
-      Path settingsPath = context.getSettingsPath();
-      GitContext gitContext = context.getGitContext();
-      gitContext.pullOrClone(gitUrl, settingsPath);
-      context.getGitContext().saveCurrentCommitId(settingsPath, context.getSettingsCommitIdPath());
-    }
-
-    @Override
-    public Step createNewStep(IdeContext context) {
-      return context.newStep("Clone settings repository");
-    }
-
-    @Override
-    public void resolveStep(Step step) {
-      step.success("Successfully updated settings repository.");
-    }
   }
 
   @Override
