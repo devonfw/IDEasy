0bf67742ddb992875cdebd7081944571f057f061
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2025-07-30 00:34:32 +0200

    generated next test with copilot

---
+++ url-updater/src/test/java/com/devonfw/tools/ide/url/tool/terraform/TerraformUrlUpdaterMock.java
@@ -0,0 +1,29 @@
+package com.devonfw.tools.ide.url.tool.terraform;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+
+/**
+ * Mock of {@link TerraformUrlUpdater} to allow integration testing with wiremock.
+ */
+public class TerraformUrlUpdaterMock extends TerraformUrlUpdater {
+
+  private final String baseUrl;
+
+  public TerraformUrlUpdaterMock(WireMockRuntimeInfo wireMockRuntimeInfo) {
+    super();
+    this.baseUrl = wireMockRuntimeInfo.getHttpBaseUrl();
+  }
+
+  @Override
+  public String getBaseUrl() {
+
+    return this.baseUrl;
+  }
+
+  @Override
+  protected String doGetVersionUrl() {
+
+    return this.baseUrl + "/repos/" + getGithubOrganization() + "/" + getGithubRepository() + "/git/refs/tags";
+  }
+}
+
