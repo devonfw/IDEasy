9cf8bb03c3f189aba8f3cf4e02c29d8e2762498c
juliane-cap <juliane.kostiuk@capgemini.com>
2025-08-13 10:07:50 +0200

    #1444: Added UvUrlUpdater

---
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/tool/uv/UvUrlUpdater.java
@@ -0,0 +1,58 @@
+package com.devonfw.tools.ide.url.tool.uv;
+
+import com.devonfw.tools.ide.url.model.folder.UrlVersion;
+import com.devonfw.tools.ide.url.updater.GithubUrlUpdater;
+import com.devonfw.tools.ide.version.VersionIdentifier;
+
+/**
+ * {@link GithubUrlUpdater} for uv.
+ */
+public class UvUrlUpdater extends GithubUrlUpdater {
+
+  private static final VersionIdentifier MIN_WIN_ARM_VID = VersionIdentifier.of("0.5.25");
+
+  @Override
+  protected String getTool() {
+
+    return "uv";
+  }
+
+  @Override
+  protected String getGithubOrganization() {
+
+    return "astral-sh";
+  }
+
+  @Override
+  protected String getGithubRepository() {
+
+    return "uv";
+  }
+
+  @Override
+  protected void addVersion(UrlVersion urlVersion) {
+
+    VersionIdentifier vid = urlVersion.getVersionIdentifier();
+    String baseUrl = getDownloadBaseUrl() + "/" + getGithubOrganization() + "/" + getGithubRepository() + "/releases/download/${version}/uv-";
+
+    doAddVersion(urlVersion, baseUrl + "x86_64-pc-windows-msvc.zip", WINDOWS, X64);
+    doAddVersion(urlVersion, baseUrl + "x86_64-unknown-linux-gnu.tar.gz", LINUX, X64);
+    doAddVersion(urlVersion, baseUrl + "aarch64-unknown-linux-gnu.tar.gz", LINUX, ARM64);
+    doAddVersion(urlVersion, baseUrl + "x86_64-apple-darwin.tar.gz", MAC, X64);
+    doAddVersion(urlVersion, baseUrl + "aarch64-apple-darwin.tar.gz", MAC, ARM64);
+    if (vid.compareVersion(MIN_WIN_ARM_VID).isGreater()) {
+      doAddVersion(urlVersion, baseUrl + "aarch64-pc-windows-msvc.zip", WINDOWS, ARM64);
+    }
+  }
+
+  @Override
+  public String getCpeVendor() {
+    return "astral-sh";
+  }
+
+  @Override
+  public String getCpeProduct() {
+    return "uv";
+  }
+
+}
