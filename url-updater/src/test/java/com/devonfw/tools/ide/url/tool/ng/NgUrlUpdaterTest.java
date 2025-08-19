0d962bf8b92686ff30acc58e2c1cafae8576751a
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-08-19 21:45:30 +0200

    #1454: add test for angular url updater added test for NgUrlUpdater added test resource for ng versions

---
+++ url-updater/src/test/java/com/devonfw/tools/ide/url/tool/ng/NgUrlUpdaterTest.java
@@ -0,0 +1,56 @@
+package com.devonfw.tools.ide.url.tool.ng;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import com.devonfw.tools.ide.url.model.folder.UrlRepository;
+import com.devonfw.tools.ide.url.updater.AbstractUrlUpdaterTest;
+import com.devonfw.tools.ide.url.updater.JsonUrlUpdater;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+
+/**
+ * Test class for integrations of the {@link NgUrlUpdater}
+ */
+@WireMockTest
+public class NgUrlUpdaterTest extends AbstractUrlUpdaterTest {
+
+  /**
+   * Test of {@link JsonUrlUpdater} for the creation of {@link NgUrlUpdater} download URLs and checksums.
+   *
+   * @param tempDir Path to a temporary directory
+   * @param wmRuntimeInfo the {@link WireMockRuntimeInfo}.
+   * @throws IOException test fails
+   */
+  @Test
+  public void testNgJsonUrlUpdaterCreatesDownloadUrlsAndChecksums(@TempDir Path tempDir, WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+
+    // arrange
+    stubFor(get(urlMatching("/@angular/cli")).willReturn(
+        aResponse().withStatus(200).withBody(getJsonBody(wmRuntimeInfo))));
+
+    stubFor(any(urlMatching("/@angular/cli/-/cli-[1-9.].*.tgz")).willReturn(aResponse().withStatus(200).withBody(DOWNLOAD_CONTENT)));
+
+    UrlRepository urlRepository = UrlRepository.load(tempDir);
+    NgUrlUpdaterMock updater = new NgUrlUpdaterMock(wmRuntimeInfo);
+
+    // act
+    updater.update(urlRepository);
+
+    // assert
+    assertUrlVersionAgnostic(tempDir.resolve("ng").resolve("ng").resolve("20.1.6"));
+  }
+
+  private static String getJsonBody(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+    return readAndResolve(PATH_INTEGRATION_TEST.resolve("NgUrlUpdater").resolve("ng-version.json"), wmRuntimeInfo);
+  }
+}
