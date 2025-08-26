faa40be4bb159d931dd0867b116238eb87f9e5a1
Sheng Yu <yusheng.email@gmail.com>
2025-08-26 11:55:38 +0200

    add: placeholder enum

--- cli/src/main/java/com/devonfw/tools/ide/tool/openrewrite/RefactorRecipeEnum.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/openrewrite/RefactorRecipeEnum.java
@@ -1,5 +1,5 @@
 package com.devonfw.tools.ide.tool.openrewrite;
 
 public enum RefactorRecipeEnum {
-  FORMAT_JAVA_CODE, REMOVE_BLANK_LINES
+  FORMAT_JAVA_CODE, REMOVE_BLANK_LINES, UNRECOGNIZED_RECIPE
 }
