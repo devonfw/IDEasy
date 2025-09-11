7c3d636927510c6e90f1eae33b021b006f86bce9
KianRolf <kian.loroff@capgemini.com>
2025-09-11 22:53:52 +0200

    Update cli/src/main/java/com/devonfw/tools/ide/variable/IdeVariables.java
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/main/java/com/devonfw/tools/ide/variable/IdeVariables.java
+++ cli/src/main/java/com/devonfw/tools/ide/variable/IdeVariables.java
@@ -32,7 +32,7 @@ public interface IdeVariables {
   VariableDefinitionPath WORKSPACE_PATH = new VariableDefinitionPath("WORKSPACE_PATH", null, c -> c.getWorkspacePath(), true);
 
   /** {@link VariableDefinition} for default CVE_MIN_SEVERITY. */
-  VariableDefinitionDoulbe CVE_MIN_SEVERIRY = new VariableDefinitionDoulbe("CVE_MIN_SEVERITY", null, c -> 1.0);
+  VariableDefinitionDoulbe CVE_MIN_SEVERIRY = new VariableDefinitionDoulbe("CVE_MIN_SEVERITY", null, c -> 0.1);
 
   /** {@link VariableDefinition} for list of tools to install by default. */
   VariableDefinitionStringList IDE_TOOLS = new VariableDefinitionStringList("IDE_TOOLS", "DEVON_IDE_TOOLS", c -> List.of("mvn", "npm"));
