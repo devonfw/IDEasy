ceddf8dad558f03ad7eda28248ee8bbb2151c8b0
Jörg Hohwiller <hohwille@users.noreply.github.com>
2025-09-12 23:26:47 +0200

    Update cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java

--- cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
@@ -729,7 +729,8 @@ public class FileAccessImpl implements FileAccess {
     return permissionStringBuilder.toString();
   }
 
-private void extractArchive(Path file, Path targetDir, Function<InputStream, ArchiveInputStream<?>> unpacker) {
+  private void extractArchive(Path file, Path targetDir, Function<InputStream, ArchiveInputStream<?>> unpacker) {
+
     this.context.info("Extracting TAR file {} to {}", file, targetDir);
 
     final List<PathLink> links = new ArrayList<>();
