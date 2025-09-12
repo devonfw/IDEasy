acbe3069f6ef8a708118d666d879776452bfd0b2
Jörg Hohwiller <hohwille@users.noreply.github.com>
2025-09-12 23:25:56 +0200

    Update cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java

--- cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
@@ -21,7 +21,6 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.DosFileAttributeView;
