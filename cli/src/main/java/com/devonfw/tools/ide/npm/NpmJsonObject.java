a37fc2b0123c8c8083aef398324d3551089b92fa
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2025-09-19 11:25:53 +0200

    added JavaDoc

--- cli/src/main/java/com/devonfw/tools/ide/npm/NpmJsonObject.java
+++ cli/src/main/java/com/devonfw/tools/ide/npm/NpmJsonObject.java
@@ -4,6 +4,8 @@ import com.devonfw.tools.ide.json.JsonObject;
 
 /**
  * {@link JsonObject} for {@link com.devonfw.tools.ide.tool.npm.Npm npm}.
+ *
+ * @param versions the {@link NpmJsonVersions} with all the version history of the artifact.
  */
 public record NpmJsonObject(NpmJsonVersions versions) implements JsonObject {
 
