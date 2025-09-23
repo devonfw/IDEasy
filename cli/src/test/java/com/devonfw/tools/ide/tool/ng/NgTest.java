e93fb352130f576b4675d4f2aedabbfa023da84a
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-09-23 16:39:07 +0200

    #907: Fixed tests fixed testNpmBuildWithProvidedArguments test fixed NgTests cleaned up build test resources (removed npm)

--- cli/src/test/java/com/devonfw/tools/ide/tool/ng/NgTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/ng/NgTest.java
@@ -1,5 +1,15 @@
 package com.devonfw.tools.ide.tool.ng;
 
+import static com.devonfw.tools.ide.context.IdeTestContext.readAndResolve;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -7,25 +17,48 @@ import com.devonfw.tools.ide.context.AbstractIdeContextTest;
 import com.devonfw.tools.ide.context.IdeTestContext;
 import com.devonfw.tools.ide.os.SystemInfo;
 import com.devonfw.tools.ide.os.SystemInfoMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 /**
  * Integration test of {@link Ng}.
  */
+@WireMockTest
 public class NgTest extends AbstractIdeContextTest {
 
   private static final String PROJECT_NG = "ng";
+
+  private static final Path PATH_INTEGRATION_TEST = Path.of("src/test/resources/ide-projects");
+
+  private static String ngVersion;
+
+  /**
+   * Creates a ng-version json file based on the given test resource in a temporary directory according to the http url and port of the
+   * {@link WireMockRuntimeInfo}.
+   *
+   * @param wmRuntimeInfo wireMock server on a random port
+   * @throws IOException on error.
+   */
+  @BeforeAll
+  public static void setupTestVersionJson(WireMockRuntimeInfo wmRuntimeInfo) throws IOException {
+    //preparing test data with dynamic port
+    Path testDataPath = PATH_INTEGRATION_TEST.resolve(PROJECT_NG);
+    ngVersion = readAndResolve(testDataPath.resolve("ng-version.json"), wmRuntimeInfo);
+  }
 
   /**
    * Tests if the {@link Ng} install works correctly across all three operating systems.
    *
    * @param os Operating system
+   * @param wireMockRuntimeInfo wireMock server on a random port
    */
   @ParameterizedTest
   @ValueSource(strings = { "windows", "mac", "linux" })
-  public void testNgInstall(String os) {
+  public void testNgInstall(String os, WireMockRuntimeInfo wireMockRuntimeInfo) {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_NG);
+    stubFor(get(urlMatching("/@angular/cli")).willReturn(aResponse().withStatus(200).withBody(ngVersion)));
+    IdeTestContext context = newContext(PROJECT_NG, wireMockRuntimeInfo);
     SystemInfo systemInfo = SystemInfoMock.of(os);
     context.setSystemInfo(systemInfo);
     Ng commandlet = new Ng(context);
@@ -41,13 +74,15 @@ public class NgTest extends AbstractIdeContextTest {
    * Tests if the {@link Ng} uninstall works correctly across all three operating systems.
    *
    * @param os Operating system
+   * @param wireMockRuntimeInfo wireMock server on a random port
    */
   @ParameterizedTest
   @ValueSource(strings = { "windows", "mac", "linux" })
-  public void testNgUninstall(String os) {
+  public void testNgUninstall(String os, WireMockRuntimeInfo wireMockRuntimeInfo) {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_NG);
+    stubFor(get(urlMatching("/@angular/cli")).willReturn(aResponse().withStatus(200).withBody(ngVersion)));
+    IdeTestContext context = newContext(PROJECT_NG, wireMockRuntimeInfo);
     SystemInfo systemInfo = SystemInfoMock.of(os);
     context.setSystemInfo(systemInfo);
     Ng commandlet = new Ng(context);
@@ -71,13 +106,15 @@ public class NgTest extends AbstractIdeContextTest {
    * Tests if {@link Ng} run works correctly across all three operating systems.
    *
    * @param os Operating system
+   * @param wireMockRuntimeInfo wireMock server on a random port
    */
   @ParameterizedTest
   @ValueSource(strings = { "windows", "mac", "linux" })
-  public void testNgRun(String os) {
+  public void testNgRun(String os, WireMockRuntimeInfo wireMockRuntimeInfo) {
 
     // arrange
-    IdeTestContext context = newContext(PROJECT_NG);
+    stubFor(get(urlMatching("/@angular/cli")).willReturn(aResponse().withStatus(200).withBody(ngVersion)));
+    IdeTestContext context = newContext(PROJECT_NG, wireMockRuntimeInfo);
     SystemInfo systemInfo = SystemInfoMock.of(os);
     context.setSystemInfo(systemInfo);
     Ng commandlet = new Ng(context);
