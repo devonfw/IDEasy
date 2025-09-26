6eff136f72e12eab715f015d35507cc4faa03009
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-09-26 13:05:22 +0200

    #1511: Fixed npm commands added prefix param to package installation, list and uninstall commands with path to current node installation

--- cli/src/main/java/com/devonfw/tools/ide/tool/node/NodeBasedCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/node/NodeBasedCommandlet.java
@@ -100,7 +100,7 @@ public abstract class NodeBasedCommandlet extends LocalToolCommandlet {
    * @param npmPackage the npm package to uninstall.
    */
   protected void runPackageUninstall(String npmPackage) {
-    runPackageManager("uninstall", "-g", npmPackage).failOnError();
+    runPackageManager("uninstall", "-g", npmPackage, "--prefix", this.context.getSoftwarePath().resolve("node").toAbsolutePath().toString()).failOnError();
   }
 
   /**
@@ -111,7 +111,7 @@ public abstract class NodeBasedCommandlet extends LocalToolCommandlet {
    */
   protected ProcessResult runPackageInstall(String npmPackage) {
 
-    return runPackageManager("install", "-g", npmPackage);
+    return runPackageManager("install", "-g", npmPackage, "--prefix", this.context.getSoftwarePath().resolve("node").toAbsolutePath().toString());
   }
 
   /**
