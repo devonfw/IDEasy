5196c9ceeaa3d9c218419c408d9b72003cddeef7
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2025-09-29 15:44:24 +0200

    #1511: Fixed npm commands (#1513)
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/main/java/com/devonfw/tools/ide/tool/npm/NpmBasedCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/npm/NpmBasedCommandlet.java
@@ -13,6 +13,7 @@ import com.devonfw.tools.ide.process.ProcessResult;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.tool.node.NodeBasedCommandlet;
 import com.devonfw.tools.ide.tool.repository.ToolRepository;
+import com.devonfw.tools.ide.util.StringUtil;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
 /**
@@ -78,7 +79,9 @@ public abstract class NpmBasedCommandlet extends NodeBasedCommandlet {
 
     ProcessContext pc = this.context.newProcess().errorHandling(errorHandling);
     Npm npm = this.context.getCommandletManager().getCommandlet(Npm.class);
-    return npm.runTool(processMode, null, pc, args);
+
+    return npm.runTool(processMode, null, pc,
+        StringUtil.extendArray(args, false, "--prefix", this.context.getSoftwarePath().resolve("node").toAbsolutePath().toString()));
   }
 
 }
