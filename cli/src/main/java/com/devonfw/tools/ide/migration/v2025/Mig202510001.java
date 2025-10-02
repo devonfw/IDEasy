31e8fb75764ab8e24f50153edb351394cb5447c2
Jörg Hohwiller <joerg.hohwiller@capgemini.com>
2025-10-02 18:30:36 +0200

    move to next release

--- cli/src/main/java/com/devonfw/tools/ide/migration/v2025/Mig202510001.java
+++ cli/src/main/java/com/devonfw/tools/ide/migration/v2025/Mig202510001.java
@@ -6,17 +6,17 @@ import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.migration.IdeVersionMigration;
 
 /**
- * Migration for 2025.02.001-beta. Removes "npm" folder from software path as the {@link com.devonfw.tools.ide.tool.npm.Npm} commandlet has changed entirely so
- * the installation of node based tools will now always happen in the "node" software folder.
+ * Migration to 2025.10.001. Removes "npm" folder from software path as the {@link com.devonfw.tools.ide.tool.npm.Npm} commandlet has changed entirely so the
+ * installation of node based tools will now always happen in the "node" software folder.
  */
-public class Mig202509001 extends IdeVersionMigration {
+public class Mig202510001 extends IdeVersionMigration {
 
   /**
    * The constructor.
    */
-  public Mig202509001() {
+  public Mig202510001() {
 
-    super("2025.09.001");
+    super("2025.10.001");
   }
 
   @Override
