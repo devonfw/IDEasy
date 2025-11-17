6f9042eb2ab408cd6864a37c64cc8e1df70fa39f
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2025-11-17 17:20:55 +0100

    #1590: Add SystemPath.findBinary(Path toolPath, Predicate<Path> filter) (#1592)
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/test/java/com/devonfw/tools/ide/common/SystemPathTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/common/SystemPathTest.java
@@ -1,14 +1,24 @@
 package com.devonfw.tools.ide.common;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import com.devonfw.tools.ide.context.AbstractIdeContextTest;
+import com.devonfw.tools.ide.context.IdeTestContext;
+import com.devonfw.tools.ide.os.SystemInfo;
+import com.devonfw.tools.ide.os.SystemInfoMock;
 
 /**
  * Unit tests of {@link SystemPath}.
  */
-public class SystemPathTest {
+public class SystemPathTest extends AbstractIdeContextTest {
 
   @ParameterizedTest
   // arrange
@@ -33,4 +43,107 @@ public class SystemPathTest {
     assertThat(testResult).isFalse();
   }
 
+  @Test
+  public void testfindBinaryFindsBashBinaryOnWindowsUsingFilterExcludingWindowsAppsInPath() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    SystemInfo systemInfo = SystemInfoMock.of("windows");
+    context.setSystemInfo(systemInfo);
+    Predicate<Path> ignoreWindowsApps = p -> checkPathToIgnoreLowercase(p, "/appdata/local/microsoft");
+    List<Path> paths = new ArrayList<>();
+    paths.add(context.getUserHome().resolve("AppData/Local/Microsoft/WindowsApps"));
+    paths.add(context.getUserHome().resolve("PortableGit/bin"));
+    SystemPath systemPath = new SystemPath(context, context.getSoftwarePath(), ';', paths);
+    Path bash = Path.of("bash");
+
+    // act
+    Path result = systemPath.findBinary(bash, ignoreWindowsApps);
+
+    // assert
+    assertThat(result).isEqualTo(context.getUserHome().resolve("PortableGit").resolve("bin").resolve("bash"));
+  }
+
+  @Test
+  public void testfindBinaryFindsGraalvmBinaryOnWindowsUsingFilterExcludingJava() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    Predicate<Path> ignoreJava = p -> checkPathToIgnoreLowercase(p, "java/bin");
+    SystemPath systemPath = new SystemPath(context, "", context.getIdeRoot(), context.getSoftwarePath(), ';', new ArrayList<>());
+    Path java = Path.of("java");
+
+    // act
+    Path result = systemPath.findBinary(java, ignoreJava);
+
+    // assert
+    assertThat(result).isEqualTo(context.getSoftwarePath().resolve("graalvm").resolve("bin").resolve("java"));
+  }
+
+  @Test
+  public void testfindBinaryFindsNothingUsingFilterIgnoringEverything() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    Predicate<Path> ignoreEverything = p -> false;
+    SystemPath systemPath = new SystemPath(context, "", context.getIdeRoot(), context.getSoftwarePath(), ';', new ArrayList<>());
+    Path java = Path.of("java");
+
+    // act
+    Path result = systemPath.findBinary(java, ignoreEverything);
+
+    // assert
+    assertThat(result).isEqualTo(java);
+  }
+
+  @Test
+  public void testfindBinaryFindsJavaBinaryOnWindowsUsingFilterExcludingGraalvm() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    Predicate<Path> ignoreGraalvm = p -> checkPathToIgnoreLowercase(p, "graalvm/bin");
+    SystemPath systemPath = new SystemPath(context, "", context.getIdeRoot(), context.getSoftwarePath(), ';', new ArrayList<>());
+    Path java = Path.of("java");
+
+    // act
+    Path result = systemPath.findBinary(java, ignoreGraalvm);
+
+    // assert
+    assertThat(result).isEqualTo(context.getSoftwarePath().resolve("java").resolve("bin").resolve("java"));
+  }
+
+  @Test
+  public void testfindBinaryFindsJavaBinaryWithoutFilter() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    SystemPath systemPath = new SystemPath(context, "", context.getIdeRoot(), context.getSoftwarePath(), ';', new ArrayList<>());
+    Path java = Path.of("java");
+
+    // act
+    Path result = systemPath.findBinary(java);
+
+    // assert
+    assertThat(result).isEqualTo(context.getSoftwarePath().resolve("java").resolve("bin").resolve("java"));
+  }
+
+  @Test
+  public void testfindBinaryFindsNothingWithoutFilter() {
+    // arrange
+    String path = "project/workspaces";
+    IdeTestContext context = newContext("find-binary", path, false);
+    SystemPath systemPath = new SystemPath(context, "", context.getIdeRoot(), context.getSoftwarePath(), ';', new ArrayList<>());
+    Path test = Path.of("test");
+
+    // act
+    Path result = systemPath.findBinary(test);
+
+    // assert
+    assertThat(result).isEqualTo(test);
+  }
+
+  private static boolean checkPathToIgnoreLowercase(Path p, String toIgnore) {
+    String s = p.toAbsolutePath().toString().replace('\\', '/').toLowerCase(Locale.ROOT);
+    return !s.contains(toIgnore);
+  }
 }
