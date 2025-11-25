a728cf80256fb0801efe8246c11e48d25eca6698
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2025-11-25 19:44:50 +0100

    #1166: Fixed XmlMerger test

--- cli/src/test/java/com/devonfw/tools/ide/merge/xml/XmlMergerTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/merge/xml/XmlMergerTest.java
@@ -145,7 +145,7 @@ class XmlMergerTest extends AbstractIdeContextTest {
 
     // assert
     assertThat(context).logAtWarning().hasEntries(
-        "XML merge namespace not found in file " + settingsUpdatePath
+        "XML merge namespace not found in file " + settingsSetupPath
             + ". If you are working in a legacy devonfw-ide project, please set IDE_XML_MERGE_LEGACY_SUPPORT_ENABLED=true to "
             + "proceed correctly.");
   }
