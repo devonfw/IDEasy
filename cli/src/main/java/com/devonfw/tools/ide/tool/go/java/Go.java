4f72ebe161ddd8d7430c31b830de761d27597f72
Marvin Meitzner <marvin.meitzner@capgemini.com>
2026-03-16 08:37:34 +0100

    #1751: create Go commandlet

---
+++ cli/src/main/java/com/devonfw/tools/ide/tool/go/java/Go.java
@@ -0,0 +1,30 @@
+package com.devonfw.tools.ide.tool.go.java;
+
+import java.util.Set;
+
+import com.devonfw.tools.ide.common.Tag;
+import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.tool.LocalToolCommandlet;
+import com.devonfw.tools.ide.tool.ToolCommandlet;
+
+/**
+ * {@link ToolCommandlet} for the Go programming language.
+ */
+public class Go extends LocalToolCommandlet {
+
+  /**
+   * The constructor.
+   *
+   * @param context the {@link IdeContext}.
+   */
+  public Go(IdeContext context) {
+
+    super(context, "go", Set.of(Tag.JAVA, Tag.RUNTIME));
+  }
+
+  @Override
+  public String getToolHelpArguments() {
+
+    return "--help";
+  }
+}
