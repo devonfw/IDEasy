6890fe909d815789041680466b417926d9014522
laim2003 <luk.faber@gmx.de>
2026-03-18 17:32:21 +0100

    added basic UI state test for the IDE selector buttons and workspace selector

--- gui/src/test/java/com/devonfw/ide/gui/AppBaseTest.java
+++ gui/src/test/java/com/devonfw/ide/gui/AppBaseTest.java
@@ -1,25 +1,40 @@
 package com.devonfw.ide.gui;
 
-import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.matcher.base.NodeMatchers;
 
 /**
  * Base Test
  */
 public class AppBaseTest extends ApplicationTest {
 
+  private Button androidStudioOpen, eclipseOpen, intellijOpen, vsCodeOpen;
+  private ComboBox<String> selectedProject, selectedWorkspace;
+
   @Override
   public void start(Stage stage) throws IOException {
 
-    new App().start(stage);
+    Parent root = FXMLLoader.load(App.class.getResource("main-view.fxml"));
+    stage.setScene(new Scene(root));
+    stage.show();
+
+    androidStudioOpen = (Button) root.lookup("#androidStudioOpen");
+    eclipseOpen = (Button) root.lookup("#eclipseOpen");
+    intellijOpen = (Button) root.lookup("#intellijOpen");
+    vsCodeOpen = (Button) root.lookup("#vsCodeOpen");
+    selectedProject = (ComboBox<String>) root.lookup("#selectedProject");
+    selectedWorkspace = (ComboBox<String>) root.lookup("#selectedWorkspace");
   }
 
   /**
@@ -39,17 +54,33 @@ public class AppBaseTest extends ApplicationTest {
       System.setProperty("prism.order", "sw");
       System.setProperty("java.awt.headless", "true");
     }
-
   }
 
   /**
-   * Test if welcome message is shown when GUI is started
+   * This test ensures, that all IDE open buttons are disabled when no project is selected.
    */
   @Test
-  @Disabled
-  public void ensureHelloMessageIsShownOnStartUp() {
+  public void testIdeOpenButtonsDisabledWhenNoProjectSelected() {
 
-    verifyThat("#hellomessage", NodeMatchers.isNotNull());
+    // assert that no project is selected
+    assertThat(selectedProject.getValue()).isNull();
+
+    // assert all IDE open buttons are disabled
+    for (Button button : new Button[] { androidStudioOpen, eclipseOpen, intellijOpen, vsCodeOpen }) {
+      assertThat(button.isDisabled()).as(button.getId() + " button should be disabled when no project has been selected").isTrue();
+    }
   }
 
+  /**
+   * Tests that the workspace ComboBox is disabled when no project is selected.
+   */
+  @Test
+  public void testWorkspaceComboBoxDisabledWhenNoProjectSelected() {
+
+    assertThat(selectedProject.getValue()).isNull();
+
+    assertThat(selectedWorkspace.isDisabled())
+        .as("selectedWorkspace ComboBox should be disabled when no project is selected")
+        .isTrue();
+  }
 }
