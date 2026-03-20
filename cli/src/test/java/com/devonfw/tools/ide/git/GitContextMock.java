e60b6be067c248dd8c39badf093daf158395dc03
MarvMa <marvin.meitzner@capgemini.com>
2026-03-20 14:42:15 +0100

    #1732: keep local changes when running ide update (git pull) (#1742)
    Co-authored-by: Jörg Hohwiller <hohwille@users.noreply.github.com>

--- cli/src/test/java/com/devonfw/tools/ide/git/GitContextMock.java
+++ cli/src/test/java/com/devonfw/tools/ide/git/GitContextMock.java
@@ -20,6 +20,16 @@ public class GitContextMock implements GitContext {
   }
 
   @Override
+  public void pullSafelyWithStash(Path repository) {
+
+  }
+
+  @Override
+  public boolean hasUntrackedFiles(Path repository) {
+    return false;
+  }
+
+  @Override
   public void pullOrClone(GitUrl gitUrl, Path repository) {
 
   }
