a109b88655d22891ca3a791b0de00a9e612b076f
laim2003 <luk.faber@gmx.de>
2026-03-23 16:48:31 +0100

    #1724: -implemented basic java -jar execution

--- cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
@@ -1,5 +1,6 @@
 package com.devonfw.tools.ide.tool.gui;
 
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Set;
 
@@ -8,11 +9,12 @@ import org.slf4j.LoggerFactory;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.process.ProcessContext;
 import com.devonfw.tools.ide.process.ProcessMode;
-import com.devonfw.tools.ide.tool.mvn.Mvn;
+import com.devonfw.tools.ide.tool.ToolInstallRequest;
+import com.devonfw.tools.ide.tool.java.Java;
 import com.devonfw.tools.ide.tool.mvn.MvnArtifact;
 import com.devonfw.tools.ide.tool.mvn.MvnBasedLocalToolCommandlet;
+import com.devonfw.tools.ide.version.VersionRange;
 
 /**
  * {@link MvnBasedLocalToolCommandlet} to launch the IDEasy GUI.
@@ -21,10 +23,9 @@ public class Gui extends MvnBasedLocalToolCommandlet {
 
   private static final Logger LOG = LoggerFactory.getLogger(Gui.class);
 
-  private final ProcessContext pc = this.context.newProcess();
+  private static final MvnArtifact ARTIFACT = MvnArtifact.ofIdeasy("ide-gui", "*!", "jar", null);
 
-  private final Mvn mvn = this.context.getCommandletManager().getCommandlet(Mvn.class);
-  private static final MvnArtifact ARTIFACT = MvnArtifact.ofIdeasy("ide-gui", "*!", "tar.gz", "${os}-${arch}");
+  private Java java;
 
   /**
    * @param context the {@link IdeContext}.
@@ -35,16 +36,30 @@ public class Gui extends MvnBasedLocalToolCommandlet {
   }
 
   @Override
+  protected void installToolDependencies(ToolInstallRequest request) {
+
+    super.installToolDependencies(request);
+
+    LOG.debug("Installing ide-gui dependencies: {}", request.getToolPath());
+
+    java = this.context.getCommandletManager().getCommandlet(Java.class);
+    java.installAsDependency(VersionRange.of("25"), request);
+  }
+
+  @Override
   protected void doRun() {
+
     install(true);
 
     LOG.debug("Starting GUI via commandlet");
 
+    Path jarPath = getToolPath().resolve(ARTIFACT.getFilename());
+
     List<String> args = List.of(
-        "exec:java",
-        "-Dexec.mainClass=com.devonfw.ide.gui.AppLauncher"
+        "-jar",
+        jarPath.toAbsolutePath().toString()
     );
 
-    mvn.runTool(pc, ProcessMode.DEFAULT, args);
+    java.runTool(context.newProcess(), ProcessMode.DEFAULT, args);
   }
 }
