5ea616972ebb2109ff96c10320010e6260cbc5b4
laim2003 <luk.faber@gmx.de>
2026-03-25 17:18:43 +0100

    - Refactored the MainController.java to use Dependency Injection - AppBaseTest.java now generates mock project directory (in order for the test to work on the CI)

--- gui/src/test/java/com/devonfw/ide/gui/AppBaseTest.java
+++ gui/src/test/java/com/devonfw/ide/gui/AppBaseTest.java
@@ -5,6 +5,8 @@ import static org.testfx.util.WaitForAsyncUtils.waitForFxEvents;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Arrays;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,7 +15,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testfx.framework.junit5.ApplicationTest;
 
 /**
@@ -21,8 +27,13 @@ import org.testfx.framework.junit5.ApplicationTest;
  */
 public class AppBaseTest extends ApplicationTest {
 
+  private static Logger LOGGER = LoggerFactory.getLogger(ApplicationTest.class);
+
   private Button androidStudioOpen, eclipseOpen, intellijOpen, vsCodeOpen;
   private ComboBox<String> selectedProject, selectedWorkspace;
+
+  @TempDir
+  private static Path temporayProjectDirectoryPath;
 
   //setting up for headless testing
   static {
@@ -40,7 +51,9 @@ public class AppBaseTest extends ApplicationTest {
     URL mainViewUrl = getClass().getResource("main-view.fxml");
     assertThat(mainViewUrl).as("Cannot resolve main UI FXML resource!").isNotNull();
 
-    Parent root = FXMLLoader.load(mainViewUrl);
+    FXMLLoader fxmlLoader = new FXMLLoader(mainViewUrl);
+    fxmlLoader.setController(new MainController(temporayProjectDirectoryPath.toString()));
+    Parent root = fxmlLoader.load();
     stage.setScene(new Scene(root));
     stage.requestFocus(); //sometimes needed for headless setup to work
     stage.show();
@@ -51,6 +64,30 @@ public class AppBaseTest extends ApplicationTest {
     vsCodeOpen = (Button) root.lookup("#vsCodeOpen");
     selectedProject = (ComboBox<String>) root.lookup("#selectedProject");
     selectedWorkspace = (ComboBox<String>) root.lookup("#selectedWorkspace");
+  }
+
+  /**
+   * Generate a temporary project directories in order to be able to test on any device (including GitHub CI). This is required for the {@link MainController}
+   * to work in the test context. Generates a structure like this: /project-[0..6]/workspaces/main
+   */
+  @BeforeAll
+  public static void generateProjectFolder() {
+
+    LOGGER.debug("tempDir: {}", temporayProjectDirectoryPath);
+    for (int i = 0; i <= 5; i++) {
+      String projectFolderName = "project-" + i;
+      assertThat(temporayProjectDirectoryPath.resolve(projectFolderName).toFile().mkdir())
+          .as("Unable to create mock project directory for mock project " + i)
+          .isTrue();
+      assertThat(temporayProjectDirectoryPath.resolve(projectFolderName).resolve("workspaces").toFile().mkdir())
+          .as("Unable to create mock workspaces directory for mock project " + i)
+          .isTrue();
+      assertThat(temporayProjectDirectoryPath.resolve(projectFolderName).resolve("workspaces").resolve("main").toFile().mkdir())
+          .as(
+              "Unable to create mock main workspace directory for mock project " + i)
+          .isTrue();
+    }
+    LOGGER.debug("project folders: {}", Arrays.toString(temporayProjectDirectoryPath.toFile().list()));
   }
 
   /**
@@ -75,7 +112,7 @@ public class AppBaseTest extends ApplicationTest {
   public void testIdeOpenButtonsEnabledWhenProjectSelected() {
 
     // assert that a project is selected
-    Platform.runLater(() -> selectedProject.getSelectionModel().select("test"));
+    Platform.runLater(() -> selectedProject.getSelectionModel().select("project-1"));
     waitForFxEvents();
 
     // assert all IDE open buttons are disabled
@@ -104,7 +141,7 @@ public class AppBaseTest extends ApplicationTest {
   public void testWorkspaceComboboxEnabledEnabledWhenProjectSelected() {
 
     // assert that a project is selected
-    Platform.runLater(() -> selectedProject.getSelectionModel().select("test"));
+    Platform.runLater(() -> selectedProject.getSelectionModel().select("project-1"));
     waitForFxEvents();
 
     // assert all IDE open buttons are disabled
