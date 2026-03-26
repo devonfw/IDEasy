b6f79cf18c9e7d2522b0499afebd2ae2bf3b19aa
laim2003 <luk.faber@gmx.de>
2026-03-26 19:52:54 +0100

    - #1724: Switched Gui.java to use LocalToolCommandlet - Implemented gui-launcher module to enable command line execution of GUI

--- cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
@@ -1,72 +1,58 @@
 package com.devonfw.tools.ide.tool.gui;
 
-import java.nio.file.Path;
 import java.util.List;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.devonfw.tools.ide.common.Tag;
+import com.devonfw.tools.ide.commandlet.Commandlet;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.ProcessMode;
-import com.devonfw.tools.ide.tool.ToolInstallRequest;
-import com.devonfw.tools.ide.tool.java.Java;
-import com.devonfw.tools.ide.tool.mvn.MvnArtifact;
+import com.devonfw.tools.ide.tool.mvn.Mvn;
 import com.devonfw.tools.ide.tool.mvn.MvnBasedLocalToolCommandlet;
-import com.devonfw.tools.ide.version.VersionRange;
+import com.devonfw.tools.ide.variable.IdeVariables;
 
 /**
  * {@link MvnBasedLocalToolCommandlet} to launch the IDEasy GUI.
  */
-public class Gui extends MvnBasedLocalToolCommandlet {
+public class Gui extends Commandlet {
 
   private static final Logger LOG = LoggerFactory.getLogger(Gui.class);
 
-  private static final MvnArtifact ARTIFACT = new MvnArtifact("ide-gui", "2026.04.001", "jar", "");
-
-  private Java java;
+  //TODO: implement constant in sync with maven property gui.relative_pom_path
+  private static final String POM_PATH = "gui-execution";
 
   /**
    * @param context the {@link IdeContext}.
    */
   public Gui(IdeContext context) {
 
-    super(context, "gui", ARTIFACT, Set.of(Tag.GUI));
+    super(context);
+    addKeyword("gui");
   }
+
 
   @Override
-  protected void installToolDependencies(ToolInstallRequest request) {
+  public String getName() {
 
-    super.installToolDependencies(request);
-    LOG.info(ARTIFACT.getDownloadUrl());
-    LOG.info("Installing ide-gui dependencies: {}", request.getRequested().getVersion().toString());
-
-    java = this.context.getCommandletManager().getCommandlet(Java.class);
-    java.installAsDependency(VersionRange.of("25"), request);
+    return "gui";
   }
 
-  @Override
-  protected boolean isExtract() {
-
-    //return false to avoid extracting the downloaded GUI jar (as we will run the jar in doRun())
-    return false;
-  }
 
   @Override
   protected void doRun() {
 
-    install(true);
-
     LOG.debug("Starting GUI via commandlet");
 
-    Path jarPath = getToolPath().resolve(ARTIFACT.getFilename());
+    Mvn mvn = context.getCommandletManager().getCommandlet(Mvn.class);
 
     List<String> args = List.of(
-        "-jar",
-        jarPath.toAbsolutePath().toString()
+        "-f",
+        IdeVariables.IDE_ROOT.get(context).toString() + "/_ide/installation/" + POM_PATH + "/pom.xml",
+        "exec:java",
+        "-Dexec.mainClass=com.devonfw.tools.ide.gui.AppLauncher"
     );
 
-    java.runTool(context.newProcess(), ProcessMode.DEFAULT, args);
+    mvn.runTool(context.newProcess(), ProcessMode.DEFAULT, args);
   }
 }
