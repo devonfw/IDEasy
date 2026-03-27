9a842bd45e59bbd8298a3d4c81afbf59fba0bfcc
jakozian <jannis.kozian@capgemini.com>
2026-03-27 13:07:24 +0100

    #1771: Change MvnUrlUpdater to extend GithubUrlReleaseUpdater

--- url-updater/src/main/java/com/devonfw/tools/ide/url/tool/mvn/MvnUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/tool/mvn/MvnUrlUpdater.java
@@ -1,41 +1,23 @@
 package com.devonfw.tools.ide.url.tool.mvn;
 
-import com.devonfw.tools.ide.url.updater.MavenBasedUrlUpdater;
+import java.util.Locale;
+
+import com.devonfw.tools.ide.url.model.folder.UrlVersion;
+import com.devonfw.tools.ide.url.updater.GithubUrlReleaseUpdater;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
 /**
- * {@link MavenBasedUrlUpdater} for mvn (maven).
+ * {@link GithubUrlReleaseUpdater} for mvn (maven).
  */
-public class MvnUrlUpdater extends MavenBasedUrlUpdater {
+public class MvnUrlUpdater extends GithubUrlReleaseUpdater {
 
-  public static final VersionIdentifier MIN_VERSION = VersionIdentifier.of("3.0.4");
-
-  @Override
-  protected String getMavenGroupIdPath() {
-
-    return "org/apache/maven";
-  }
-
-  @Override
-  protected String getMavenArtifcatId() {
-
-    return "maven-core";
-  }
+  private static final VersionIdentifier MIN_VERSION = VersionIdentifier.of("3.0.4");
+  private static final VersionIdentifier MAVEN_4_IDENTIFIER = VersionIdentifier.of("4.0.0");
 
   @Override
   public String getTool() {
 
     return "mvn";
-  }
-
-  @Override
-  protected boolean isValidVersion(String version) {
-
-    VersionIdentifier artifactVersion = VersionIdentifier.of(version);
-    if (artifactVersion != null) {
-      return artifactVersion.isGreaterOrEqual(MIN_VERSION);
-    }
-    return true;
   }
 
   @Override
@@ -48,5 +30,63 @@ public class MvnUrlUpdater extends MavenBasedUrlUpdater {
   public String getCpeProduct() {
 
     return "maven";
+  }
+
+  @Override
+  protected String getGithubOrganization() {
+    return "apache";
+  }
+
+  @Override
+  protected String getGithubRepository() {
+
+    return "maven";
+  }
+
+  @Override
+  protected String getDownloadBaseUrl() {
+
+    return "https://archive.apache.org";
+  }
+
+  @Override
+  public String mapVersion(String version) {
+
+    // Workaround to get Release Candidates.
+    // A better solution would be a flag or a map to signal which version are wanted.
+    String vLower = version.toLowerCase(Locale.ROOT);
+    if (vLower.contains("-rc")) {
+      return version;
+    } else {
+      return super.mapVersion(version);
+    }
+  }
+
+  @Override
+  protected void addVersion(UrlVersion urlVersion) {
+
+    VersionIdentifier versionIdentifier = urlVersion.getVersionIdentifier();
+    if (versionIdentifier.compareVersion(MIN_VERSION).isGreater()) {
+      String version = mapVersion(urlVersion.getName());
+
+      // This is just a workaround because apache archive sorts its versions into maven-x folders.
+      // A better solution would be to extract the major version of the VersionIdentifier
+      // and put that into the String Formatter but no such method exists yet.
+      String majorFolder = versionIdentifier.compareVersion(MAVEN_4_IDENTIFIER).isGreater()
+          ? "maven-3"
+          : "maven-4";
+
+      String downloadUrl = String.format(
+          "%s/dist/maven/%s/%s/binaries/%s-%s-%s-bin.tar.gz",
+          getDownloadBaseUrl(),
+          majorFolder,
+          version,
+          getGithubOrganization(),
+          getGithubRepository(),
+          version
+      );
+
+      doAddVersion(urlVersion, downloadUrl);
+    }
   }
 }
