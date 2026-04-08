a8a69d7b662f9cd158f58c46201c6e34c20106c3
ShodiBoy1 <sobirovsh@yahoo.com>
2026-04-09 00:15:19 +0200

    revert fix

--- cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/io/FileAccessImpl.java
@@ -706,30 +706,12 @@ public class FileAccessImpl extends HttpDownloader implements FileAccess {
     postExtractHook(postExtractHook, properInstallDir);
     move(properInstallDir, targetDir);
     delete(tmpDir);
-    removeQuarantineAttribute(targetDir);
   }
 
   private void postExtractHook(Consumer<Path> postExtractHook, Path properInstallDir) {
 
     if (postExtractHook != null) {
       postExtractHook.accept(properInstallDir);
-    }
-  }
-
-  private void removeQuarantineAttribute(Path path) {
-
-    if (!this.context.getSystemInfo().isMac()) {
-      return;
-    }
-    LOG.debug("Removing macOS quarantine attribute from {}", path);
-    try {
-      ProcessResult result = this.context.newProcess().executable("xattr")
-          .addArgs("-r", "-d", "com.apple.quarantine", path).run(ProcessMode.DEFAULT_SILENT);
-      if (!result.isSuccessful()) {
-        LOG.trace("Could not remove quarantine attribute from {} (may not have been set)", path);
-      }
-    } catch (Exception e) {
-      LOG.trace("Could not remove quarantine attribute from {} - xattr not available: {}", path, e.getMessage());
     }
   }
 
