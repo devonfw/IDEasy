59f50975f041043aba38bcf43c02e01284c457c0
Lukas Faber <luk.faber@gmx.de>
2026-04-09 19:58:53 +0200

    #1724: added java version dependency (for some reason, these changes got lost. Probably a shelving issue.) - fixed issues with MainClass path

--- cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/gui/Gui.java
@@ -1,5 +1,6 @@
 package com.devonfw.tools.ide.tool.gui;
 
+import java.nio.file.Path;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,18 +8,27 @@ import org.slf4j.LoggerFactory;
 
 import com.devonfw.tools.ide.commandlet.Commandlet;
 import com.devonfw.tools.ide.context.IdeContext;
+import com.devonfw.tools.ide.process.ProcessContext;
+import com.devonfw.tools.ide.process.ProcessContextImpl;
 import com.devonfw.tools.ide.process.ProcessMode;
+import com.devonfw.tools.ide.tool.ToolEdition;
+import com.devonfw.tools.ide.tool.ToolEditionAndVersion;
+import com.devonfw.tools.ide.tool.ToolInstallRequest;
+import com.devonfw.tools.ide.tool.java.Java;
 import com.devonfw.tools.ide.tool.mvn.Mvn;
-import com.devonfw.tools.ide.tool.mvn.MvnBasedLocalToolCommandlet;
 import com.devonfw.tools.ide.variable.IdeVariables;
+import com.devonfw.tools.ide.version.BoundaryType;
+import com.devonfw.tools.ide.version.VersionIdentifier;
+import com.devonfw.tools.ide.version.VersionRange;
 
 /**
- * {@link MvnBasedLocalToolCommandlet} to launch the IDEasy GUI.
+ * {@link Commandlet} to launch the IDEasy GUI.
  */
 public class Gui extends Commandlet {
 
   private static final Logger LOG = LoggerFactory.getLogger(Gui.class);
 
+  //TODO: implement constant in sync with maven property gui.relative_pom_path
   private static final String POM_PATH = "gui-execution";
 
   /**
@@ -39,17 +49,38 @@ public class Gui extends Commandlet {
   @Override
   protected void doRun() {
 
+    ProcessContext processContext = new ProcessContextImpl(this.context);
+
+    ToolInstallRequest toolInstallRequest = new ToolInstallRequest(true);
+    toolInstallRequest.setProcessContext(processContext);
+    toolInstallRequest.setRequested(
+        new ToolEditionAndVersion(
+            new ToolEdition("java", "25"),
+            VersionRange.of(VersionIdentifier.of("25"), VersionIdentifier.of(""), BoundaryType.RIGHT_OPEN)
+        )
+    );
+
+    Java java = this.context.getCommandletManager().getCommandlet(Java.class);
+    java.installAsDependency(
+        (VersionRange) toolInstallRequest.getRequested().getVersion(),
+        toolInstallRequest
+    );
+
     LOG.debug("Starting GUI via commandlet");
 
     Mvn mvn = context.getCommandletManager().getCommandlet(Mvn.class);
 
     List<String> args = List.of(
         "-f",
-        IdeVariables.IDE_ROOT.get(context).toString() + "/_ide/installation/" + POM_PATH + "/pom.xml",
+        Path.of(IdeVariables.IDE_ROOT.get(context).toString(), "_ide", "installation", POM_PATH, "pom.xml").toString(),
         "exec:java",
         "-Dexec.mainClass=com.devonfw.ide.gui.AppLauncher"
     );
 
-    mvn.runTool(context.newProcess(), ProcessMode.DEFAULT, args);
+    try {
+      mvn.runTool(processContext, ProcessMode.DEFAULT, args);
+    } catch (IllegalStateException e) {
+      LOG.error("ERROR WHILE LAUNCHING GUI: Recommended to check if POM file exists in _ide/installation/gui-execution/pom.xml", e);
+    }
   }
 }
