7567c8fefd454d3d9c92707f454d956d36081309
MarvMa <marvin.meitzner@capgemini.com>
2026-04-15 15:18:00 +0200

    #1174: requested changes from pr

--- url-updater/src/main/java/com/devonfw/tools/ide/url/UpdateInitiator.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/UpdateInitiator.java
@@ -20,21 +20,21 @@ public class UpdateInitiator {
 
   /**
    * @param args the command-line arguments. arg[0] points to the {@code ide-urls} repository. arg[1] defines a timeout for GitHub actions in Duration
-   *     string format and arg[2] can be used to specify a single tool to update instead of all tools. The timeout is used to prevent the GitHub action from
-   *     running into a timeout error due to too long execution time.
+   *     string format and arg[2] can be used to specify a single tool to update instead of all tools, either by toolname (e.g. java) or using the Classname of
+   *     the Updater (e.g. JavaAzulUrlUpdater). The timeout is used to prevent the GitHub action from running into a timeout error due to too long execution
+   *     time.
    */
   public static void main(String[] args) {
 
     if (args.length == 0) {
       logger.error("Error: Missing path to repository as well as missing timeout as command line arguments.");
-      logger.error("Usage: java UpdateInitiator <path_to_repository> <duration_string_format>");
+      logger.error("Usage: java UpdateInitiator <path_to_repository> <duration_string_format> <tool_to_test|updater_class_name>");
       System.exit(1);
     }
 
     String pathToRepo = args[0];
     Instant expirationTime = null;
     String selectedTool = null;
-    String selectedEdition = null;
 
     if (args.length < 2) {
       logger.warn("Timeout was not set, setting timeout to infinite instead.");
@@ -49,9 +49,6 @@ public class UpdateInitiator {
       }
       if (args.length > 2) {
         selectedTool = args[2];
-      }
-      if (args.length > 3) {
-        selectedEdition = args[3];
       }
     }
 
@@ -68,9 +65,6 @@ public class UpdateInitiator {
     if (selectedTool == null) {
       updateManager.updateAll();
     } else {
-      if (selectedEdition != null) {
-        updateManager.update(selectedTool, selectedEdition);
-      }
       updateManager.update(selectedTool);
     }
 
