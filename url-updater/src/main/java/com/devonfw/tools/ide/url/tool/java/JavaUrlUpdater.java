6035f8361e2f3f1f7dd38b6b4563c11c903968c3
MarvMa <marvin.meitzner@capgemini.com>
2026-04-20 09:48:25 +0200

    #1775: update implementation and update cpes for azure

--- url-updater/src/main/java/com/devonfw/tools/ide/url/tool/java/JavaUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/tool/java/JavaUrlUpdater.java
@@ -112,7 +112,7 @@ public class JavaUrlUpdater extends JsonUrlUpdater<JavaJsonObject, JavaJsonVersi
   @Override
   protected void initCpe(CpeRegistry cpe) {
     cpe.addVendor("oracle")
-        .addVendor("jdk")
+        .addProduct("jdk")
         .addProduct("java_se");
   }
 }
