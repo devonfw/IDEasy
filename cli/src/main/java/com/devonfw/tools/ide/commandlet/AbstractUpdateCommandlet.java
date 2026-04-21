fc45ce772deb5242b2db3b635cd8b6e442d9345a
Henok Lachmann <henok.lachmann@capgemini.com>
2026-04-21 13:58:55 +0200

    Add missing symlink check

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/AbstractUpdateCommandlet.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/AbstractUpdateCommandlet.java
@@ -175,7 +175,7 @@ public abstract class AbstractUpdateCommandlet extends Commandlet {
     Path settingsPath = this.context.getSettingsPath();
     GitContext gitContext = this.context.getGitContext();
     // here we do not use pullOrClone to prevent asking a pointless question for repository URL...
-    if (Files.isDirectory(settingsPath) && this.context.getGitContext().isGitRepo(settingsPath)) {
+    if (Files.isDirectory(settingsPath) && this.context.getGitContext().isGitRepo(settingsPath) || this.context.isSettingsRepositorySymlinkOrJunction()) {
       if (this.context.isForcePull() || this.context.isForceMode()) {
         if (gitContext.hasUntrackedFiles(settingsPath)) {
           gitContext.pullSafelyWithStash(settingsPath);
