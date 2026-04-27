f63af46131bd1db0e260574aacfc928da50564d2
Henok Lachmann <henok.lachmann@capgemini.com>
2026-04-27 11:01:19 +0200

    Change CompletionCandidate test to entries list

--- cli/src/main/java/com/devonfw/tools/ide/completion/CompletionCandidate.java
+++ cli/src/main/java/com/devonfw/tools/ide/completion/CompletionCandidate.java
@@ -1,16 +1,24 @@
 package com.devonfw.tools.ide.completion;
 
+import java.util.List;
+
 /**
  * Candidate for auto-completion.
  *
  * @param text the text to suggest (CLI argument value).
  * @param description the description of the candidate.
  */
-public record CompletionCandidate(String text, String description) implements Comparable<CompletionCandidate> {
+public record CompletionCandidate(List<String> entries,
+                                  String description,
+                                  boolean complete) implements Comparable<CompletionCandidate> {
 
   @Override
   public int compareTo(CompletionCandidate o) {
 
-    return this.text.compareTo(o.text);
+    return this.text().compareTo(o.text());
+  }
+
+  public String text() {
+    return String.join(" ", this.entries);
   }
 }
