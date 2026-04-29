d1230886657c27f19b59fabe9aa225675b7a8985
jakozian <jannis.kozian@capgemini.com>
2026-04-29 11:20:58 +0200

    #1869: Add interface methods to WindowsHelperImpl

--- cli/src/main/java/com/devonfw/tools/ide/os/WindowsHelperImpl.java
+++ cli/src/main/java/com/devonfw/tools/ide/os/WindowsHelperImpl.java
@@ -21,6 +21,13 @@ public class WindowsHelperImpl implements WindowsHelper {
   /** Registry key for the users environment variables. */
   public static final String HKCU_ENVIRONMENT = "HKCU\\Environment";
 
+  /** Common Windows registry base paths containing (uninstall) information for installed applications (system-wide and per-user). */
+  private static final String[] REGISTRY_BASE_PATHS = {
+      "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+      "HKLM\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall",
+      "HKLM\\SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+  };
+
   private final IdeContext context;
 
   /**
@@ -67,7 +74,44 @@ public class WindowsHelperImpl implements WindowsHelper {
     return null;
   }
 
-  private List<String> runReg(String... args) {
+  @Override
+  public String getDisplayNameFromRegistry(String appName) {
+    return getRegistryValueBySearch(appName, "DisplayName");
+  }
+
+  @Override
+  public String getDisplayIconFromRegistry(String appName) {
+    return getRegistryValueBySearch(appName, "DisplayIcon");
+  }
+
+  @Override
+  public String getUninstallStringFromRegistry(String appName) {
+    return getRegistryValueBySearch(appName, "UninstallString");
+  }
+
+  @Override
+  public String getInstallLocationFromRegistry(String appName) {
+    return getRegistryValueBySearch(appName, "InstallLocation");
+  }
+
+  private String getRegistryValueBySearch(String appName, String key) {
+
+    for (String registryBasePath : REGISTRY_BASE_PATHS) {
+      List<String> out = runReg("query", registryBasePath, "/s", "/f", appName);
+      if (out != null) {
+        return retrieveRegString(key, out);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Executes a Windows registry command and returns its output.
+   *
+   * @param args the registry command arguments.
+   * @return the command output lines, or {@code null} if the command failed
+   */
+  protected List<String> runReg(String... args) {
     ProcessResult result = this.context.newProcess()
         .errorHandling(ProcessErrorHandling.LOG_WARNING)
         .executable("reg")
