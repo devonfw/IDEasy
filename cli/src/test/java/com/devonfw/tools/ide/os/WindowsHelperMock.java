7c8f432c3b682320f25ed86bef2d88f40e5a15c6
jakozian <jannis.kozian@capgemini.com>
2026-04-29 11:21:33 +0200

    #1869: Add interface methods to WindowsHelperMock

--- cli/src/test/java/com/devonfw/tools/ide/os/WindowsHelperMock.java
+++ cli/src/test/java/com/devonfw/tools/ide/os/WindowsHelperMock.java
@@ -1,5 +1,6 @@
 package com.devonfw.tools.ide.os;
 
+import java.util.List;
 import java.util.Properties;
 
 import com.devonfw.tools.ide.context.IdeContext;
@@ -10,6 +11,18 @@ import com.devonfw.tools.ide.context.IdeContext;
 public class WindowsHelperMock extends WindowsHelperImpl {
 
   private final Properties env;
+
+  private static final String MOCK_APP_NAME = "TestApp";
+
+  private static final String MOCK_DISPLAY_NAME = "Test Application";
+
+  private static final String MOCK_INSTALL_LOCATION = "C:\\Program Files\\TestApp";
+
+  private static final String MOCK_DISPLAY_ICON =
+      "C:\\Program Files\\TestApp\\testapp.exe,0";
+  private static final String MOCK_UNINSTALL_STRING =
+      "\"C:\\Program Files\\TestApp\\uninstall.exe\"";
+
 
   /**
    * The constructor.
@@ -42,6 +55,26 @@ public class WindowsHelperMock extends WindowsHelperImpl {
   }
 
   @Override
+  public String getDisplayNameFromRegistry(String appName) {
+    return matchesApp(appName) ? MOCK_DISPLAY_NAME : null;
+  }
+
+  @Override
+  public String getDisplayIconFromRegistry(String appName) {
+    return matchesApp(appName) ? MOCK_DISPLAY_ICON : null;
+  }
+
+  @Override
+  public String getUninstallStringFromRegistry(String appName) {
+    return matchesApp(appName) ? MOCK_UNINSTALL_STRING : null;
+  }
+
+  @Override
+  public String getInstallLocationFromRegistry(String appName) {
+    return matchesApp(appName) ? MOCK_INSTALL_LOCATION : null;
+  }
+
+  @Override
   public String getRegistryValue(String path, String key) {
 
     if (WindowsHelperImpl.HKCU_ENVIRONMENT.equals(path)) {
@@ -50,5 +83,36 @@ public class WindowsHelperMock extends WindowsHelperImpl {
       return super.getRegistryValue(path, key);
     }
     return null;
+  }
+
+  private boolean matchesApp(String appName) {
+    return appName != null
+        && appName.equalsIgnoreCase(MOCK_APP_NAME);
+  }
+
+  @Override
+  protected List<String> runReg(String... args) {
+
+    // simulate reg.exe filtering: "/f <appName>"
+    String searchValue = null;
+    for (int i = 0; i < args.length - 1; i++) {
+      if ("/f".equalsIgnoreCase(args[i])) {
+        searchValue = args[i + 1];
+        break;
+      }
+    }
+
+    // Only return output if searched app matches
+    if (!"TestApp".equalsIgnoreCase(searchValue)) {
+      return List.of(); // same behavior as reg.exe: no results
+    }
+
+    return List.of(
+        "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\TestApp",
+        "    DisplayName    REG_SZ    Test Application",
+        "    DisplayIcon    REG_SZ    C:\\Program Files\\TestApp\\testapp.exe,0",
+        "    InstallLocation    REG_SZ    C:\\Program Files\\TestApp",
+        "    UninstallString    REG_SZ    \"C:\\Program Files\\TestApp\\uninstall.exe\""
+    );
   }
 }
