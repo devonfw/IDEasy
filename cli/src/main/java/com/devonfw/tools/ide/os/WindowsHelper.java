53fff3f7d11a73096caa327a11240c6882d44214
jakozian <jannis.kozian@capgemini.com>
2026-04-29 11:15:58 +0200

    #1869: Extend WindowsHelper interface with methods to retrieve entries from registry

--- cli/src/main/java/com/devonfw/tools/ide/os/WindowsHelper.java
+++ cli/src/main/java/com/devonfw/tools/ide/os/WindowsHelper.java
@@ -33,6 +33,30 @@ public interface WindowsHelper {
   String getRegistryValue(String path, String key);
 
   /**
+   * @param appName the application name to search for in the Windows registry.
+   * @return the DisplayName entry if the application is found in the Windows registry or {@code null} if nothing was found.
+   */
+  String getDisplayNameFromRegistry(String appName);
+
+  /**
+   * @param appName the application name to search for in the Windows registry.
+   * @return the DisplayIcon entry if the application is found in the Windows registry or {@code null} if nothing was found.
+   */
+  String getDisplayIconFromRegistry(String appName);
+
+  /**
+   * @param appName the application name to search for in the Windows registry.
+   * @return the UninstallString entry if the application is found in the Windows registry or {@code null} if nothing was found.
+   */
+  String getUninstallStringFromRegistry(String appName);
+
+  /**
+   * @param appName the application name to search for in the Windows registry.
+   * @return the InstallLocation entry if the application is found in the Windows registry or {@code null} if nothing was found.
+   */
+  String getInstallLocationFromRegistry(String appName);
+
+  /**
    * @param context the {@link IdeContext}.
    * @return the instance of {@link WindowsHelper}.
    */
