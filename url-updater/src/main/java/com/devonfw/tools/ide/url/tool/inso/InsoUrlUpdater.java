2e6c6bf668b9012956044a6669cba716d82232e5
KarimALotfy <karim.a.lotfy@capgemini.com>
2026-05-12 10:31:17 +0200

    #1905 : Refactored InsoUrlUpdater to use GithubUrlReleaseUpdater instead of GithubUrlTagUpdater

--- url-updater/src/main/java/com/devonfw/tools/ide/url/tool/inso/InsoUrlUpdater.java
+++ url-updater/src/main/java/com/devonfw/tools/ide/url/tool/inso/InsoUrlUpdater.java
@@ -1,23 +1,25 @@
 package com.devonfw.tools.ide.url.tool.inso;
 
 import com.devonfw.tools.ide.url.model.folder.UrlVersion;
+import com.devonfw.tools.ide.url.updater.GithubUrlReleaseUpdater;
 import com.devonfw.tools.ide.url.updater.GithubUrlTagUpdater;
 import com.devonfw.tools.ide.version.VersionIdentifier;
 
 /**
  * {@link GithubUrlTagUpdater} for GitHub Insomnia CLI.
  * <p>
- * Follows the official installation structure from GitHub's insomnia repository: <a href="https://github.com/Kong/insomnia">https://github.com/Kong/insomnia</a>.
+ * Follows the official installation structure from GitHub's insomnia repository: <a
+ * href="https://github.com/Kong/insomnia">https://github.com/Kong/insomnia</a>.
  * <p>
- * Download URL pattern: https://github.com/Kong/insomnia/releases/download/core@${version}/inso-${os}-${version}.${ext}
- * Examples:
- * - <a href="https://github.com/Kong/insomnia/releases/download/core@12.5.0/inso-linux-x64-12.5.0.tar.xz">github.com/Kong/insomnia/releases/download/core@12.5.0/inso-linux-x64-12.5.0.tar.xz</a>
- * - <a href="https://github.com/Kong/insomnia/releases/download/core@12.5.0-beta.0/inso-windows-12.5.0-beta.0.zip">github.com/Kong/insomnia/releases/download/core@12.5.0-beta.0/inso-windows-12.5.0-beta.0.zip</a>
+ * Download URL pattern: https://github.com/Kong/insomnia/releases/download/core@${version}/inso-${os}-${version}.${ext} Examples: - <a
+ * href="https://github.com/Kong/insomnia/releases/download/core@12.5.0/inso-linux-x64-12.5.0.tar.xz">github.com/Kong/insomnia/releases/download/core@12.5.0/inso-linux-x64-12.5.0.tar.xz</a>
+ * - <a
+ * href="https://github.com/Kong/insomnia/releases/download/core@12.5.0-beta.0/inso-windows-12.5.0-beta.0.zip">github.com/Kong/insomnia/releases/download/core@12.5.0-beta.0/inso-windows-12.5.0-beta.0.zip</a>
  */
-public class InsoUrlUpdater extends GithubUrlTagUpdater {
+public class InsoUrlUpdater extends GithubUrlReleaseUpdater {
 
   private static final String BASE_URL = "https://github.com";
-   private static final VersionIdentifier MIN_INSO_VID = VersionIdentifier.of("11.5.0");
+  private static final VersionIdentifier MIN_INSO_VID = VersionIdentifier.of("11.5.0");
 
   @Override
   protected String getGithubOrganization() {
