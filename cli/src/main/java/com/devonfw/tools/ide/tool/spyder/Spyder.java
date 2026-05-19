b416e1fcfdafae816862f5eb576d3ab5ddadd3d6
tineff96 <nefftim96@gmail.com>
2026-05-19 14:28:04 +0200

    #1946 Support PipBasedCommandlet in PipRepository

--- cli/src/main/java/com/devonfw/tools/ide/tool/spyder/Spyder.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/spyder/Spyder.java
@@ -4,24 +4,13 @@ import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
-import com.devonfw.tools.ide.tool.PackageManagerRequest;
-import com.devonfw.tools.ide.tool.ToolInstallRequest;
-import com.devonfw.tools.ide.tool.ToolInstallation;
 import com.devonfw.tools.ide.tool.pip.PipBasedCommandlet;
+
 
 public class Spyder extends PipBasedCommandlet {
 
   public Spyder(IdeContext context) {
     super(context, "spyder", Set.of(Tag.SPYDER, Tag.IDE));
-  }
-
-  @Override
-  public ToolInstallation install(ToolInstallRequest request) {
-
-    // Spyder is a pip package -> delegate installation to pip
-    runPackageManager(new PackageManagerRequest("install", getPackageName()));
-
-    return null;
   }
 
   @Override
