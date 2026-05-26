403c029d46eaf41ed5a443aca2d1c4940efb8c9b
Laert Llaveshi <l.llaveshi@tum.de>
2026-05-26 15:48:32 +0200

    #1719: Fixing MSVC commandlet

--- cli/src/main/java/com/devonfw/tools/ide/tool/msvc/Msvc.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/msvc/Msvc.java
@@ -6,10 +6,8 @@ import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.ProcessErrorHandling;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 import com.devonfw.tools.ide.tool.ToolInstallRequest;
-import com.devonfw.tools.ide.version.VersionIdentifier;
 
 public class Msvc extends LocalToolCommandlet {
-  private static final String MSVC_INSTALLER_URL = "https://aka.ms/vs/17/release/vs_BuildTools.exe";
 
   public Msvc(IdeContext context) {
     super(context, "msvc", Set.of());
@@ -18,13 +16,6 @@ public class Msvc extends LocalToolCommandlet {
   @Override
   protected boolean isExtract() {
     return false; // Native .exe installer
-  }
-
-  @Override
-  protected Path downloadTool(String edition, VersionIdentifier resolvedVersion) {
-    Path tempFile = this.context.getFileAccess().createTempFile("msvc", ".exe");
-    this.context.getFileAccess().download(MSVC_INSTALLER_URL, tempFile);
-    return tempFile;
   }
 
   @Override
