dab292f6788641270bde75f6393f40365632d759
laim2003 <luk.faber@gmx.de>
2026-05-26 16:46:32 +0200

    #1802: removed caching from ProjectManager

--- gui/src/main/java/com/devonfw/ide/gui/context/ProjectManager.java
+++ gui/src/main/java/com/devonfw/ide/gui/context/ProjectManager.java
@@ -2,11 +2,9 @@ package com.devonfw.ide.gui.context;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -20,9 +18,6 @@ public class ProjectManager {
   private static final Logger LOG = LoggerFactory.getLogger(ProjectManager.class);
 
   private final Path ideRootDirectory;
-
-  private final List<String> projectNames = new ArrayList<>();
-  private final Map<String, List<String>> workspaces = new HashMap<>(); // key: project name, value: list of workspace names
 
   /**
    * Service class that reads the list of projects/workspaces
@@ -47,71 +42,38 @@ public class ProjectManager {
   }
 
   /**
-   * read all projects in the users IDE_ROOT directory
+   * @return the list of project (names) in the project directory
    */
-  protected void readProjects() {
-
-    projectNames.clear();
+  public List<String> getProjectNames() {
 
     try (Stream<Path> subPaths = Files.list(ideRootDirectory)) {
-      subPaths
+      return subPaths
           .filter(Files::isDirectory)
           .map(Path::getFileName)
           .map(Path::toString)
           .filter(name -> !name.startsWith("_") && Files.exists(ideRootDirectory.resolve(name).resolve("workspaces")))
-          .forEach(projectNames::add);
+          .toList();
+
     } catch (IOException e) {
       throw new RuntimeException("Failed to read project list!", e);
     }
   }
 
   /**
-   * reads all workspaces of all loaded projects.
-   */
-  protected void readWorkspaces() {
-
-    workspaces.clear();
-
-    if (projectNames.isEmpty()) {
-      LOG.warn("Project list is empty. Therefore no workspaces can be read.");
-    }
-
-    for (String projectName : projectNames) {
-      Path projectDirectory = ideRootDirectory.resolve(projectName);
-      Path workspacesDirectory = projectDirectory.resolve("workspaces");
-
-      ArrayList<String> workspaceNames = new ArrayList<>();
-
-      try (Stream<Path> subPaths = Files.list(workspacesDirectory)) {
-        subPaths
-            .filter(Files::isDirectory)
-            .map(Path::getFileName)
-            .map(Path::toString)
-            .forEach(workspaceNames::add);
-
-        workspaces.put(projectName, workspaceNames);
-      } catch (IOException e) {
-        throw new RuntimeException("Failed to read workspaces!", e);
-      }
-    }
-  }
-
-  /**
-   * @return the list of project (names) in the project directory
-   */
-  public List<String> getProjectNames() {
-
-    readProjects();
-    return projectNames;
-  }
-
-  /**
    * @param projectName name of the project for which the workspace names should be returned
    * @return the list of workspace (names) for the given project name
    */
-  public List<String> getWorkspaceNames(String projectName) {
+  public List<String> getWorkspaceNames(String projectName) throws NotDirectoryException {
 
-    readWorkspaces();
-    return workspaces.get(projectName);
+    Path workspacesDir = ideRootDirectory.resolve(projectName).resolve("workspaces");
+    if (!Files.isDirectory(workspacesDir)) {
+      throw new NotDirectoryException("invalid workspaces directory for project: " + projectName);
+    }
+    try (Stream<Path> subPaths = Files.list(workspacesDir)) {
+      return subPaths.filter(Files::isDirectory)
+          .map(Path::getFileName).map(Path::toString).toList();
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to read workspaces for " + projectName, e);
+    }
   }
 }
