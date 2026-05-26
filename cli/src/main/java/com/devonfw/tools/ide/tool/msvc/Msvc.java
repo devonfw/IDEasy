7194b36461502a4d8ca68d3a3c8fdb467c0c9d62
Laert Llaveshi <l.llaveshi@tum.de>
2026-05-26 19:57:11 +0200

    #1719: Fix MSVC commandlet

--- cli/src/main/java/com/devonfw/tools/ide/tool/msvc/Msvc.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/msvc/Msvc.java
@@ -2,6 +2,7 @@ package com.devonfw.tools.ide.tool.msvc;
 
 import java.nio.file.Path;
 import java.util.Set;
+
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.ProcessErrorHandling;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
@@ -9,20 +10,36 @@ import com.devonfw.tools.ide.tool.ToolInstallRequest;
 
 public class Msvc extends LocalToolCommandlet {
 
+  private static final String BUILDTOOLS_URL = "https://aka.ms/vs/17/release/vs_BuildTools.exe";
+
   public Msvc(IdeContext context) {
     super(context, "msvc", Set.of());
   }
 
   @Override
-  protected boolean isExtract() {
-    return false; // Native .exe installer
+  protected boolean isIgnoreMissingSoftwareVersionFile() {
+    return true;
   }
 
   @Override
-  protected void onInstall(ToolInstallRequest request, Path installationPath, Path installerScript) {
-    this.context.newProcess().errorHandling(ProcessErrorHandling.THROW_CLI).executable(installerScript)
+  protected void performToolInstallation(ToolInstallRequest request, Path installationPath) {
+
+    this.context.getFileAccess().mkdirs(installationPath);
+
+    Path installer = this.context.getDownloadPath().resolve("vs_BuildTools.exe");
+    this.context.newProcess().errorHandling(ProcessErrorHandling.THROW_CLI)
+        .executable("curl.exe")
+        .addArgs("-fSL", "-o", installer.toString(), BUILDTOOLS_URL)
+        .run();
+
+    this.context.writeVersionFile(request.getRequested().getResolvedVersion(), installationPath);
+
+    this.context.newProcess().errorHandling(ProcessErrorHandling.THROW_CLI)
+        .executable(installer)
         .withExitCodeAcceptor(code -> (code == 0) || (code == 3010))
-        .addArgs("--quiet", "--wait", "--norestart", "--nocache", "--add", "Microsoft.VisualStudio.Workload.VCTools")
+        .addArgs("--installPath", installationPath.toString(),
+            "--add", "Microsoft.VisualStudio.Workload.VCTools",
+            "--quiet", "--wait", "--norestart", "--nocache")
         .run();
   }
 }
