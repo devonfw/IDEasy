9728d0ab4e6a63e4b638108a059b1970ff540897
meshehi <shehimedis@proton.me>
2026-07-17 08:51:58 +0200

    added context to installation

--- cli/src/main/java/com/devonfw/tools/ide/tool/PackageManagerRequest.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/PackageManagerRequest.java
@@ -32,6 +32,9 @@ public final class PackageManagerRequest {
 
   private ProcessMode processMode;
 
+  private ToolInstallRequest installRequest;
+
+
   /**
    * The constructor.
    *
@@ -165,5 +168,25 @@ public final class PackageManagerRequest {
     this.processMode = processMode;
     return this;
   }
+
+  /**
+   * @return the installation context of the PackageManagerRequest
+   */
+  public ToolInstallRequest getInstallRequest() {
+    return this.installRequest;
+  }
+
+  /**
+   * @param installRequest sets installation context if it is missing
+   * @return this {@link PackageManagerRequest} for fluent API calls.
+   */
+  public PackageManagerRequest setInstallRequest(ToolInstallRequest installRequest) {
+    if (this.installRequest != null) {
+      throw new IllegalStateException();
+    }
+    this.installRequest = installRequest;
+    return this;
+  }
+
 
 }
