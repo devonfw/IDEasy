e161665c28eed0b8621d4aa6d8e6dc2c4dfe6957
caylipp <philipphoang3005@gmail.com>
2026-07-22 10:42:22 +0200

    #825: Add Gc Log Analyzer commandlet - fix test

--- cli/src/test/java/com/devonfw/tools/ide/tool/gcloganalyzer/GcLogAnalyzerTest.java
+++ cli/src/test/java/com/devonfw/tools/ide/tool/gcloganalyzer/GcLogAnalyzerTest.java
@@ -22,37 +22,66 @@ class GcLogAnalyzerTest extends AbstractIdeContextTest {
   @Test
   void testGcLogAnalyzerInstall(WireMockRuntimeInfo wireMockRuntimeInfo) {
 
-    IdeTestContext context = newContext(PROJECT_GCLOGANALYZER, wireMockRuntimeInfo);
-    GcLogAnalyzer commandlet = context.getCommandletManager().getCommandlet(GcLogAnalyzer.class);
+    // arrange
+    IdeTestContext context =
+        newContext(PROJECT_GCLOGANALYZER, wireMockRuntimeInfo);
+    GcLogAnalyzer commandlet = new GcLogAnalyzer(context);
 
+    // act
     commandlet.install();
 
-    assertThat(commandlet.getInstalledVersion().toString()).isEqualTo(VERSION);
-    assertThat(context.getSoftwarePath().resolve("gcloganalyzer/.ide.software.version")).hasContent(VERSION);
-    assertThat(context.getSoftwarePath()
-        .resolve("gcloganalyzer/GCLogAnalyzer-" + ARTIFACT_VERSION + "-ca.jar"))
-        .hasContent("This is a fake GC Log Analyzer jar file.\n");
-    assertThat(context.getSoftwarePath()
-        .resolve("gcloganalyzer/GCLogAnalyzer-" + ARTIFACT_VERSION + "-ca"))
-        .doesNotExist();
-    assertThat(context).logAtSuccess()
-        .hasMessageContaining("Successfully installed gcloganalyzer in version " + VERSION);
+    // assert
+    assertInstallation(context, commandlet);
   }
 
   @Test
-  void testGcLogAnalyzerRunWithArguments(WireMockRuntimeInfo wireMockRuntimeInfo) {
+  void testGcLogAnalyzerRunWithArguments(
+      WireMockRuntimeInfo wireMockRuntimeInfo) {
 
-    IdeTestContext context = newContext(PROJECT_GCLOGANALYZER, wireMockRuntimeInfo);
-    GcLogAnalyzer commandlet = context.getCommandletManager().getCommandlet(GcLogAnalyzer.class);
+    // arrange
+    IdeTestContext context =
+        newContext(PROJECT_GCLOGANALYZER, wireMockRuntimeInfo);
+    GcLogAnalyzer commandlet = new GcLogAnalyzer(context);
 
     commandlet.arguments.addValue("--generate-html");
     commandlet.arguments.addValue("output");
     commandlet.arguments.addValue("gc.log");
 
+    // act
     commandlet.run();
 
-    assertThat(context.getSoftwarePath().resolve("java/bin/java")).exists();
+    // assert
+    assertInstallation(context, commandlet);
     assertThat(context).logAtInfo().hasMessageContaining(
-        "java -jar GCLogAnalyzer-" + ARTIFACT_VERSION + "-ca.jar --generate-html output gc.log");
+        "java -jar GCLogAnalyzer-" + ARTIFACT_VERSION
+            + "-ca.jar --generate-html output gc.log");
+  }
+
+  private void assertInstallation(
+      IdeTestContext context, GcLogAnalyzer commandlet) {
+
+    assertThat(commandlet.getInstalledVersion().toString())
+        .isEqualTo(VERSION);
+
+    assertThat(context.getSoftwarePath()
+        .resolve("java/bin/java"))
+        .exists();
+
+    assertThat(context.getSoftwarePath()
+        .resolve("gcloganalyzer/.ide.software.version"))
+        .hasContent(VERSION);
+
+    assertThat(context.getSoftwarePath()
+        .resolve("gcloganalyzer/GCLogAnalyzer-"
+            + ARTIFACT_VERSION + "-ca.jar"))
+        .hasContent("This is a fake GC Log Analyzer jar file.\n");
+
+    assertThat(context.getSoftwarePath()
+        .resolve("gcloganalyzer/GCLogAnalyzer-"
+            + ARTIFACT_VERSION + "-ca"))
+        .doesNotExist();
+
+    assertThat(context).logAtSuccess().hasMessageContaining(
+        "Successfully installed gcloganalyzer in version " + VERSION);
   }
 }
