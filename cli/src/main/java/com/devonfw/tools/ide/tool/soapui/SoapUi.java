4d5cc63dffcbc6e88405d828802991c30c8bf24e
mariusfreyer <marius.freyer@gmail.com>
2026-07-27 09:26:33 +0200

    #2187: Fix review remarks

--- cli/src/main/java/com/devonfw/tools/ide/tool/soapui/SoapUi.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/soapui/SoapUi.java
@@ -1,44 +1,47 @@
 package com.devonfw.tools.ide.tool.soapui;
 
-import java.util.Set;
-
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.ProcessMode;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
+
+import java.util.Set;
 
 /**
  * {@link LocalToolCommandlet} for <a href="https://www.soapui.org/">SoapUI</a>, an open source API testing tool for SOAP and REST services.
  */
 public class SoapUi extends LocalToolCommandlet {
 
-  private static final String SOAPUI = "soapui";
+    private static final String SOAPUI = "soapui";
 
-  private static final String SOAPUI_BAT = SOAPUI + ".bat";
+    private static final String SOAPUI_BAT = SOAPUI + ".bat";
 
-  private static final String SOAPUI_BASH_SCRIPT = SOAPUI + ".sh";
+    private static final String SOAPUI_BASH_SCRIPT = SOAPUI + ".sh";
 
-  /**
-   * The constructor.
-   *
-   * @param ideContext {@link IdeContext}.
-   */
-  public SoapUi(IdeContext ideContext) {
-    super(ideContext, SOAPUI, Set.of(Tag.TEST, Tag.REST));
-  }
+    /**
+     * The constructor.
+     *
+     * @param ideContext {@link IdeContext}.
+     */
+    public SoapUi(IdeContext ideContext) {
 
-  @Override
-  protected String getBinaryName() {
-    if (this.context.getSystemInfo().isWindows()) {
-      return SOAPUI_BAT;
-    } else {
-      return SOAPUI_BASH_SCRIPT;
+        super(ideContext, SOAPUI, Set.of(Tag.TEST, Tag.REST));
     }
-  }
 
-  @Override
-  protected void doRun() {
-    runTool(ProcessMode.BACKGROUND_SILENT, null, this.arguments.asList());
-  }
+    @Override
+    protected String getBinaryName() {
+
+        if (this.context.getSystemInfo().isWindows()) {
+            return SOAPUI_BAT;
+        } else {
+            return SOAPUI_BASH_SCRIPT;
+        }
+    }
+
+    @Override
+    protected void doRun() {
+
+        runTool(ProcessMode.BACKGROUND_SILENT, null, this.arguments.asList());
+    }
 
 }
