8f8f19166eb0740b3d6c34e49a4a54168c03ce5d
Hiepiscus <le-hiep.ha@capgemini.com>
2026-07-27 14:25:55 +0200

    #2182: remove sorted list and correct a few formatting violations

--- gui/src/main/java/com/devonfw/ide/gui/MainController.java
+++ gui/src/main/java/com/devonfw/ide/gui/MainController.java
@@ -7,6 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
