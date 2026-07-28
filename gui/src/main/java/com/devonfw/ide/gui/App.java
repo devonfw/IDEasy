571524eb31064e42d9e4a02b04c321db25c067b5
mariusfreyer <marius.freyer@gmail.com>
2026-07-28 14:42:54 +0200

    #2206: Fix formatting

--- gui/src/main/java/com/devonfw/ide/gui/App.java
+++ gui/src/main/java/com/devonfw/ide/gui/App.java
@@ -2,11 +2,6 @@ package com.devonfw.ide.gui;
 
 import java.io.IOException;
 
-import com.devonfw.ide.gui.modal.IdeDialog;
-import com.devonfw.ide.gui.nls.NlsService;
-import com.devonfw.tools.ide.os.SystemInfoImpl;
-import com.devonfw.tools.ide.variable.IdeVariables;
-import com.devonfw.tools.ide.version.IdeVersion;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +11,12 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import com.devonfw.ide.gui.modal.IdeDialog;
+import com.devonfw.ide.gui.nls.NlsService;
+import com.devonfw.tools.ide.os.SystemInfoImpl;
+import com.devonfw.tools.ide.variable.IdeVariables;
+import com.devonfw.tools.ide.version.IdeVersion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
