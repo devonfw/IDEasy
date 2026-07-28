1d1ee0896b64b495f88dd279758f4b27e1c67edb
oanding <oscar.anding@capgemini.com>
2026-07-28 11:56:40 +0200

    #2126 Removed investigation comments in MainController.java

--- gui/src/main/java/com/devonfw/ide/gui/MainController.java
+++ gui/src/main/java/com/devonfw/ide/gui/MainController.java
@@ -77,20 +77,13 @@ public class MainController {
   }
 
   private void initLanguageComboBox() {
-
     this.languageMap.clear();
     selectedLanguage.getItems().clear();
 
-    var availableLocales = nlsService.getAvailableLocales();
-    LOG.info(availableLocales.toString());
-    // This loop is iterating over de and en but only sets the key for de twice
-    for (Locale locale : availableLocales) {
-      // Maybe some file was not found and then he defaults to ROOT or something.
+    for (Locale locale : nlsService.getAvailableLocales()) {
       String displayName = nlsService.getLanguageDisplayName(locale);
       this.languageMap.put(displayName, locale);
     }
-
-    var languageMap = this.languageMap.keySet();
 
     selectedLanguage.getItems().addAll(this.languageMap.keySet());
     //initial value
