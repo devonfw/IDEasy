452b1d112d456b91d7594123caa873d6e7613dad
mariusfreyer <marius.freyer@gmail.com>
2026-07-28 14:35:45 +0200

    #2206: Add system property for macOS Dock title

--- gui/src/main/java/com/devonfw/ide/gui/App.java
+++ gui/src/main/java/com/devonfw/ide/gui/App.java
@@ -1,6 +1,12 @@
 package com.devonfw.ide.gui;
 
 import java.io.IOException;
+
+import com.devonfw.ide.gui.modal.IdeDialog;
+import com.devonfw.ide.gui.nls.NlsService;
+import com.devonfw.tools.ide.os.SystemInfoImpl;
+import com.devonfw.tools.ide.variable.IdeVariables;
+import com.devonfw.tools.ide.version.IdeVersion;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -10,88 +16,99 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.devonfw.ide.gui.modal.IdeDialog;
-import com.devonfw.ide.gui.nls.NlsService;
-import com.devonfw.tools.ide.variable.IdeVariables;
-import com.devonfw.tools.ide.version.IdeVersion;
 
 /**
  * GUI Application for IDEasy
  */
 public class App extends Application {
 
-  Parent root;
+    Parent root;
 
-  private Stage primaryStage;
+    private Stage primaryStage;
 
-  private NlsService nlsService;
+    private NlsService nlsService;
 
-  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-  @Override
-  public void start(Stage primaryStage) throws IOException {
+    @Override
+    public void init() {
 
-    this.primaryStage = primaryStage;
-
-    this.nlsService = new NlsService(null);
-
-    Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-          LOG.error("Uncaught exception in thread {}: {}", thread.getName(), throwable.getMessage(), throwable);
-          Platform.runLater(() -> new IdeDialog(IdeDialog.AlertType.ERROR, throwable.getMessage()).showAndWait());
-        }
-    );
-
-    root = loadMainView();
-
-    this.nlsService.addLocaleChangeListener(this::reloadMainView);
-
-    Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
-    Scene scene = new Scene(root, bounds.getWidth() / 2, bounds.getHeight() / 2);
-
-    Image icon = new Image("com/devonfw/ide/gui/assets/devonfw.png");
-    primaryStage.getIcons().add(icon);
-    primaryStage.setTitle("IDEasy - version " + IdeVersion.getVersionString());
-    primaryStage.setScene(scene);
-    primaryStage.setMinWidth(scene.getWidth());
-    primaryStage.setMinHeight(scene.getHeight());
-    primaryStage.show();
-  }
-
-  @Override
-  public void stop() {
-
-    this.nlsService.removeLocaleChangeListener(this::reloadMainView);
-  }
-
-  private void reloadMainView() {
-
-    try {
-      Parent reloadedRoot = loadMainView();
-      this.root = reloadedRoot;
-      if (this.primaryStage != null && this.primaryStage.getScene() != null) {
-        this.primaryStage.getScene().setRoot(reloadedRoot);
-      }
-    } catch (IOException e) {
-      LOG.error("Failed to reload main view after locale change", e);
+        configureMacOSDockTitle();
     }
-  }
 
-  private Parent loadMainView() throws IOException {
+    /**
+     * Set the macOS dock icon label to "IDEasy" instead of the default process name "java". No-op on other platforms.
+     */
+    private void configureMacOSDockTitle() {
 
-    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
-    fxmlLoader.setResources(this.nlsService.getResourceBundle());
-    fxmlLoader.setController(new MainController(System.getenv(IdeVariables.IDE_ROOT.getName()), this.nlsService));
-    return fxmlLoader.load();
-  }
+        if (SystemInfoImpl.INSTANCE.isMac()) {
+            System.setProperty("apple.awt.application.name", "IDEasy");
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        this.primaryStage = primaryStage;
+
+        this.nlsService = new NlsService(null);
+
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+                    LOG.error("Uncaught exception in thread {}: {}", thread.getName(), throwable.getMessage(), throwable);
+                    Platform.runLater(() -> new IdeDialog(IdeDialog.AlertType.ERROR, throwable.getMessage()).showAndWait());
+                }
+        );
+
+        root = loadMainView();
+
+        this.nlsService.addLocaleChangeListener(this::reloadMainView);
+
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        Scene scene = new Scene(root, bounds.getWidth() / 2, bounds.getHeight() / 2);
+
+        Image icon = new Image("com/devonfw/ide/gui/assets/devonfw.png");
+        primaryStage.getIcons().add(icon);
+        primaryStage.setTitle("IDEasy - version " + IdeVersion.getVersionString());
+        primaryStage.setScene(scene);
+        primaryStage.setMinWidth(scene.getWidth());
+        primaryStage.setMinHeight(scene.getHeight());
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+
+        this.nlsService.removeLocaleChangeListener(this::reloadMainView);
+    }
+
+    private void reloadMainView() {
+
+        try {
+            Parent reloadedRoot = loadMainView();
+            this.root = reloadedRoot;
+            if (this.primaryStage != null && this.primaryStage.getScene() != null) {
+                this.primaryStage.getScene().setRoot(reloadedRoot);
+            }
+        } catch (IOException e) {
+            LOG.error("Failed to reload main view after locale change", e);
+        }
+    }
+
+    private Parent loadMainView() throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("main-view.fxml"));
+        fxmlLoader.setResources(this.nlsService.getResourceBundle());
+        fxmlLoader.setController(new MainController(System.getenv(IdeVariables.IDE_ROOT.getName()), this.nlsService));
+        return fxmlLoader.load();
+    }
 
 
-  @SuppressWarnings("MissingJavadoc")
-  public static void main(String[] args) {
+    @SuppressWarnings("MissingJavadoc")
+    public static void main(String[] args) {
 
-    launch(args);
-  }
+        launch(args);
+    }
+
 }
