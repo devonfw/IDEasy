08c34a6e0e776966959610f598517862e83122f0
mariusfreyer <marius.freyer@gmail.com>
2026-08-03 16:34:24 +0200

    #2187: Fix review remarks

--- cli/src/main/java/com/devonfw/tools/ide/tool/soapui/SoapUi.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/soapui/SoapUi.java
@@ -1,10 +1,12 @@
 package com.devonfw.tools.ide.tool.soapui;
 
+import java.util.List;
 import java.util.Set;
 
 import com.devonfw.tools.ide.common.Tag;
 import com.devonfw.tools.ide.context.IdeContext;
 import com.devonfw.tools.ide.process.ProcessMode;
+import com.devonfw.tools.ide.process.ProcessResult;
 import com.devonfw.tools.ide.tool.LocalToolCommandlet;
 
 /**
@@ -12,36 +14,36 @@ import com.devonfw.tools.ide.tool.LocalToolCommandlet;
  */
 public class SoapUi extends LocalToolCommandlet {
 
-    private static final String SOAPUI = "soapui";
+  private static final String SOAPUI = "soapui";
 
-    private static final String SOAPUI_BAT = SOAPUI + ".bat";
+  private static final String SOAPUI_BAT = SOAPUI + ".bat";
 
-    private static final String SOAPUI_BASH_SCRIPT = SOAPUI + ".sh";
+  private static final String SOAPUI_BASH_SCRIPT = SOAPUI + ".sh";
 
-    /**
-     * The constructor.
-     *
-     * @param ideContext {@link IdeContext}.
-     */
-    public SoapUi(IdeContext ideContext) {
+  /**
+   * The constructor.
+   *
+   * @param ideContext {@link IdeContext}.
+   */
+  public SoapUi(IdeContext ideContext) {
 
-        super(ideContext, SOAPUI, Set.of(Tag.TEST, Tag.REST));
+    super(ideContext, SOAPUI, Set.of(Tag.TEST, Tag.REST));
+  }
+
+  @Override
+  protected String getBinaryName() {
+
+    if (this.context.getSystemInfo().isWindows()) {
+      return SOAPUI_BAT;
+    } else {
+      return SOAPUI_BASH_SCRIPT;
     }
+  }
 
-    @Override
-    protected String getBinaryName() {
+  @Override
+  public ProcessResult runTool(List<String> args) {
 
-        if (this.context.getSystemInfo().isWindows()) {
-            return SOAPUI_BAT;
-        } else {
-            return SOAPUI_BASH_SCRIPT;
-        }
-    }
-
-    @Override
-    protected void doRun() {
-
-        runTool(ProcessMode.BACKGROUND_SILENT, null, this.arguments.asList());
-    }
+    return runTool(ProcessMode.BACKGROUND_SILENT, null, args);
+  }
 
 }
