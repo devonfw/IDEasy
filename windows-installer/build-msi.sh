c7b51710b50dff35a33431a9cd1644b0d00cb1aa
caylipp <philipphoang3005@gmail.com>
2026-08-05 14:29:41 +0200

    #2255: Address MSI build script review

--- windows-installer/build-msi.sh
+++ windows-installer/build-msi.sh
@@ -9,14 +9,16 @@ mvn -B -ntp -f documentation/pom.xml clean install
 
 mkdir -p windows-installer/msi-files
 cp documentation/target/generated-docs/IDEasy.pdf windows-installer/msi-files
-cp -r cli/target/package/. windows-installer/msi-files
+cp -r cli/target/package/* windows-installer/msi-files
 rm -rf windows-installer/msi-files/system/mac
 rm -rf windows-installer/msi-files/system/linux
 cp cli/target/ideasy.exe windows-installer/msi-files/bin
 
 cd windows-installer
 
-dotnet tool install --global wix --version 5.0.2
+if ! command -v wix > /dev/null 2>&1; then
+  dotnet tool install --global wix --version 5.0.2
+fi
 wix extension add WixToolset.UI.wixext/5.0.2
 wix extension add WixToolset.Util.wixext/5.0.2
 
