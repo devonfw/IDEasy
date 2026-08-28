79da20217fb213417c81b4caf4a63e7d128c2077
laim2003 <luk.faber@gmx.de>
2026-08-28 17:34:38 +0200

    #1695: added javadocs

--- cli/src/main/java/com/devonfw/tools/ide/commandlet/update/settings/SettingsHealthCheckResult.java
+++ cli/src/main/java/com/devonfw/tools/ide/commandlet/update/settings/SettingsHealthCheckResult.java
@@ -5,31 +5,34 @@ import com.devonfw.tools.ide.git.repository.RepositoryType;
 import java.nio.file.Path;
 
 /**
- * Result of the settings {@link SettingsUpdater#checkSettings() health check}.
+ * Result of the settings {@link SettingsUpdater#checkSettings(Path)}  health check}.
  *
  * @param status the {@link HealthCheckResultStatus}.
  * @param repositoryType the {@link RepositoryType} of the settings repository.
  * @param errorMessage the reason why the settings could not be updated or {@code null} if the health check succeeded.
+ * @param temporarySettingsDirectory path to the temporary folder this health check was performed on.
  */
 public record SettingsHealthCheckResult(HealthCheckResultStatus status, RepositoryType repositoryType, Path temporarySettingsDirectory, String errorMessage) {
 
   /**
    * @param status the {@link HealthCheckResultStatus}.
    * @param repositoryType the {@link RepositoryType}.
+   * @param temporarySettingsDirectory path to the temporary folder this health check was performed on.
    * @return a {@link SettingsHealthCheckResult} for a successful health check.
    */
-  public static SettingsHealthCheckResult of(HealthCheckResultStatus status, RepositoryType repositoryType, Path temporaryRepoDirectory) {
+  public static SettingsHealthCheckResult of(HealthCheckResultStatus status, RepositoryType repositoryType, Path temporarySettingsDirectory) {
 
-    return new SettingsHealthCheckResult(status, repositoryType, temporaryRepoDirectory, null);
+    return new SettingsHealthCheckResult(status, repositoryType, temporarySettingsDirectory, null);
   }
 
   /**
    * @param repositoryType the {@link RepositoryType} of the settings that are already present.
    * @param errorMessage the reason why the settings could not be updated.
+   * @param temporarySettingsDirectory path to the temporary folder this health check was performed on.
    * @return a {@link SettingsHealthCheckResult} for a failed but recoverable health check.
    */
-  public static SettingsHealthCheckResult failed(RepositoryType repositoryType, String errorMessage, Path temporaryRepoDirectory) {
+  public static SettingsHealthCheckResult failed(RepositoryType repositoryType, String errorMessage, Path temporarySettingsDirectory) {
 
-    return new SettingsHealthCheckResult(HealthCheckResultStatus.SETTINGS_INVALID, repositoryType, temporaryRepoDirectory, errorMessage);
+    return new SettingsHealthCheckResult(HealthCheckResultStatus.SETTINGS_INVALID, repositoryType, temporarySettingsDirectory, errorMessage);
   }
 }
