cc58deff724e6bedd6170bf53aa730f21f5e1ae7
Laert Llaveshi <l.llaveshi@tum.de>
2026-09-16 11:00:29 +0200

    #854: Use resolved kubectl binary for version detection

--- cli/src/main/java/com/devonfw/tools/ide/tool/kubectl/KubeCtl.java
+++ cli/src/main/java/com/devonfw/tools/ide/tool/kubectl/KubeCtl.java
@@ -38,11 +38,12 @@ public class KubeCtl extends DelegatingToolCommandlet {
   @Override
   protected EditionAndVersion computeInstalledEditionAndVersion() {
 
-    if (!isCommandAvailable(this.tool)) {
+    String kubectl = getBinaryName();
+    if (!isCommandAvailable(kubectl)) {
       return super.computeInstalledEditionAndVersion();
     }
 
-    List<String> outputs = this.context.newProcess().runAndGetOutput(this.tool, "version", "--client");
+    List<String> outputs = this.context.newProcess().runAndGetOutput(kubectl, "version", "--client");
     String singleLineOutput = String.join("\n", outputs);
     VersionIdentifier version = resolveVersionWithPattern(singleLineOutput, KUBECTL_VERSION_PATTERN);
     return new EditionAndVersion(this.tool, version);
